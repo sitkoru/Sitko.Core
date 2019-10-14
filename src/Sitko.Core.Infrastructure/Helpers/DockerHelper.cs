29ccb11126bf4c97325c425b035926e42e413864
George <sonicgd@gmail.com>
2019-10-14 10:30:37 +0500

    Move docker helper to infra

--- src/Sitko.Core.Infrastructure/Helpers/DockerHelper.cs
+++ src/Sitko.Core.Infrastructure/Helpers/DockerHelper.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 
-namespace Sitko.Core.Grpc.Server
+namespace Sitko.Core.Infrastructure.Helpers
 {
     public static class DockerHelper
     {
