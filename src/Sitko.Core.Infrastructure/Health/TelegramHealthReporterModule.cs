708264d26aab2064026c0562257097b9109e7f9d
George <sonicgd@gmail.com>
2019-10-14 14:40:28 +0500

    Require health module

--- src/Sitko.Core.Infrastructure/Health/TelegramHealthReporterModule.cs
+++ src/Sitko.Core.Infrastructure/Health/TelegramHealthReporterModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -9,15 +10,16 @@ namespace Sitko.Core.Infrastructure.Health
 {
     public class TelegramHealthReporterModule : BaseApplicationModule<TelegramHealthCheckPublisherOptions>
     {
+        public override List<Type> GetRequiredModules()
+        {
+            return new List<Type> {typeof(HealthModule)};
+        }
+
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
             IHostEnvironment environment)
         {
             base.ConfigureServices(services, configuration, environment);
-            services.AddHealthChecks();
-            services.Configure<HealthCheckPublisherOptions>(options =>
-            {
-                options.Delay = TimeSpan.FromSeconds(2);
-            });
+            services.Configure<HealthCheckPublisherOptions>(options => { });
 
             services.AddSingleton<IHealthCheckPublisher, TelegramHealthCheckPublisher>();
         }
