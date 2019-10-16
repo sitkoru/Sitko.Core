68267706fc82775bb9bd2c0a881ebfc350007fd3
George <sonicgd@gmail.com>
2019-10-16 12:08:00 +0500

    Add ability to define all assemblies with automapper profiles in config

--- src/Sitko.Core.Infrastructure/AutoMapperModule.cs
+++ src/Sitko.Core.Infrastructure/AutoMapperModule.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,19 +9,20 @@ using Sitko.Core.App;
 
 namespace Sitko.Core.Infrastructure
 {
-    public class AutoMapperModule<T> : BaseApplicationModule<AutoMapperModuleConfig>
+    public class AutoMapperModule : BaseApplicationModule<AutoMapperModuleConfig>
     {
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
             IHostEnvironment environment)
         {
             base.ConfigureServices(services, configuration, environment);
-            services.AddAutoMapper(Config.Configure, typeof(T).Assembly, typeof(AutoMapperModule<>).Assembly);
+            services.AddAutoMapper(Config.Configure, Config.Assemblies);
         }
     }
 
     public class AutoMapperModuleConfig
     {
         public Action<IMapperConfigurationExpression> Configure { get; }
+        public readonly List<Assembly> Assemblies = new List<Assembly> {typeof(AutoMapperModule).Assembly};
 
         public AutoMapperModuleConfig(Action<IMapperConfigurationExpression> configure)
         {
