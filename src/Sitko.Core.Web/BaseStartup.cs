771ad7f97b8346e030a8a1b1f7a4e61416690b86
George <sonicgd@gmail.com>
2019-10-31 11:35:47 +0500

    Apply fix for same site cookies policy in new chrome - https://devblogs.microsoft.com/aspnet/upcoming-samesite-cookie-changes-in-asp-net-and-asp-net-core/

--- src/Sitko.Core.Web/BaseStartup.cs
+++ src/Sitko.Core.Web/BaseStartup.cs
@@ -4,6 +4,7 @@ using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -33,6 +34,14 @@ namespace Sitko.Core.Web
             services.AddHttpContextAccessor();
             services.AddScoped<Flasher>();
             services.AddSession();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = (SameSiteMode)(-1);
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            });
             ConfigureAppServices(services);
         }
 
@@ -114,7 +123,7 @@ namespace Sitko.Core.Web
                 appBuilder.UseDeveloperExceptionPage();
             }
 
-            
+
             appBuilder.UseMiddleware<RequestIdMiddleware>();
             if (Environment.IsProduction())
             {
@@ -133,13 +142,14 @@ namespace Sitko.Core.Web
 
             CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
             CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
-
+            appBuilder.UseCookiePolicy();
             appBuilder.UseStaticFiles();
             appBuilder.UseSession();
 
             application.BeforeRoutingHook(Configuration, Environment, appBuilder);
             ConfigureBeforeRouting(appBuilder);
             appBuilder.UseRouting();
+
             application.AfterRoutingHook(Configuration, Environment, appBuilder);
             ConfigureAfterRouting(appBuilder);
 
@@ -148,6 +158,56 @@ namespace Sitko.Core.Web
                 application.EndpointsHook(Configuration, Environment, appBuilder, endpoints);
                 ConfigureEndpoints(appBuilder, endpoints);
             });
+        }
+
+        // https://devblogs.microsoft.com/aspnet/upcoming-samesite-cookie-changes-in-asp-net-and-asp-net-core/
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite > (SameSiteMode)(-1))
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                if (DisallowsSameSiteNone(userAgent))
+                {
+                    // TODO: Update on 3.1 to user SameSiteMode.Unspecified
+                    // For .NET Core < 3.1 set SameSite = -1
+                    options.SameSite = (SameSiteMode)(-1);
+                }
+            }
+        }
+
+        public static bool DisallowsSameSiteNone(string userAgent)
+        {
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
