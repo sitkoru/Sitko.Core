70aee94c8077f5ffbedaa9e74d1d744842e4e61c
George <sonicgd@gmail.com>
2019-11-01 10:12:50 +0500

    Allow to change microsoft messages logging level at runtime

--- src/Sitko.Core.Logging/LogLevelSwitcher.cs
+++ src/Sitko.Core.Logging/LogLevelSwitcher.cs
@@ -5,5 +5,6 @@ namespace Sitko.Core.Logging
     public class LogLevelSwitcher
     {
         public readonly LoggingLevelSwitch Switch = new LoggingLevelSwitch();
+        public readonly LoggingLevelSwitch MsMessagesSwitch = new LoggingLevelSwitch();
     }
 }
