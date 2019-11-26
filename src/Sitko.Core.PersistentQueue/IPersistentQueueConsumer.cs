0aa08c129539f2c8cfbca633c27899415f17f5d5
George <sonicgd@gmail.com>
2019-11-26 17:08:51 +0500

    Add ability to pass different options to consumer

--- src/Sitko.Core.PersistentQueue/IPersistentQueueConsumer.cs
+++ src/Sitko.Core.PersistentQueue/IPersistentQueueConsumer.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Threading.Tasks;
 using Google.Protobuf;
+using Sitko.Core.PersistentQueue.HostedService;
 
 namespace Sitko.Core.PersistentQueue
 {
     public interface IPersistentQueueConsumer<TMessage> : IDisposable where TMessage : IMessage, new()
     {
-        Task RunAsync(Func<TMessage, PersistentQueueMessageContext, Task<bool>> callback);
+        Task RunAsync(Func<TMessage, PersistentQueueMessageContext, Task<bool>> callback,
+            PersistedQueueHostedServiceOptions<TMessage> options = null);
 
         Task RunWithResponseAsync<TResponse>(
-            Func<TMessage, PersistentQueueMessageContext, Task<(bool isSuccess, TResponse response)>> callback)
+            Func<TMessage, PersistentQueueMessageContext, Task<(bool isSuccess, TResponse response)>> callback,
+            PersistedQueueHostedServiceOptions<TMessage> options = null)
             where TResponse : IMessage, new();
 
         Task StopAsync();
