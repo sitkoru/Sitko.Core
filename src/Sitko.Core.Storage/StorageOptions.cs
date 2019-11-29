3da42e935962bc944e6e270ba6b730970b7cf3c7
George <sonicgd@gmail.com>
2019-11-29 11:16:29 +0500

    Add image processing to storage

--- src/Sitko.Core.Storage/StorageOptions.cs
+++ src/Sitko.Core.Storage/StorageOptions.cs
@@ -5,5 +5,14 @@ namespace Sitko.Core.Storage
     public class StorageOptions : IStorageOptions
     {
         public Uri PublicUri { get; set; }
+
+        public bool ProcessImages { get; set; }
+
+        public int LargeThumbnailWidth { get; set; } = 800;
+        public int LargeThumbnailHeight { get; set; } = 800;
+        public int MediumThumbnailWidth { get; set; } = 300;
+        public int MediumThumbnailHeight { get; set; } = 300;
+        public int SmallThumbnailWidth { get; set; } = 100;
+        public int SmallThumbnailHeight { get; set; } = 100;
     }
 }
