6c52113c9e8300d9cd5acd359fdc5bfe48a6754f
George <sonicgd@gmail.com>
2019-12-18 12:01:16 +0500

    Add new email modules

---
+++ src/Sitko.Core.Email/MailEntry.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Sitko.Core.Email
+{
+    public class MailEntry<T> : MailEntry
+    {
+        public T Message { get; }
+
+        public MailEntry(string subject, IEnumerable<string> recipients, T message) : base(subject, recipients)
+        {
+            Message = message;
+        }
+    }
+
+    public class MailEntry
+    {
+        public MailEntry(string subject, IEnumerable<string> recipients)
+        {
+            Subject = subject;
+            Recipients = recipients.ToArray();
+        }
+
+        public string Subject { get; }
+        public string[] Recipients { get; }
+        public List<MailEntryAttachment> Attachments { get; } = new List<MailEntryAttachment>();
+    }
+
+    public class MailEntryAttachment
+    {
+        public MailEntryAttachment(string name, string type, Stream data)
+        {
+            Name = name;
+            Type = type;
+            Data = data;
+        }
+
+        public string Name { get; }
+        public string Type { get; }
+        public Stream Data { get; }
+    }
+}
