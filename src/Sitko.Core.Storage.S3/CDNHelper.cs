663f724d502bea2b3c652002f540dd7923df090b
George <sonicgd@gmail.com>
2019-12-26 11:00:34 +0500

    Make CDNHelper generic

--- src/Sitko.Core.Storage.S3/CDNHelper.cs
+++ src/Sitko.Core.Storage.S3/CDNHelper.cs
@@ -4,16 +4,16 @@ using JetBrains.Annotations;
 namespace Sitko.Core.Storage.S3
 {
     [UsedImplicitly]
-    public class CdnHelper
+    public class CdnHelper<T> where T : S3StorageOptions
     {
-        private readonly S3StorageOptions _options;
+        private readonly T _options;
 
-        public CdnHelper(S3StorageOptions options)
+        public CdnHelper(T options)
         {
             _options = options;
         }
 
-        public string GetCdnUrl(string url, Func<S3StorageOptions, string> path)
+        public string GetCdnUrl(string url, Func<T, string> path)
         {
             return $"{_options.PublicUri}{path(_options)}/{url}";
         }
