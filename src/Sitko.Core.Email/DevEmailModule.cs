cce6eb7f9fe1485bc53154e9e114a9c8eb367a95
George <sonicgd@gmail.com>
2019-12-27 14:38:38 +0500

    Add dev sender

--- src/Sitko.Core.Email/DevEmailModule.cs
+++ src/Sitko.Core.Email/DevEmailModule.cs
@@ -1,4 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentEmail.Core;
+using FluentEmail.Core.Interfaces;
+using FluentEmail.Core.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace Sitko.Core.Email
 {
@@ -6,6 +15,7 @@ namespace Sitko.Core.Email
     {
         protected override void ConfigureBuilder(FluentEmailServicesBuilder builder)
         {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender, DevEmailSender>());
         }
     }
 
@@ -13,6 +23,33 @@ namespace Sitko.Core.Email
     {
         public DevEmailModuleConfig() : base("dev@localhost", "localhost", "http")
         {
+        }
+    }
+
+    public class DevEmailSender : ISender
+    {
+        private readonly ILogger<DevEmailSender> _logger;
+
+        public DevEmailSender(ILogger<DevEmailSender> logger)
+        {
+            _logger = logger;
+        }
+
+        public SendResponse Send(IFluentEmail email, CancellationToken? token = null)
+        {
+            _logger.LogInformation("Send email {subject} to {recipients}", email.Data.Subject,
+                string.Join(", ", email.Data.ToAddresses));
+            return new SendResponse {MessageId = Guid.NewGuid().ToString(), ErrorMessages = new List<string>()};
+        }
+
+        public Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null)
+        {
+            _logger.LogInformation("Send email {subject} to {recipients}", email.Data.Subject,
+                string.Join(", ", email.Data.ToAddresses));
+            return Task.FromResult(new SendResponse
+            {
+                MessageId = Guid.NewGuid().ToString(), ErrorMessages = new List<string>()
+            });
         }
     }
 }
