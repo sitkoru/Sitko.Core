5c522dd4d99dd535f9d419ee24ffdab388fc0023
George <sonicgd@gmail.com>
2020-01-24 17:05:08 +0500

    Drop old persistent queue

--- src/Sitko.Core.PersistentQueue/Internal/ReconnectedEventHandlerArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sitko.Core.PersistentQueue.Internal
-{
-    public delegate void ReconnectedEventHandler(object sender, ReconnectedEventHandlerArgs args);
-
-    public class ReconnectedEventHandlerArgs
-    {
-    }
-}
