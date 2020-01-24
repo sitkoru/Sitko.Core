d9db424e2c45210b0d7751f1e18231e0b083a09b
George <sonicgd@gmail.com>
2020-01-24 15:15:57 +0500

    Fix targeting, netstandard2.1 is enough

---
+++ src/Sitko.Core.Queue.InMemory/ChannelExtensions.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Sitko.Core.Queue.InMemory
+{
+    public static class ChannelExtensions
+    {
+        public static async IAsyncEnumerable<T> ReadAllAsync<T>(this ChannelReader<T> reader,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            while (await reader.WaitToReadAsync(cancellationToken).ConfigureAwait(false))
+            {
+                while (reader.TryRead(out T item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
