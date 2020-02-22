a71af7267c4bde48471423a7a3eb24b75044ffe7
George <sonicgd@gmail.com>
2020-02-22 19:45:32 +0500

    Move human size method to helper

---
+++ src/Sitko.Core.Storage/Helpers.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Sitko.Core.Storage
+{
+    public static class Helpers
+    {
+        private static readonly string[] _units = {"bytes", "KB", "MB", "GB", "TB", "PB"};
+
+        public static string HumanSize(long fileSize)
+        {
+            if (fileSize < 1)
+            {
+                return "-";
+            }
+
+            var unit = 0;
+
+            double size = fileSize;
+            while (size >= 1024)
+            {
+                size /= 1024;
+                unit++;
+            }
+
+            return $"{Math.Round(size, 2):N}{_units[unit]}";
+        }
+    }
+}
--- src/Sitko.Core.Storage/StorageItem.cs
+++ src/Sitko.Core.Storage/StorageItem.cs
@@ -9,27 +9,12 @@ namespace Sitko.Core.Storage
         public string FilePath { get; set; }
         public string Path { get; set; }
         public string StorageFileName => FilePath.Substring(FilePath.LastIndexOf('/') + 1);
-        private readonly string[] _units = {"bytes", "KB", "MB", "GB", "TB", "PB"};
-
+        
         public string HumanSize
         {
             get
             {
-                if (FileSize < 1)
-                {
-                    return "-";
-                }
-
-                var unit = 0;
-
-                double size = FileSize;
-                while (size >= 1024)
-                {
-                    size /= 1024;
-                    unit++;
-                }
-
-                return $"{Math.Round(size, 2):N}{_units[unit]}";
+                return Helpers.HumanSize(FileSize);
             }
         }
 
