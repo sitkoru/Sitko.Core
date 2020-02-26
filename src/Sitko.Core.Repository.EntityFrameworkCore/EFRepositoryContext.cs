ff7f39acab088d52644d52b83dc70538052d8e30
George <sonicgd@gmail.com>
2020-02-26 18:19:38 +0500

    Remove scope management from EF repositories

--- src/Sitko.Core.Repository.EntityFrameworkCore/EFRepositoryContext.cs
+++ src/Sitko.Core.Repository.EntityFrameworkCore/EFRepositoryContext.cs
@@ -1,30 +1,24 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Sitko.Core.Repository.EntityFrameworkCore
 {
-    public class EFRepositoryContext<TEntity, TEntityPk, TDbContext> : IRepositoryContext<TEntity, TEntityPk>, IDisposable
-        where TEntity : class, IEntity<TEntityPk>
-        where TDbContext : DbContext
+    public class EFRepositoryContext<TEntity, TEntityPk, TDbContext> : IRepositoryContext<TEntity, TEntityPk>
+        where TEntity : class, IEntity<TEntityPk> where TDbContext : DbContext
     {
         private readonly ILoggerFactory _loggerFactory;
-        private readonly IServiceScope _scope;
 
-        public EFRepositoryContext(IServiceScopeFactory serviceScopeFactory, ILoggerFactory loggerFactory)
+        public EFRepositoryContext(TDbContext dbContext, RepositoryFiltersManager filtersManager,
+            ILoggerFactory loggerFactory, List<IValidator<TEntity>>? validators = null,
+            List<IAccessChecker<TEntity, TEntityPk>>? accessCheckers = null)
         {
-            _scope = serviceScopeFactory.CreateScope();
             _loggerFactory = loggerFactory;
-            DbContext = _scope.ServiceProvider.GetRequiredService<TDbContext>();
-            FiltersManager = _scope.ServiceProvider.GetRequiredService<RepositoryFiltersManager>();
-            Validators = _scope.ServiceProvider.GetServices<IValidator<TEntity>>()?.ToList() ??
-                         new List<IValidator<TEntity>>();
-            AccessCheckers = _scope.ServiceProvider.GetServices<IAccessChecker<TEntity, TEntityPk>>()?.ToList() ??
-                             new List<IAccessChecker<TEntity, TEntityPk>>();
+            DbContext = dbContext;
+            FiltersManager = filtersManager;
+            Validators = validators;
+            AccessCheckers = accessCheckers;
         }
 
         internal TDbContext DbContext { get; }
@@ -35,10 +29,5 @@ namespace Sitko.Core.Repository.EntityFrameworkCore
         public RepositoryFiltersManager FiltersManager { get; }
         public List<IValidator<TEntity>> Validators { get; }
         public List<IAccessChecker<TEntity, TEntityPk>> AccessCheckers { get; }
-
-        public void Dispose()
-        {
-            _scope.Dispose();
-        }
     }
 }
