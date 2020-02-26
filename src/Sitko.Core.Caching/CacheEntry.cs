02aefad531a88cd8cb5fd2d6eec4122f0889762b
George <sonicgd@gmail.com>
2020-02-26 17:48:11 +0500

    Suppress warnings

--- src/Sitko.Core.Caching/CacheEntry.cs
+++ src/Sitko.Core.Caching/CacheEntry.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Primitives;
+// ReSharper disable All
 
 namespace Sitko.Core.Caching
 {
