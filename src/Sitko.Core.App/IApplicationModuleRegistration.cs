925b4e7a0b874d6ef31c2f1458ecd493bd8b9bf2
George <sonicgd@gmail.com>
2020-03-11 17:13:30 +0500

    Refactor registration classes

---
+++ src/Sitko.Core.App/IApplicationModuleRegistration.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Sitko.Core.App
+{
+    public interface IApplicationModuleRegistration
+    {
+        IApplicationModule CreateModule(IHostEnvironment environment, IConfiguration configuration,
+            Application application);
+    }
+}
