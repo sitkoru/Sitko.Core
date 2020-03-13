091272433ac9c5bb1fcfa5017725e0b6c80d09e9
George <sonicgd@gmail.com>
2020-03-13 17:11:03 +0500

    Check if apm agent is configured

--- src/Sitko.Core.Queue.Apm/QueueElasticApmMiddleware.cs
+++ src/Sitko.Core.Queue.Apm/QueueElasticApmMiddleware.cs
@@ -7,37 +7,54 @@ namespace Sitko.Core.Queue.Apm
     {
         private ITracer? _tracer;
 
-        private ITracer GetTracer()
+        private ITracer? GetTracer()
         {
-            return _tracer ??= Elastic.Apm.Agent.Tracer;
+            if (Elastic.Apm.Agent.IsConfigured)
+            {
+                return _tracer ??= Elastic.Apm.Agent.Tracer;    
+            }
+
+            return null;
         }
 
         public override Task<QueuePublishResult> PublishAsync<T>(T message, QueueMessageContext messageContext,
             PublishAsyncDelegate<T>? callback = null)
         {
-            var transaction = GetTracer().CurrentTransaction;
-            if (transaction == null)
+            var tracer = GetTracer();
+            if (tracer != null)
             {
-                return GetTracer().CaptureTransaction($"Publish {message.GetType().FullName}",
-                    ApiConstants.TypeExternal, () => base.PublishAsync(message, messageContext, callback));
+                var transaction = tracer.CurrentTransaction;
+                if (transaction == null)
+                {
+                    return tracer.CaptureTransaction($"Publish {message.GetType().FullName}",
+                        ApiConstants.TypeExternal, () => base.PublishAsync(message, messageContext, callback));
+                }
+
+                return transaction.CaptureSpan($"Publish {message.GetType().FullName}", ApiConstants.TypeExternal,
+                    () => base.PublishAsync(message, messageContext, callback), "Queue");
             }
 
-            return transaction.CaptureSpan($"Publish {message.GetType().FullName}", ApiConstants.TypeExternal,
-                () => base.PublishAsync(message, messageContext, callback), "Queue");
+            return base.PublishAsync(message, messageContext, callback);
         }
 
         public override Task<bool> ReceiveAsync<T>(T message, QueueMessageContext messageContext,
             ReceiveAsyncDelegate<T>? callback = null)
         {
-            var transaction = GetTracer().CurrentTransaction;
-            if (transaction == null)
+            var tracer = GetTracer();
+            if (tracer != null)
             {
-                return GetTracer().CaptureTransaction($"Process {message.GetType().FullName}",
-                    ApiConstants.TypeRequest, () => base.ReceiveAsync(message, messageContext, callback));
+                var transaction = tracer.CurrentTransaction;
+                if (transaction == null)
+                {
+                    return tracer.CaptureTransaction($"Process {message.GetType().FullName}",
+                        ApiConstants.TypeRequest, () => base.ReceiveAsync(message, messageContext, callback));
+                }
+
+                return transaction.CaptureSpan($"Process {message.GetType().FullName}", ApiConstants.TypeExternal,
+                    () => base.ReceiveAsync(message, messageContext, callback), "Queue");
             }
 
-            return transaction.CaptureSpan($"Process {message.GetType().FullName}", ApiConstants.TypeExternal,
-                () => base.ReceiveAsync(message, messageContext, callback), "Queue");
+            return base.ReceiveAsync(message, messageContext, callback);
         }
     }
 }
