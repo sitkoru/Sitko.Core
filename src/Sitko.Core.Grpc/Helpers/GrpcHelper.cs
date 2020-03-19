8ece46fe6e6bcecb6281b2f12ce31dc66c62cc9c
George <sonicgd@gmail.com>
2020-03-19 14:55:22 +0500

    Helper for generic grpc method exception logging

--- src/Sitko.Core.Grpc/Helpers/GrpcHelper.cs
+++ src/Sitko.Core.Grpc/Helpers/GrpcHelper.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Logging;
 
 namespace Sitko.Core.Grpc.Helpers
 {
@@ -54,6 +56,14 @@ namespace Sitko.Core.Grpc.Helpers
         {
             responseInfo.IsSuccess = false;
             responseInfo.Error = new ApiResponseError {Code = code, Errors = {ex.ToString()}};
+        }
+
+        public static void SetException(this ApiResponseInfo responseInfo, Exception ex, ILogger logger, object request,
+            int code = 500, [CallerMemberName] string? methodName = null)
+        {
+            responseInfo.SetException(ex, code);
+            logger.LogError(ex, "Error in method {MethodName}. Request: {@Request}. Error: {ErrorText}", methodName,
+                request, ex.ToString());
         }
     }
 }
