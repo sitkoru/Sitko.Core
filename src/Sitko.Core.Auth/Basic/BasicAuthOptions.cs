511d380aedae9e86c407ec490508423b15e33a5a
George <sonicgd@gmail.com>
2020-03-19 17:42:49 +0500

    Add setter to Realm

--- src/Sitko.Core.Auth/Basic/BasicAuthOptions.cs
+++ src/Sitko.Core.Auth/Basic/BasicAuthOptions.cs
@@ -2,7 +2,7 @@ namespace Sitko.Core.Auth.Basic
 {
     public class BasicAuthOptions : AuthOptions
     {
-        public string Realm { get; } = "Basic Auth";
+        public string Realm { get; set; } = "Basic Auth";
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
