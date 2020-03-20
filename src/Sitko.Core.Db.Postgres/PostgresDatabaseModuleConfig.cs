5187a98b7df33e8c90b813de6768949340afe9f1
George <sonicgd@gmail.com>
2020-03-20 15:45:51 +0500

    Fill default postgres username

--- src/Sitko.Core.Db.Postgres/PostgresDatabaseModuleConfig.cs
+++ src/Sitko.Core.Db.Postgres/PostgresDatabaseModuleConfig.cs
@@ -7,7 +7,7 @@ namespace Sitko.Core.Db.Postgres
     {
         public string Host { get; set; } = "localhost";
         public int Port { get; set; } = 5432;
-        public string Username { get; set; } = string.Empty;
+        public string Username { get; set; } = "postgres";
         public string Password { get; set; } = string.Empty;
         public bool EnableNpgsqlPooling { get; set; } = true;
         public bool EnableContextPooling { get; set; } = true;
