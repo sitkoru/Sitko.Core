6f648df440158110e4ab4015bc237c3fb1b5f4d3
George <sonicgd@gmail.com>
2020-03-20 15:12:32 +0500

    Cleanup

--- src/Sitko.Core.App.Web/WebApplication.cs
+++ src/Sitko.Core.App.Web/WebApplication.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Sitko.Core.App;
 
 namespace Sitko.Core.App.Web
 {
