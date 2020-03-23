46dc55b4171f09104fb2cf7f149ec702dc548b84
George <sonicgd@gmail.com>
2020-03-23 14:20:06 +0500

    drop ip address property from grpc server, use only host

--- src/Sitko.Core.Grpc.Server/GrpcServerOptions.cs
+++ src/Sitko.Core.Grpc.Server/GrpcServerOptions.cs
@@ -4,9 +4,7 @@ namespace Sitko.Core.Grpc.Server
 {
     public class GrpcServerOptions
     {
-        public string? IpAddress { get; set; }
         public string Version { get; set; } = "dev";
-
         public string? Host { get; set; }
         public TimeSpan ChecksInterval { get; set; } = TimeSpan.FromSeconds(60);
         public TimeSpan DeregisterTimeout { get; set; } = TimeSpan.FromSeconds(60);
