f31523a12ed55d1e89540462b9181419700b6a4d
George <sonicgd@gmail.com>
2020-03-23 16:12:15 +0500

    Cleanup

--- src/Sitko.Core.App.Web/IWebApplicationModule.cs
+++ src/Sitko.Core.App.Web/IWebApplicationModule.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sitko.Core.App;
 
 namespace Sitko.Core.App.Web
 {
