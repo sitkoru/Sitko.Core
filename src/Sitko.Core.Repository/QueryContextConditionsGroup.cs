2300ab43de747d9cb01220ca111c65a35a0b4ce8
George <sonicgd@gmail.com>
2020-03-24 18:04:55 +0500

    Fix repository filter deserialization

--- src/Sitko.Core.Repository/QueryContextConditionsGroup.cs
+++ src/Sitko.Core.Repository/QueryContextConditionsGroup.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Sitko.Core.Repository
 {
     public class QueryContextConditionsGroup
     {
+        [JsonConstructor]
         public QueryContextConditionsGroup(List<QueryContextCondition> conditions)
         {
             Conditions = conditions;
