2eb573907ef63324b6f7a415d403dc745f8ea063
George <sonicgd@gmail.com>
2020-03-26 12:12:02 +0500

    Add Consul web registration health check

--- src/Sitko.Core.Consul.Web/ConsulWebModule.cs
+++ src/Sitko.Core.Consul.Web/ConsulWebModule.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Consul;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Sitko.Core.App;
@@ -22,6 +25,13 @@ namespace Sitko.Core.Consul.Web
         public override List<Type> GetRequiredModules()
         {
             return new List<Type> {typeof(ConsulModule)};
+        }
+
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            base.ConfigureServices(services, configuration, environment);
+            services.AddHealthChecks().AddCheck<ConsulWebHealthCheck>("Consul registration");
         }
 
         public override async Task ApplicationStarted(IConfiguration configuration, IHostEnvironment environment,
@@ -80,6 +90,36 @@ namespace Sitko.Core.Consul.Web
             var logger = serviceProvider.GetRequiredService<ILogger<ConsulWebModule>>();
             logger.LogInformation("Remove service from Consul");
             await consulClient.Agent.ServiceDeregister(environment.ApplicationName);
+        }
+    }
+
+    public class ConsulWebHealthCheck : IHealthCheck
+    {
+        private readonly IConsulClient _consulClient;
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public ConsulWebHealthCheck(IConsulClient consulClient, IHostEnvironment hostEnvironment)
+        {
+            _consulClient = consulClient;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            var serviceName = _hostEnvironment.ApplicationName;
+            var serviceResponse = await _consulClient.Catalog.Service(serviceName, "metrics", cancellationToken);
+            if (serviceResponse.StatusCode == HttpStatusCode.OK)
+            {
+                if (serviceResponse.Response.Any())
+                {
+                    return HealthCheckResult.Healthy();
+                }
+
+                return HealthCheckResult.Unhealthy($"No service registered with name {serviceName}");
+            }
+
+            return HealthCheckResult.Unhealthy($"Error response from consul: {serviceResponse.StatusCode}");
         }
     }
 
