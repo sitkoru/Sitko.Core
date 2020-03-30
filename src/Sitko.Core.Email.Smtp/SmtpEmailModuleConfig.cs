d0f595ba1c3cfb260b9c4be7db5a43204f96519b
George Drak <sonicgd@gmail.com>
2020-03-30 14:48:15 +0500

    Fix

--- src/Sitko.Core.Email.Smtp/SmtpEmailModuleConfig.cs
+++ src/Sitko.Core.Email.Smtp/SmtpEmailModuleConfig.cs
@@ -6,8 +6,8 @@ namespace Sitko.Core.Email.Smtp
     {
         public string Server { get; set; } = "localhost";
         public int Port { get; set; } = 25;
-        public string UserName { get; } = string.Empty;
-        public string Password { get; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public SecureSocketOptions SocketOptions { get; set; } = SecureSocketOptions.Auto;
     }
 }
