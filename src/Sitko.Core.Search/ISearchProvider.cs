a176706589f0d48a27c6e851e4b6738e0ebc73e0
George Drak <sonicgd@gmail.com>
2020-06-04 13:02:56 +0500

    Add method to get only entity ids from search index.

--- src/Sitko.Core.Search/ISearchProvider.cs
+++ src/Sitko.Core.Search/ISearchProvider.cs
@@ -15,6 +15,7 @@ namespace Sitko.Core.Search
     public interface ISearchProvider<T> : ISearchProvider where T : class
     {
         Task<T[]> SearchAsync(string term, int limit, CancellationToken cancellationToken = default);
+        Task<string[]> SearchIdsAsync(string term, int limit, CancellationToken cancellationToken = default);
         Task<T[]> GetSimilarAsync(string id, int limit, CancellationToken cancellationToken = default);
         Task AddOrUpdateEntityAsync(T entity, CancellationToken cancellationToken = default);
         Task<bool> AddOrUpdateEntitiesAsync(T[] entities, CancellationToken cancellationToken = default);
