642fc5aa829db176340ce950e1812ad7ed8ecc50
George Drak <sonicgd@gmail.com>
2020-06-11 12:48:00 +0500

    Add more checks

--- src/Sitko.Core.HangFire/Components/UseQueueFromScheduledFilter.cs
+++ src/Sitko.Core.HangFire/Components/UseQueueFromScheduledFilter.cs
@@ -3,19 +3,20 @@ using Hangfire.States;
 
 namespace Sitko.Core.HangFire.Components
 {
-    // // https://github.com/HangfireIO/Hangfire/issues/748
+    // https://github.com/HangfireIO/Hangfire/issues/748
     public class UseQueueFromScheduledFilter : IJobFilter, IElectStateFilter
     {
         public void OnStateElection(ElectStateContext context)
         {
-            var enqueuedState = context.CandidateState as EnqueuedState;
-
-            if (enqueuedState != null)
+            if (context.CandidateState is EnqueuedState enqueuedState && context.BackgroundJob != null)
             {
                 var stateData = context.Connection.GetStateData(context.BackgroundJob.Id);
-                if (stateData.Data.TryGetValue("Queue", out var queueName))
+                if (stateData != null)
                 {
-                    enqueuedState.Queue = queueName;
+                    if (stateData.Data.TryGetValue("Queue", out var queueName))
+                    {
+                        enqueuedState.Queue = queueName;
+                    }
                 }
             }
         }
