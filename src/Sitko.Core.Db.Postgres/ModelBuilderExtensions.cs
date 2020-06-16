f96340e8a5b52f02993fe17be0e87a17236234e0
George Drak <sonicgd@gmail.com>
2020-06-16 17:12:42 +0500

    Add helper to register entities with json properties for correct changes tracking

---
+++ src/Sitko.Core.Db.Postgres/ModelBuilderExtensions.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+
+namespace Sitko.Core.Db.Postgres
+{
+    public static class ModelBuilderExtensions
+    {
+        private static JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.Auto,
+            MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead
+        };
+
+        private static string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, _jsonSettings);
+        }
+
+        private static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, _jsonSettings);
+        }
+
+
+        public static void RegisterJsonConversion<TEntity, TData>(this ModelBuilder modelBuilder,
+            Expression<Func<TEntity, TData>> getProperty, string name)
+            where TEntity : class where TData : new()
+        {
+            var valueComparer = new ValueComparer<TData>(
+                (c1, c2) => c1.Equals(c2),
+                c => c.GetHashCode(),
+                c => Deserialize<TData>(Serialize(c)));
+            modelBuilder
+                .Entity<TEntity>()
+                .Property(getProperty)
+                .HasColumnType("jsonb")
+                .HasColumnName(name)
+                .HasConversion(blocks => Serialize(blocks),
+                    json => Deserialize<TData>(json) ?? new TData())
+                .Metadata.SetValueComparer(valueComparer);
+        }
+
+        public static void RegisterJsonCollectionConversion<TEntity, TData>(this ModelBuilder modelBuilder,
+            Expression<Func<TEntity, ICollection<TData>>> getProperty, string name)
+            where TEntity : class
+        {
+            var valueComparer = new ValueComparer<ICollection<TData>>(
+                (c1, c2) => c1.SequenceEqual(c2),
+                c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                c => Deserialize<ICollection<TData>>(Serialize(c)));
+            modelBuilder
+                .Entity<TEntity>()
+                .Property(getProperty)
+                .HasColumnType("jsonb")
+                .HasColumnName(name)
+                .HasConversion(blocks => Serialize(blocks),
+                    json => Deserialize<ICollection<TData>>(json) ?? new List<TData>())
+                .Metadata.SetValueComparer(valueComparer);
+        }
+    }
+}
