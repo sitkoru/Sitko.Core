f16d33f3b4f119fc3d89a4e8e82a75be7285d9f1
George Drak (sonicgd) <sonicgd@gmail.com>
2020-09-24 16:28:37 +0500

    cleanup

--- src/Sitko.Core.Grpc.Server.Consul/GrpcServerConsulModule.cs
+++ src/Sitko.Core.Grpc.Server.Consul/GrpcServerConsulModule.cs
@@ -1,6 +1,3 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Sitko.Core.App;
 using Sitko.Core.Grpc.Server.Discovery;
 
