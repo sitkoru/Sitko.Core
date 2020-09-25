3ff53adf788f1177760c1130ad0474c5ea759faf
George Drak (sonicgd) <sonicgd@gmail.com>
2020-09-25 15:07:02 +0500

    Method to add external grpc client with address from configuration

--- src/Sitko.Core.Grpc.Client/External/ExternalGrpcClientExtensions.cs
+++ src/Sitko.Core.Grpc.Client/External/ExternalGrpcClientExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using Grpc.Core;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Sitko.Core.App;
 
 namespace Sitko.Core.Grpc.Client.External
@@ -8,14 +10,16 @@ namespace Sitko.Core.Grpc.Client.External
     [PublicAPI]
     public static class ExternalGrpcClientExtensions
     {
-        public static TApplication AddExternalGrpcClient<TApplication, TClient>(this TApplication application, string address,
+        public static TApplication AddExternalGrpcClient<TApplication, TClient>(this TApplication application,
+            string address,
             Action<GrpcClientModuleConfig>? configure = null)
             where TApplication : Application<TApplication> where TClient : ClientBase<TClient>
         {
             return application.AddExternalGrpcClient<TApplication, TClient>(new Uri(address), configure);
         }
 
-        public static TApplication AddExternalGrpcClient<TApplication, TClient>(this TApplication application, Uri address,
+        public static TApplication AddExternalGrpcClient<TApplication, TClient>(this TApplication application,
+            Uri address,
             Action<GrpcClientModuleConfig>? configure = null)
             where TApplication : Application<TApplication> where TClient : ClientBase<TClient>
         {
@@ -23,6 +27,20 @@ namespace Sitko.Core.Grpc.Client.External
                 environment, moduleConfig) =>
             {
                 moduleConfig.Address = address;
+                configure?.Invoke(moduleConfig);
+            });
+            return application;
+        }
+
+        public static TApplication AddExternalGrpcClient<TApplication, TClient>(this TApplication application,
+            Func<IConfiguration, IHostEnvironment, Uri> getAddress,
+            Action<GrpcClientModuleConfig>? configure = null)
+            where TApplication : Application<TApplication> where TClient : ClientBase<TClient>
+        {
+            application.AddModule<ExternalGrpcClientModule<TClient>, GrpcClientStaticModuleConfig>((configuration,
+                environment, moduleConfig) =>
+            {
+                moduleConfig.Address = getAddress(configuration, environment);
                 configure?.Invoke(moduleConfig);
             });
             return application;
