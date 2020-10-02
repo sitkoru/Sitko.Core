6130f818834403514f695e54d726f3c5a3a3b5d8
George Drak (sonicgd) <sonicgd@gmail.com>
2020-10-02 15:28:44 +0500

    #72 add pdf renderer module

---
+++ src/Sitko.Core.Pdf/PdfRenderer.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+
+namespace Sitko.Core.Pdf
+{
+    [UsedImplicitly]
+    internal class PdfRenderer : IPdfRenderer
+    {
+        private readonly PdfRendererModuleConfig _config;
+        private readonly ILogger<PdfRenderer> _logger;
+
+        public PdfRenderer(PdfRendererModuleConfig config, ILogger<PdfRenderer> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public async Task<byte[]> GetPdfByUrlAsync(string url, PdfOptions? options = null)
+        {
+            try
+            {
+                await using var browser = await GetBrowserAsync();
+                var page = await browser.NewPageAsync();
+                await page.GoToAsync(url);
+                options ??= GetDefaultOptions();
+                var pdf = await page.PdfDataAsync(options);
+                return pdf;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while generating pdf from url: {errorText}", ex.ToString());
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public PdfOptions GetDefaultOptions()
+        {
+            return new PdfOptions {PrintBackground = true};
+        }
+
+        public async Task<byte[]> GetPdfByHtmlAsync(string html, PdfOptions? options = null)
+        {
+            try
+            {
+                await using var browser = await GetBrowserAsync();
+                await using var page = await browser.NewPageAsync();
+                await page.SetContentAsync(html);
+                options ??= GetDefaultOptions();
+                var pdf = await page.PdfDataAsync(options);
+                return pdf;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while generating pdf from html: {errorText}", ex.ToString());
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        private async Task<Browser> GetBrowserAsync()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            return await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true, Args = new[] {"--no-sandbox"}, IgnoreHTTPSErrors = _config.IgnoreHTTPSErrors
+            });
+        }
+    }
+}
---
+++ tests/Sitko.Core.Pdf.Tests/BasePdfTest.cs
@@ -0,0 +1,12 @@
+using Sitko.Core.Xunit;
+using Xunit.Abstractions;
+
+namespace Sitko.Core.Pdf.Tests
+{
+    public class BasePdfTest : BaseTest<PdfTestScope>
+    {
+        public BasePdfTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+    }
+}
