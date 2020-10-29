4f923b486759d9a68c110fb7b9da652ac6277b3a
George Drak (sonicgd) <sonicgd@gmail.com>
2020-10-29 17:45:15 +0500

    Switch to ttl checks for grpc, remove unused options

--- src/Sitko.Core.Grpc.Server/GrpcServerOptions.cs
+++ src/Sitko.Core.Grpc.Server/GrpcServerOptions.cs
@@ -9,8 +9,6 @@ namespace Sitko.Core.Grpc.Server
         public string? Host { get; set; }
         public int? Port { get; set; }
 
-        public bool UseTls { get; set; } = true;
-        public bool ValidateTls { get; set; } = true;
         public TimeSpan ChecksInterval { get; set; } = TimeSpan.FromSeconds(60);
         public TimeSpan DeregisterTimeout { get; set; } = TimeSpan.FromSeconds(60);
 
