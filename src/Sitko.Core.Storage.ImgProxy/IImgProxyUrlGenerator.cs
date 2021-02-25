d84e0c991521c621fe7cca36eaf69dbbeb4eac8a
George Drak <sonicgd@gmail.com>
2021-02-25 18:11:02 +0500

    Add storage imgproxy module

---
+++ src/Sitko.Core.Storage.ImgProxy/IImgProxyUrlGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitko.Core.Storage.ImgProxy
+{
+    public interface IImgProxyUrlGenerator<TStorageOptions> where TStorageOptions : StorageOptions
+    {
+        /// <summary>
+        /// Generate url for resized image
+        /// </summary>
+        /// <param name="item">StorageItem image to resize</param>
+        /// <param name="width">Resize width to</param>
+        /// <param name="height">Resize height to</param>
+        /// <param name="type">Resize type - auto, fit, fill. Default is auto</param>
+        /// <param name="enlarge">Allow to enlarge image. Default is false</param>
+        /// <returns></returns>
+        string Resize(StorageItem item, int width, int height, string type = "auto", bool enlarge = false);
+    }
+}
