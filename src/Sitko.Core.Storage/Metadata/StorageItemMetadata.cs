8093a6bb567db7c600ca624f35808297eca876ad
George Drak <sonicgd@gmail.com>
2021-02-25 18:11:17 +0500

    Add missing file

---
+++ src/Sitko.Core.Storage/Metadata/StorageItemMetadata.cs
@@ -0,0 +1,21 @@
+using System.Text.Json;
+
+namespace Sitko.Core.Storage.Metadata
+{
+    public class StorageItemMetadata
+    {
+        public string? FileName { get; set; }
+
+        public string? Data { get; set; }
+
+        public void SetData(object data)
+        {
+            Data = JsonSerializer.Serialize(data);
+        }
+
+        public TData? GetData<TData>() where TData : class
+        {
+            return string.IsNullOrEmpty(Data) ? null : JsonSerializer.Deserialize<TData>(Data);
+        }
+    }
+}
