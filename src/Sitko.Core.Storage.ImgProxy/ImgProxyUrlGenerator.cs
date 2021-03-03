d33b16a5ed39bcee7a01e57a3552d0ecc0b4d00f
George Drak <sonicgd@gmail.com>
2021-03-03 10:56:34 +0500

    Start imgproxy url generator

---
+++ src/Sitko.Core.Storage.ImgProxy/ImgProxyUrlGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ImgProxy;
+using Microsoft.Extensions.Logging;
+
+namespace Sitko.Core.Storage.ImgProxy
+{
+    public class ImgProxyUrlGenerator<TStorageOptions> : IImgProxyUrlGenerator<TStorageOptions>
+        where TStorageOptions : StorageOptions
+    {
+        private readonly IStorage<TStorageOptions> _storage;
+        private readonly StorageImgProxyModuleConfig<TStorageOptions> _options;
+        private readonly ILogger<ImgProxyUrlGenerator<TStorageOptions>> _logger;
+        private readonly ImgProxyBuilder _imgProxyBuilder;
+
+        public ImgProxyUrlGenerator(IStorage<TStorageOptions> storage,
+            StorageImgProxyModuleConfig<TStorageOptions> options,
+            ILogger<ImgProxyUrlGenerator<TStorageOptions>> logger)
+        {
+            _storage = storage;
+            _options = options;
+            _logger = logger;
+            _imgProxyBuilder = ImgProxyBuilder.New.WithEndpoint(options.Host)
+                .WithCredentials(options.Key, options.Salt);
+        }
+
+        public string Resize(StorageItem item, int width, int height, string type = "auto", bool enlarge = false)
+        {
+            return _imgProxyBuilder.WithResize(type, width, height, enlarge)
+                .Build(_storage.PublicUri(item).ToString(), _options.EncodeUrls);
+        }
+    }
+}
---
+++ src/Sitko.Core.Storage.ImgProxy/StorageImgProxyModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Storage.ImgProxy
+{
+    public class
+        StorageImgProxyModule<TStorageOptions> : BaseApplicationModule<StorageImgProxyModuleConfig<TStorageOptions>>
+        where TStorageOptions : StorageOptions
+    {
+        public StorageImgProxyModule(StorageImgProxyModuleConfig<TStorageOptions> config, Application application) :
+            base(config, application)
+        {
+        }
+
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            base.ConfigureServices(services, configuration, environment);
+            services.AddSingleton<IImgProxyUrlGenerator<TStorageOptions>, ImgProxyUrlGenerator<TStorageOptions>>();
+        }
+
+        public override List<Type> GetRequiredModules()
+        {
+            return new() {typeof(IStorageModule)};
+        }
+    }
+
+    public class StorageImgProxyModuleConfig<TStorageOptions> where TStorageOptions : StorageOptions
+    {
+        public string Host { get; set; }
+        public string Key { get; set; }
+        public string Salt { get; set; }
+        public bool EncodeUrls { get; set; } = false;
+    }
+}
