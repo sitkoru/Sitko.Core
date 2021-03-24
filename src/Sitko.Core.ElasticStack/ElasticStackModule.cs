2ba07532d42596acb6f72ff7fcf926fb99248485
George Drak <sonicgd@gmail.com>
2021-03-24 13:27:44 +0500

    Switch Elastic APM to host builder configuration

--- src/Sitko.Core.ElasticStack/ElasticStackModule.cs
+++ src/Sitko.Core.ElasticStack/ElasticStackModule.cs
@@ -1,27 +1,91 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using Elastic.Apm.NetCoreAll;
 using Elastic.Apm.SerilogEnricher;
 using Elastic.CommonSchema.Serilog;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.Elasticsearch;
 using Sitko.Core.App;
 using Sitko.Core.App.Logging;
-using Sitko.Core.App.Web;
 
 namespace Sitko.Core.ElasticStack
 {
-    public class ElasticStackModule : BaseApplicationModule<ElasticStackModuleConfig>, IWebApplicationModule
+    public class ElasticStackModule : BaseApplicationModule<ElasticStackModuleConfig>,
+        IHostBuilderModule<ElasticStackModuleConfig>
     {
         public ElasticStackModule(ElasticStackModuleConfig config, Application application) : base(config, application)
         {
+        }
+
+        public void ConfigureHostBuilder(IHostBuilder hostBuilder, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            if (Config.ApmEnabled)
+            {
+                Environment.SetEnvironmentVariable("ELASTIC_APM_SERVICE_NAME", Application.Name);
+                Environment.SetEnvironmentVariable("ELASTIC_APM_SERVICE_VERSION", Application.Version);
+                Environment.SetEnvironmentVariable("ELASTIC_APM_TRANSACTION_SAMPLE_RATE",
+                    Config.ApmTransactionSampleRate.ToString(CultureInfo.InvariantCulture));
+                Environment.SetEnvironmentVariable("ElasticApm:TransactionMaxSpans",
+                    Config.ApmTransactionMaxSpans.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:CentralConfig", Config.ApmCentralConfig.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:SanitizeFieldNames", Config.ApmCentralConfig.ToString());
+                if (Config.ApmSanitizeFieldNames != null && Config.ApmSanitizeFieldNames.Any())
+                {
+                    Environment.SetEnvironmentVariable("ElasticApm:SanitizeFieldNames",
+                        string.Join(", ", Config.ApmSanitizeFieldNames));
+                }
+
+                if (Config.ApmGlobalLabels.Any())
+                {
+                    Environment.SetEnvironmentVariable("ElasticApm:GlobalLabels",
+                        string.Join(",", Config.ApmGlobalLabels.Select(kv => $"{kv.Key}={kv.Value}")));
+                }
+
+                if (Config.ApmServerUrls != null && Config.ApmServerUrls.Any())
+                {
+                    Environment.SetEnvironmentVariable("ElasticApm:ServerUrls", string.Join(",", Config.ApmServerUrls));
+                }
+
+                Environment.SetEnvironmentVariable("ElasticApm:SecretToken", Config.ApmSecretToken);
+                Environment.SetEnvironmentVariable("ElasticApm:ApiKey", Config.ApmApiKey);
+                Environment.SetEnvironmentVariable("ElasticApm:VerifyServerCert",
+                    Config.ApmVerifyServerCert.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:FlushInterval",
+                    $"{Config.ApmFlushInterval.TotalSeconds}s");
+                Environment.SetEnvironmentVariable("ElasticApm:MaxBatchEventCount",
+                    Config.ApmMaxBatchEventCount.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:MaxQueueEventCount",
+                    Config.ApmMaxQueueEventCount.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:MetricsInterval",
+                    $"{Config.ApmMetricsInterval.TotalSeconds}s");
+                if (Config.ApmDisableMetrics != null && Config.ApmDisableMetrics.Any())
+                {
+                    Environment.SetEnvironmentVariable("ElasticApm:DisableMetrics",
+                        string.Join(",", Config.ApmDisableMetrics));
+                }
+
+                Environment.SetEnvironmentVariable("ElasticApm:CaptureBody", Config.ApmCaptureBody);
+                if (Config.ApmCaptureBodyContentTypes != null && Config.ApmCaptureBodyContentTypes.Any())
+                {
+                    Environment.SetEnvironmentVariable("ElasticApm:CaptureBodyContentTypes",
+                        string.Join(",", Config.ApmCaptureBodyContentTypes));
+                }
+
+                Environment.SetEnvironmentVariable("ElasticApm:CaptureHeaders", Config.ApmCaptureHeaders.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:UseElasticTraceparentHeader",
+                    Config.ApmUseElasticTraceparentHeader.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:StackTraceLimit", Config.ApmStackTraceLimit.ToString());
+                Environment.SetEnvironmentVariable("ElasticApm:SpanFramesMinDuration",
+                    $"{Config.ApmSpanFramesMinDuration.TotalMilliseconds}ms");
+                Environment.SetEnvironmentVariable("ElasticApm:ApmLogLevel", Config.ApmLogLevel);
+                hostBuilder.UseAllElasticApm();
+            }
         }
 
         public override void ConfigureLogging(LoggerConfiguration loggerConfiguration,
@@ -68,64 +132,6 @@ namespace Sitko.Core.ElasticStack
             }
         }
 
-        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
-            IHostEnvironment environment)
-        {
-            base.ConfigureServices(services, configuration, environment);
-            if (Config != null && Config.ApmEnabled)
-            {
-                configuration["ElasticApm:ServiceName"] = Application.Name;
-                configuration["ElasticApm:ServiceVersion"] = Application.Version;
-                configuration["ElasticApm:TransactionSampleRate"] =
-                    Config.ApmTransactionSampleRate.ToString(CultureInfo.InvariantCulture);
-                configuration["ElasticApm:TransactionMaxSpans"] = Config.ApmTransactionMaxSpans.ToString();
-                configuration["ElasticApm:CentralConfig"] = Config.ApmCentralConfig.ToString();
-                configuration["ElasticApm:SanitizeFieldNames"] = Config.ApmCentralConfig.ToString();
-                if (Config.ApmSanitizeFieldNames != null && Config.ApmSanitizeFieldNames.Any())
-                {
-                    configuration["ElasticApm:SanitizeFieldNames"] = string.Join(", ", Config.ApmSanitizeFieldNames);
-                }
-
-                if (Config.ApmGlobalLabels.Any())
-                {
-                    configuration["ElasticApm:GlobalLabels"] =
-                        string.Join(",", Config.ApmGlobalLabels.Select(kv => $"{kv.Key}={kv.Value}"));
-                }
-
-                if (Config.ApmServerUrls != null && Config.ApmServerUrls.Any())
-                {
-                    configuration["ElasticApm:ServerUrls"] = string.Join(",", Config.ApmServerUrls);
-                }
-
-                configuration["ElasticApm:SecretToken"] = Config.ApmSecretToken;
-                configuration["ElasticApm:ApiKey"] = Config.ApmApiKey;
-                configuration["ElasticApm:VerifyServerCert"] = Config.ApmVerifyServerCert.ToString();
-                configuration["ElasticApm:FlushInterval"] = $"{Config.ApmFlushInterval.TotalSeconds}s";
-                configuration["ElasticApm:MaxBatchEventCount"] = Config.ApmMaxBatchEventCount.ToString();
-                configuration["ElasticApm:MaxQueueEventCount"] = Config.ApmMaxQueueEventCount.ToString();
-                configuration["ElasticApm:MetricsInterval"] = $"{Config.ApmMetricsInterval.TotalSeconds}s";
-                if (Config.ApmDisableMetrics != null && Config.ApmDisableMetrics.Any())
-                {
-                    configuration["ElasticApm:DisableMetrics"] = string.Join(",", Config.ApmDisableMetrics);
-                }
-
-                configuration["ElasticApm:CaptureBody"] = Config.ApmCaptureBody;
-                if (Config.ApmCaptureBodyContentTypes != null && Config.ApmCaptureBodyContentTypes.Any())
-                {
-                    configuration["ElasticApm:CaptureBodyContentTypes"] =
-                        string.Join(",", Config.ApmCaptureBodyContentTypes);
-                }
-
-                configuration["ElasticApm:CaptureHeaders"] = Config.ApmCaptureHeaders.ToString();
-                configuration["ElasticApm:UseElasticTraceparentHeader"] =
-                    Config.ApmUseElasticTraceparentHeader.ToString();
-                configuration["ElasticApm:StackTraceLimit"] = Config.ApmStackTraceLimit.ToString();
-                configuration["ElasticApm:SpanFramesMinDuration"] =
-                    $"{Config.ApmSpanFramesMinDuration.TotalMilliseconds}ms";
-                configuration["ElasticApm:ApmLogLevel"] = Config.ApmLogLevel;
-            }
-        }
-
         public override void CheckConfig()
         {
             base.CheckConfig();
@@ -143,15 +149,6 @@ namespace Sitko.Core.ElasticStack
                 {
                     throw new ArgumentException("ElasticSearchUrls can't be empty");
                 }
-            }
-        }
-
-        public void ConfigureAppBuilder(IConfiguration configuration, IHostEnvironment environment,
-            IApplicationBuilder appBuilder)
-        {
-            if (Config.ApmEnabled)
-            {
-                appBuilder.UseAllElasticApm(configuration);
             }
         }
     }
