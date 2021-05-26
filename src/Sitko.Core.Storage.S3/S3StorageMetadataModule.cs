c62c724184f12d1b4a498e02acf22e5bf06e70ab
George Drak <sonicgd@gmail.com>
2021-05-26 19:07:00 +0500

    #267 add missing module

---
+++ src/Sitko.Core.Storage.S3/S3StorageMetadataModule.cs
@@ -0,0 +1,19 @@
+﻿using Sitko.Core.App;
+using Sitko.Core.Storage.Metadata;
+
+namespace Sitko.Core.Storage.S3
+{
+    public class S3StorageMetadataModule<TStorageOptions> : BaseStorageMetadataModule<TStorageOptions,
+        S3StorageMetadataProvider<TStorageOptions>, S3StorageMetadataProviderOptions>
+        where TStorageOptions : S3StorageOptions, new()
+    {
+        public override string GetConfigKey()
+        {
+            return $"Storage:Metadata:FileSystem:{typeof(TStorageOptions).Name}";
+        }
+
+        public S3StorageMetadataModule(Application application) : base(application)
+        {
+        }
+    }
+}
