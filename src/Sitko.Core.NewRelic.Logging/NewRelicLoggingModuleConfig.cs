6bd45119051fd544bdaad3961796bf76e4772aca
George Drak <sonicgd@gmail.com>
2021-05-27 16:04:53 +0500

    #267 newrelic module

--- src/Sitko.Core.NewRelic.Logging/NewRelicLoggingModuleConfig.cs
+++ src/Sitko.Core.NewRelic.Logging/NewRelicLoggingModuleConfig.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+using Sitko.Core.App;
+
 namespace Sitko.Core.NewRelic.Logging
 {
-    public class NewRelicLoggingModuleConfig
+    public class NewRelicLoggingModuleConfig : BaseModuleConfig
     {
         public string LicenseKey { get; set; } = string.Empty;
         public bool EnableLogging { get; set; } = false;
         public string LogsUrl { get; set; } = "https://log-api.newrelic.com/log/v1";
+
+        public override (bool isSuccess, IEnumerable<string> errors) CheckConfig()
+        {
+            var result = base.CheckConfig();
+            if (result.isSuccess)
+            {
+                if (string.IsNullOrEmpty(LicenseKey))
+                {
+                    return (false, new[] {"Provide License Key for NewRelic"});
+                }
+            }
+
+            return result;
+        }
     }
 }
