4d7672f02b7ff7b068b9b5f0250f1d71009da300
George Drak <sonicgd@gmail.com>
2021-05-27 14:38:50 +0500

    #267 automapper module

--- src/Sitko.Core.Automapper/AutoMapperModule.cs
+++ src/Sitko.Core.Automapper/AutoMapperModule.cs
@@ -2,28 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using AutoMapper;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Sitko.Core.App;
 
 namespace Sitko.Core.Automapper
 {
     public class AutoMapperModule : BaseApplicationModule<AutoMapperModuleConfig>
     {
-        public AutoMapperModule(AutoMapperModuleConfig config, Application application) : base(config, application)
+        public override string GetConfigKey()
         {
+            return "AutoMapper";
         }
 
-        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
-            IHostEnvironment environment)
+        public override void ConfigureServices(ApplicationContext context, IServiceCollection services,
+            AutoMapperModuleConfig startupConfig)
         {
-            base.ConfigureServices(services, configuration, environment);
-            services.AddAutoMapper(Config.Configure, Config.Assemblies);
+            base.ConfigureServices(context, services, startupConfig);
+            services.AddAutoMapper(startupConfig.Configure, startupConfig.Assemblies);
         }
     }
 
-    public class AutoMapperModuleConfig
+    public class AutoMapperModuleConfig : BaseModuleConfig
     {
         public Action<IMapperConfigurationExpression> Configure { get; set; } = _ => { };
         public readonly List<Assembly> Assemblies = new() {typeof(AutoMapperModule).Assembly};
