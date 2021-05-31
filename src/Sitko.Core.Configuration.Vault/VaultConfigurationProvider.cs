5df04ae5b7751f75246203b3993903c233a5d77b
George Drak <sonicgd@gmail.com>
2021-05-31 11:37:09 +0500

    #267 rework vault configuration options, support config mapping

--- src/Sitko.Core.Configuration.Vault/VaultConfigurationProvider.cs
+++ src/Sitko.Core.Configuration.Vault/VaultConfigurationProvider.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using Sitko.Core.App;
 using VaultSharp.Extensions.Configuration;
 
@@ -8,21 +12,72 @@ namespace Sitko.Core.Configuration.Vault
 {
     public static class ApplicationExtensions
     {
+        private static bool _serviceRegistered;
+        private static bool _hasSecrets;
+        private static bool _isEnabled;
+
         public static Application AddVaultConfiguration(this Application application,
-            Func<VaultOptions> configureOptions, string path, string mountPoint)
+            Action<HostBuilderContext, VaultConfigurationOptions>? configureOptions = null,
+            Func<HostBuilderContext, bool>? isEnabled = null)
         {
             application.ConfigureAppConfiguration((context, builder) =>
             {
-                builder.AddVaultConfiguration(configureOptions,
-                    path,
-                    mountPoint);
+                if (isEnabled is not null && !isEnabled(context))
+                {
+                    return;
+                }
+
+                _isEnabled = true;
+
+                var options = new VaultConfigurationOptions();
+                context.Configuration.Bind("vault", options);
+                configureOptions?.Invoke(context, options);
+                if (!options.Secrets.Any())
+                {
+                    return;
+                }
+
+                _hasSecrets = true;
+                foreach (var secret in options.Secrets)
+                {
+                    builder.AddVaultConfiguration(options.GetOptions,
+                        secret,
+                        options.MountPoint);
+                }
             });
             application.ConfigureServices((context, services) =>
             {
-                services.AddSingleton((IConfigurationRoot)context.Configuration);
+                if (!_isEnabled || !_hasSecrets || _serviceRegistered)
+                {
+                    return;
+                }
+
+                services.TryAddSingleton((IConfigurationRoot)context.Configuration);
                 services.AddHostedService<VaultChangeWatcher>();
+
+                _serviceRegistered = true;
             });
             return application;
+        }
+    }
+
+    public class VaultConfigurationOptions
+    {
+        public List<string> Secrets { get; set; } = new();
+        public string Uri { get; set; }
+        public string Token { get; set; }
+        public string MountPoint { get; set; }
+        public string? VaultSecret { get; set; } = null;
+        public string? VaultRoleId { get; set; } = null;
+        public bool ReloadOnChange { get; set; } = true;
+        public int ReloadCheckIntervalSeconds { get; set; } = 60;
+        public bool OmitVaultKeyName { get; set; } = false;
+        public IEnumerable<char>? AdditionalCharactersForConfigurationPath { get; set; } = null;
+
+        public VaultOptions GetOptions()
+        {
+            return new(Uri, Token, VaultSecret, VaultRoleId, ReloadOnChange, ReloadCheckIntervalSeconds,
+                OmitVaultKeyName, AdditionalCharactersForConfigurationPath);
         }
     }
 }
