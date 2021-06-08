ba9dfcb1c3454e362cec3c699755f8e88201059c
George Drak <sonicgd@gmail.com>
2021-06-08 16:49:06 +0500

    Add base ant repository list component

--- src/Sitko.Core.Blazor.AntDesign/Components/AntListComponent.cs
+++ src/Sitko.Core.Blazor.AntDesign/Components/AntListComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AntDesign;
+using AntDesign.TableModels;
 using Sitko.Core.App.Blazor.Components;
 
 namespace Sitko.Core.Blazor.AntDesign.Components
@@ -11,6 +13,7 @@ namespace Sitko.Core.Blazor.AntDesign.Components
         protected int Count;
         protected Pagination _pagination;
         protected int PageSize { get; set; } = 50;
+        protected string DefaultSorting { get; set; } = "";
 
         protected override async Task OnInitializedAsync()
         {
@@ -19,15 +22,27 @@ namespace Sitko.Core.Blazor.AntDesign.Components
             MarkAsInitialized();
         }
 
-        protected async Task LoadDataAsync(int page = 1)
+        protected async Task OnChange(QueryModel<TItem> queryModel)
+        {
+            var orderBy = string.Join(",",
+                queryModel.SortModel
+                    .Where(s => s.Sort is not null)
+                    .OrderBy(s => s.Priority)
+                    .Select(s =>
+                        $"{(s.Sort == SortDirection.Descending.Name ? "-" : "")}{s.FieldName.ToLowerInvariant()}"));
+            await LoadDataAsync(string.IsNullOrEmpty(orderBy) ? DefaultSorting : orderBy,
+                queryModel.PageIndex);
+        }
+
+        protected async Task LoadDataAsync(string orderBy = "", int page = 1)
         {
             StartLoading();
-            (var items, int itemsCount) = await GetDataAsync();
+            (var items, int itemsCount) = await GetDataAsync(orderBy, page);
             Items = items;
             Count = itemsCount;
             StopLoading();
         }
 
-        protected abstract Task<(IEnumerable<TItem> items, int totalItemsCount)> GetDataAsync();
+        protected abstract Task<(TItem[] items, int itemsCount)> GetDataAsync(string orderBy, int page = 1);
     }
 }
