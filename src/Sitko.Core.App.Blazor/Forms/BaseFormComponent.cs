ec469ef3d3fa2a24e6b551b679ff3012b16e7fcd
George Drak <sonicgd@gmail.com>
2021-06-11 12:19:54 +0500

    Export Form from component, add OnSave callback

--- src/Sitko.Core.App.Blazor/Forms/BaseFormComponent.cs
+++ src/Sitko.Core.App.Blazor/Forms/BaseFormComponent.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Sitko.Core.App.Blazor.Components;
 
 namespace Sitko.Core.App.Blazor.Forms
@@ -6,14 +8,17 @@ namespace Sitko.Core.App.Blazor.Forms
     public abstract class BaseFormComponent<TEntity, TForm> : BaseComponent where TForm : BaseForm<TEntity>
         where TEntity : class, new()
     {
-        protected TForm Form { get; set; }
+        public TForm Form { get; private set; }
         protected abstract Task InitializeForm();
-        
+
+        [Parameter] public Func<TEntity, Task>? OnSave { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
             Form = GetService<TForm>();
             Form.SetParent(this);
+            Form.OnSave = entity => OnSave is not null ? OnSave(entity) : Task.CompletedTask;
             await ConfigureFormAsync();
             await InitializeForm();
             MarkAsInitialized();
