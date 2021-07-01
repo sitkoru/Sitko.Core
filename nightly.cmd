b97e3d939f5f9a7794a78529243d6dd1dda61852
George Drak <sonicgd@gmail.com>
2021-07-01 15:27:32 +0500

    Upd version

--- nightly.cmd
+++ nightly.cmd
@@ -2,4 +2,4 @@
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
 
-dotnet pack -c Release /p:Version=7.11.6-nightly.%ldt% -o %1
+dotnet pack -c Release /p:Version=8.0.0-nightly.%ldt% -o %1
