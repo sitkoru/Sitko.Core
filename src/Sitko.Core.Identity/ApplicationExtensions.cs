2d4d67cd6aad9e6ff2936e6a8a3792dcda4dd19a
George Drak <sonicgd@gmail.com>
2021-07-12 18:51:22 +0500

    Fix AddIdentity helpers

--- src/Sitko.Core.Identity/ApplicationExtensions.cs
+++ src/Sitko.Core.Identity/ApplicationExtensions.cs
@@ -10,14 +10,25 @@ namespace Sitko.Core.Identity
     public static class ApplicationExtensions
     {
         public static Application AddIdentity<TUser, TRole, TPk, TDbContext>(this Application application,
-            Action<IConfiguration, IHostEnvironment, IdentityModuleOptions>? configure = null)
+            Action<IConfiguration, IHostEnvironment, IdentityModuleOptions> configure, string? optionsKey = null)
             where TUser : IdentityUser<TPk>
             where TRole : IdentityRole<TPk>
             where TPk : IEquatable<TPk>
             where TDbContext : IdentityDbContext<TUser, TRole, TPk>
         {
             return application.AddModule<IdentityModule<TUser, TRole, TPk, TDbContext>, IdentityModuleOptions>(
-                configure);
+                configure, optionsKey);
+        }
+
+        public static Application AddIdentity<TUser, TRole, TPk, TDbContext>(this Application application,
+            Action<IdentityModuleOptions>? configure = null, string? optionsKey = null)
+            where TUser : IdentityUser<TPk>
+            where TRole : IdentityRole<TPk>
+            where TPk : IEquatable<TPk>
+            where TDbContext : IdentityDbContext<TUser, TRole, TPk>
+        {
+            return application.AddModule<IdentityModule<TUser, TRole, TPk, TDbContext>, IdentityModuleOptions>(
+                configure, optionsKey);
         }
     }
 }
