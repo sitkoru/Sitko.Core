718d70e6443818d65d65b7c61e1d7d449ffa2645
George Drak <sonicgd@gmail.com>
2021-07-14 14:07:09 +0500

    Extract abstract LocalizationModule

--- src/Sitko.Core.App/Localization/JsonLocalizationModule.cs
+++ src/Sitko.Core.App/Localization/JsonLocalizationModule.cs
@@ -1,29 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Localization;
 
 namespace Sitko.Core.App.Localization
 {
-    public class JsonLocalizationModule : BaseApplicationModule<JsonLocalizationModuleOptions>
+    public class JsonLocalizationModule : LocalizationModule<JsonLocalizationModuleOptions, JsonStringLocalizerFactory>
     {
         public override string GetOptionsKey()
         {
             return "Localization:Json";
         }
-
-        public override void ConfigureServices(ApplicationContext context, IServiceCollection services,
-            JsonLocalizationModuleOptions startupOptions)
-        {
-            base.ConfigureServices(context, services, startupOptions);
-            services.TryAddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
-            services.TryAddTransient(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
-        }
     }
 
-    public class JsonLocalizationModuleOptions : BaseModuleOptions
+    public class JsonLocalizationModuleOptions : LocalizationModuleOptions
     {
         private readonly HashSet<Type> _defaultResources = new();
         public Type[] DefaultResources => _defaultResources.ToArray();
---
+++ src/Sitko.Core.App/Localization/LocalizationModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Localization;
+
+namespace Sitko.Core.App.Localization
+{
+    public abstract class LocalizationModule<TModuleOptions, TFactory> : BaseApplicationModule<TModuleOptions>
+        where TModuleOptions : LocalizationModuleOptions, new()
+        where TFactory : class, IStringLocalizerFactory
+    {
+        public override string GetOptionsKey()
+        {
+            return "Localization";
+        }
+
+        public override void ConfigureServices(ApplicationContext context, IServiceCollection services,
+            TModuleOptions startupOptions)
+        {
+            base.ConfigureServices(context, services, startupOptions);
+            services.TryAddSingleton<IStringLocalizerFactory, TFactory>();
+            services.TryAddTransient(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
+        }
+    }
+    
+    public class LocalizationModuleOptions : BaseModuleOptions
+    {
+    }
+}
