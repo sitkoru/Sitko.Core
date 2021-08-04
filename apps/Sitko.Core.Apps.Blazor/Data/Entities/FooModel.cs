2f018d54388cc592686b1196b70842bed96e239e
George Drak <sonicgd@gmail.com>
2021-08-04 15:13:12 +0500

    Add Foo validator

--- apps/Sitko.Core.Apps.Blazor/Data/Entities/FooModel.cs
+++ apps/Sitko.Core.Apps.Blazor/Data/Entities/FooModel.cs
@@ -4,9 +4,15 @@ using Sitko.Core.Repository;
 namespace Sitko.Core.Apps.Blazor.Data.Entities
 {
     using System.ComponentModel.DataAnnotations.Schema;
+    using FluentValidation;
 
     public class FooModel : Entity<Guid>
     {
         public string Foo { get; set; } = "";
+    }
+
+    public class FooValidator : AbstractValidator<FooModel>
+    {
+        public FooValidator() => RuleFor(f => f.Foo).NotEmpty();
     }
 }
