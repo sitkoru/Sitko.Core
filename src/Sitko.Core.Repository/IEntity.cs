8a3832f8d617d82fd75121cfb8da2dc9f6259b1c
George Drak <sonicgd@gmail.com>
2021-08-05 16:14:42 +0500

    Add base entity record

--- src/Sitko.Core.Repository/IEntity.cs
+++ src/Sitko.Core.Repository/IEntity.cs
@@ -21,4 +21,14 @@ namespace Sitko.Core.Repository
         public virtual TEntityPk Id { get; set; }
 #pragma warning restore 8618
     }
+
+    public abstract record EntityRecord<TEntityPk> : IEntity<TEntityPk>
+    {
+        public virtual object? GetId() => Id;
+
+        [Key]
+#pragma warning disable 8618
+        public virtual TEntityPk Id { get; set; }
+#pragma warning restore 8618
+    }
 }
