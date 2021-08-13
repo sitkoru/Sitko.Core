2661a1b61139025a10d75d2ccffc4a21972dfa30
George Drak <sonicgd@gmail.com>
2021-08-13 18:01:41 +0500

    build(scripts): delete old files before build new nightly packages

--- nightly.cmd
+++ nightly.cmd
@@ -2,4 +2,5 @@
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
 
+DEL /Q /F /S "%1\Sitko.Core.*"
 dotnet pack -c Release /p:Version=8.0.0-nightly.%ldt% -o %1
