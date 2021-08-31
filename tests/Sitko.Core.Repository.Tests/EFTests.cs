7dc7ac7fc3bcfb0c581ad60f352f2f9fa6386277
George Drak <sonicgd@gmail.com>
2021-08-31 15:48:17 +0500

    test(repository): add disconnected many-to-many tests

--- tests/Sitko.Core.Repository.Tests/EFTests.cs
+++ tests/Sitko.Core.Repository.Tests/EFTests.cs
@@ -776,6 +776,189 @@ namespace Sitko.Core.Repository.Tests
                 Assert.Equal(count - 1, updatedBar!.Foos.Count);
             }
         }
+
+        [Fact]
+        public async Task DisconnectedManyToMany()
+        {
+            var scope = await GetScopeAsync();
+            BarModel? originalBar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                originalBar = await repository1.GetAsync(q => q.Where(b => !b.BazModels.Any()));
+                originalBar.Should().NotBeNull();
+            }
+
+            BarModel? bar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                bar = await repository1.GetAsync(q => q.Where(b => b.Id == originalBar!.Id));
+                bar.Should().NotBeNull();
+            }
+
+            bar!.BazModels.Should().BeEmpty();
+
+            AddOrUpdateOperationResult<BazModel, Guid> newTestResult;
+            using (var scope2 = scope.CreateScope())
+            {
+                var repository2 = scope2.ServiceProvider.GetRequiredService<BazRepository>();
+                newTestResult = await repository2.AddAsync(await repository2.NewAsync());
+            }
+
+            newTestResult.IsSuccess.Should().BeTrue();
+
+            bar.BazModels.Add(newTestResult.Entity);
+
+            using (var scope3 = scope.CreateScope())
+            {
+                var repository3 = scope3.ServiceProvider.GetRequiredService<BarRepository>();
+                var updateResult = await repository3.UpdateAsync(bar, originalBar);
+                updateResult.IsSuccess.Should().BeTrue();
+                updateResult.Changes.Should().ContainSingle();
+            }
+
+            using (var finalScope = scope.CreateScope())
+            {
+                var repository = finalScope.ServiceProvider.GetRequiredService<BarRepository>();
+                var updatedBar =
+                    await repository.GetAsync(q => q.Where(b => b.Id == bar.Id).Include(b => b.BazModels));
+                updatedBar!.BazModels.Should().ContainSingle();
+            }
+        }
+
+        [Fact]
+        public async Task DisconnectedManyToManyUpdateEntity()
+        {
+            var scope = await GetScopeAsync();
+            BarModel? originalBar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                originalBar = await repository1.GetAsync(q => q.Where(b => b.BazModels.Any()).Include(b => b.Foos));
+                originalBar.Should().NotBeNull();
+            }
+
+            BarModel? bar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                bar = await repository1.GetAsync(q => q.Where(b => b.Id == originalBar!.Id).Include(b => b.BazModels));
+                bar.Should().NotBeNull();
+            }
+
+            bar!.BazModels.Should().NotBeEmpty();
+            var foo = bar.BazModels.OrderBy(_ => Guid.NewGuid()).First(); // change random
+            var newText = Guid.NewGuid().ToString();
+            foo.Baz = newText;
+
+            using (var scope3 = scope.CreateScope())
+            {
+                var repository3 = scope3.ServiceProvider.GetRequiredService<BarRepository>();
+                var updateResult = await repository3.UpdateAsync(bar, originalBar);
+                updateResult.IsSuccess.Should().BeTrue();
+                updateResult.Changes.Should().ContainSingle();
+            }
+
+            using (var finalScope = scope.CreateScope())
+            {
+                var repository = finalScope.ServiceProvider.GetRequiredService<BarRepository>();
+                var updatedBar =
+                    await repository.GetAsync(q => q.Where(b => b.Id == bar.Id).Include(b => b.Foos));
+                updatedBar!.BazModels.First(f => f.Id == foo.Id).Baz.Should().BeEquivalentTo(newText);
+            }
+        }
+
+        [Fact]
+        public async Task DisconnectedManyToManyMultipleEntity()
+        {
+            var scope = await GetScopeAsync();
+            BarModel? originalBar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                originalBar = await repository1.GetAsync(q => q.Where(b => b.BazModels.Any()));
+                originalBar.Should().NotBeNull();
+            }
+
+            BarModel? bar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                bar = await repository1.GetAsync(q => q.Where(b => b.Id == originalBar!.Id));
+                bar.Should().NotBeNull();
+            }
+
+            bar!.BazModels.Should().NotBeEmpty();
+            var count = bar.BazModels.Count;
+            var baz1 = new BazModel();
+            var baz2 = new BazModel();
+            bar.BazModels.Remove(bar.BazModels.OrderBy(_ => Guid.NewGuid()).First());
+            bar.BazModels.Add(baz1);
+            bar.BazModels.Add(baz2);
+
+            using (var scope3 = scope.CreateScope())
+            {
+                var repository3 = scope3.ServiceProvider.GetRequiredService<BarRepository>();
+                var updateResult = await repository3.UpdateAsync(bar, originalBar);
+                updateResult.IsSuccess.Should().BeTrue();
+                updateResult.Changes.Should().ContainSingle();
+            }
+
+            using (var finalScope = scope.CreateScope())
+            {
+                var repository = finalScope.ServiceProvider.GetRequiredService<BarRepository>();
+                var updatedBar =
+                    await repository.GetAsync(q => q.Where(b => b.Id == bar.Id).Include(b => b.BazModels));
+                updatedBar!.BazModels.Should().NotBeEmpty();
+                updatedBar.BazModels.Count.Should().Be(count + 1);
+            }
+        }
+
+        [Fact]
+        public async Task DisconnectedManyToManyDeleteEntity()
+        {
+            var scope = await GetScopeAsync();
+            BarModel? originalBar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                originalBar =
+                    await repository1.GetAsync(q => q.Where(b => b.BazModels.Count > 1).Include(b => b.BazModels));
+                originalBar.Should().NotBeNull();
+            }
+
+            BarModel? bar;
+            using (var scope1 = scope.CreateScope())
+            {
+                var repository1 = scope1.ServiceProvider.GetRequiredService<BarRepository>();
+                bar = await repository1.GetAsync(q => q.Where(b => b.Id == originalBar!.Id).Include(b => b.BazModels));
+                bar.Should().NotBeNull();
+            }
+
+
+            bar!.BazModels.Should().NotBeEmpty();
+            var count = bar.BazModels.Count;
+            count.Should().Be(2);
+
+            bar.BazModels.Remove(bar.BazModels.OrderBy(_ => Guid.NewGuid()).First()); // delete random
+            using (var scope3 = scope.CreateScope())
+            {
+                var repository3 = scope3.ServiceProvider.GetRequiredService<BarRepository>();
+                var updateResult =
+                    await repository3.UpdateAsync(bar, originalBar);
+                updateResult.IsSuccess.Should().BeTrue();
+                updateResult.Changes.Should().ContainSingle();
+            }
+
+            using (var finalScope = scope.CreateScope())
+            {
+                var repository = finalScope.ServiceProvider.GetRequiredService<BarRepository>();
+                var updatedBar =
+                    await repository.GetAsync(q => q.Where(b => b.Id == bar.Id).Include(b => b.BazModels));
+                updatedBar!.BazModels.Count.Should().Be(count - 1);
+            }
+        }
     }
 
     public class TestDbContext : DbContext
@@ -879,6 +1062,14 @@ namespace Sitko.Core.Repository.Tests
             base.AddIncludes(query).Include(b => b.BazModels).Include(b => b.Foos).ThenInclude(f => f.BazModels);
     }
 
+    public class BazRepository : EFRepository<BazModel, Guid, TestDbContext>
+    {
+        public BazRepository(EFRepositoryContext<BazModel, Guid, TestDbContext> repositoryContext) : base(
+            repositoryContext)
+        {
+        }
+    }
+
     public class FooRepository : EFRepository<FooModel, Guid, TestDbContext>
     {
         public FooRepository(EFRepositoryContext<FooModel, Guid, TestDbContext> repositoryContext) : base(
@@ -929,7 +1120,12 @@ namespace Sitko.Core.Repository.Tests
                     JsonModels = new List<BaseJsonModel> { new JsonModelBar(), new JsonModelFoo() }
                 },
                 new() { Id = Guid.NewGuid() },
-                new() { Id = Guid.NewGuid() }
+                new() { Id = Guid.NewGuid() },
+                new() { Id = Guid.NewGuid() },
+                new() { Id = Guid.NewGuid() },
+                new() { Id = Guid.NewGuid() },
+                new() { Id = Guid.NewGuid() },
+                new() { Id = Guid.NewGuid() },
             };
             await dbContext.AddRangeAsync(barModels);
 
