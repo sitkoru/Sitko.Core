213c5d38aa2d3ebf6cbe70aead60ff60402105e6
George Drak <sonicgd@gmail.com>
2021-09-02 18:30:02 +0500

    feat(grpc): add IsSuccess shortcut to IGrpcResponse

--- src/Sitko.Core.Grpc/Extensions/GrpcExtensions.cs
+++ src/Sitko.Core.Grpc/Extensions/GrpcExtensions.cs
@@ -12,19 +12,21 @@ namespace Sitko.Core.Grpc.Extensions
         public static void SetError(this IGrpcResponse response, string error, int code = 500)
         {
             response.ResponseInfo.IsSuccess = false;
-            response.ResponseInfo.Error = new ApiResponseError {Code = code, Errors = {error}};
+            response.ResponseInfo.Error = new ApiResponseError { Code = code, Errors = { error } };
         }
 
         public static void SetErrors(this IGrpcResponse response, IEnumerable<string> errors, int code = 500)
         {
             response.ResponseInfo.IsSuccess = false;
-            response.ResponseInfo.Error = new ApiResponseError {Code = code, Errors = {errors}};
+            response.ResponseInfo.Error = new ApiResponseError { Code = code, Errors = { errors } };
         }
 
         public static void SetException(this IGrpcResponse response, Exception ex, int code = 500)
         {
             response.ResponseInfo.IsSuccess = false;
-            response.ResponseInfo.Error = new ApiResponseError {Code = code, Errors = {ex.ToString()}};
+            response.ResponseInfo.Error = new ApiResponseError { Code = code, Errors = { ex.ToString() } };
         }
+
+        public static bool IsSuccess(this IGrpcResponse response) => response.ResponseInfo.IsSuccess;
     }
 }
