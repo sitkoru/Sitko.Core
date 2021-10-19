c6a9c2b06b59e3f8a85c5bbf7f4a005ba9065657
George Drak <sonicgd@gmail.com>
2021-10-19 18:58:53 +0500

    feat(results): add classes for operation results

---
+++ src/Sitko.Core.App/Results/OperationResult.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace Sitko.Core.App.Results;
+
+public interface IOperationResult
+{
+    bool IsSuccess { get; }
+    Exception? Exception { get; }
+    string? ErrorMessage { get; }
+}
+
+[PublicAPI]
+public class OperationResult : IOperationResult
+{
+    public OperationResult() => IsSuccess = true;
+
+    public OperationResult(string errorMessage)
+    {
+        IsSuccess = false;
+        ErrorMessage = errorMessage;
+    }
+
+    public OperationResult(Exception exception) : this(exception.Message) => Exception = exception;
+#if NET5_0_OR_GREATER
+    [MemberNotNullWhen(false, nameof(ErrorMessage))]
+#endif
+    public bool IsSuccess { get; private set; }
+
+    public string? ErrorMessage { get; private set; }
+    public Exception? Exception { get; private set; }
+
+    public virtual void SetError(string errorMessage)
+    {
+        IsSuccess = false;
+        ErrorMessage = errorMessage;
+    }
+
+    public virtual void SetException(Exception exception)
+    {
+        SetError(exception.Message);
+        Exception = exception;
+    }
+}
+
+[PublicAPI]
+public class OperationResult<T> : IOperationResult
+{
+    public OperationResult(T result)
+    {
+        IsSuccess = true;
+        Result = result;
+    }
+
+    public OperationResult(string errorMessage)
+    {
+        IsSuccess = false;
+        ErrorMessage = errorMessage;
+    }
+
+    public OperationResult(Exception exception) : this(exception.Message) => Exception = exception;
+
+    public T? Result { get; private set; }
+#if NET5_0_OR_GREATER
+    [MemberNotNullWhen(true, nameof(Result))]
+    [MemberNotNullWhen(false, nameof(ErrorMessage))]
+#endif
+    public bool IsSuccess { get; private set; }
+
+    public Exception? Exception { get; }
+    public string? ErrorMessage { get; }
+
+    public OperationResult<T> SetResult(T result)
+    {
+        Result = result;
+        return this;
+    }
+}
---
+++ tests/Sitko.Core.App.Tests/ResultTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using FluentAssertions;
+using Sitko.Core.App.Results;
+using Sitko.Core.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sitko.Core.App.Tests;
+
+public class ResultTests : BaseTest
+{
+    public ResultTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public void Success()
+    {
+        var result = new OperationResult();
+        result.IsSuccess.Should().BeTrue();
+        result.ErrorMessage.Should().BeNull();
+        result.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void Error()
+    {
+        var result = new OperationResult("Error");
+        result.IsSuccess.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Error");
+        result.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void SetError()
+    {
+        var result = new OperationResult();
+        result.IsSuccess.Should().BeTrue();
+        result.SetError("Error");
+        result.IsSuccess.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Error");
+    }
+
+    [Fact]
+    public void SetException()
+    {
+        var ex = new InvalidOperationException("Exception");
+        var result = new OperationResult();
+        result.IsSuccess.Should().BeTrue();
+        result.SetException(ex);
+        result.IsSuccess.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Exception");
+        result.Exception.Should().Be(ex);
+    }
+
+    [Fact]
+    public void Exception()
+    {
+        var ex = new InvalidOperationException("Exception");
+        var result = new OperationResult(ex);
+        result.IsSuccess.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Exception");
+        result.Exception.Should().Be(ex);
+    }
+
+    [Fact]
+    public void Result()
+    {
+        var result = new OperationResult<Foo>(new Foo(1));
+        result.IsSuccess.Should().BeTrue();
+        result.Result.Should().NotBeNull();
+        result.Result!.Value.Should().Be(1);
+        result.ErrorMessage.Should().BeNull();
+        result.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void ResultError()
+    {
+        var result = new OperationResult<Foo>("Error");
+        result.IsSuccess.Should().BeFalse();
+        result.Result.Should().BeNull();
+        result.ErrorMessage.Should().Be("Error");
+        result.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void ResultException()
+    {
+        var ex = new InvalidOperationException("Exception");
+        var result = new OperationResult<Foo>(ex);
+        result.IsSuccess.Should().BeFalse();
+        result.Result.Should().BeNull();
+        result.ErrorMessage.Should().Be("Exception");
+        result.Exception.Should().Be(ex);
+    }
+}
+
+public class Foo
+{
+    public int Value { get; }
+
+    public Foo(int value) => Value = value;
+}
