87743d95528451c357d363f99f071181c9f8db7e
George Drak <sonicgd@gmail.com>
2021-11-11 17:26:39 +0500

    feat(swagger): remove swagger auth, add endpoint configuration

--- src/Sitko.Core.Swagger/SwaggerModule.cs
+++ src/Sitko.Core.Swagger/SwaggerModule.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,51 +7,53 @@ using Microsoft.OpenApi.Models;
 using Sitko.Core.App;
 using Sitko.Core.App.Web;
 
-namespace Sitko.Core.Swagger
+namespace Sitko.Core.Swagger;
+
+public class SwaggerModule : BaseApplicationModule<SwaggerModuleOptions>, IWebApplicationModule
 {
-    public class SwaggerModule : BaseApplicationModule<SwaggerModuleOptions>, IWebApplicationModule
-    {
-        public override string OptionsKey => "Swagger";
+    public override string OptionsKey => "Swagger";
 
-        public override void ConfigureServices(ApplicationContext context, IServiceCollection services,
-            SwaggerModuleOptions startupOptions)
+    public void ConfigureAfterUseRouting(IConfiguration configuration, IHostEnvironment environment,
+        IApplicationBuilder appBuilder)
+    {
+        var config = GetOptions(appBuilder.ApplicationServices);
+        appBuilder.UseSwagger();
+        var endPoint = !string.IsNullOrEmpty(config.Endpoint) ? config.Endpoint : $"{config.Version}/swagger.json";
+        appBuilder.UseSwaggerUI(c => { c.SwaggerEndpoint(endPoint, $"{config.Title} ({config.Version})"); });
+    }
+
+    public override void ConfigureServices(ApplicationContext context, IServiceCollection services,
+        SwaggerModuleOptions startupOptions)
+    {
+        base.ConfigureServices(context, services, startupOptions);
+        services.AddSwaggerGen(c =>
         {
-            base.ConfigureServices(context, services, startupOptions);
-            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = startupOptions.Title, Version = startupOptions.Version });
+            if (startupOptions.EnableTokenAuth)
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = startupOptions.Title, Version = startupOptions.Version});
-                if (startupOptions.EnableTokenAuth)
-                {
-                    c.AddSecurityDefinition("Bearer",
-                        new OpenApiSecurityScheme()
-                        {
-                            Description = "Auth token",
-                            Name = "Authorization",
-                            In = ParameterLocation.Header,
-                            Type = SecuritySchemeType.ApiKey
-                        });
-                    var security = new OpenApiSecurityRequirement
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
                     {
-                        {new OpenApiSecurityScheme {Name = "Bearer"}, System.Array.Empty<string>()}
-                    };
-                    c.AddSecurityRequirement(security);
-                }
-            });
-        }
-
-        public void ConfigureAfterUseRouting(IConfiguration configuration, IHostEnvironment environment,
-            IApplicationBuilder appBuilder)
-        {
-            var config = GetOptions(appBuilder.ApplicationServices);
-            appBuilder.UseSwaggerAuthorized($"{config.Title} ({config.Version})", "v1/swagger.json");
-        }
+                        Description = "Auth token",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
+                    });
+                var security = new OpenApiSecurityRequirement
+                {
+                    { new OpenApiSecurityScheme { Name = "Bearer" }, Array.Empty<string>() }
+                };
+                c.AddSecurityRequirement(security);
+            }
+        });
     }
+}
 
-    public class SwaggerModuleOptions : BaseModuleOptions
-    {
-        public string Title { get; set; } = "App";
-        public string Version { get; set; } = "v1";
+public class SwaggerModuleOptions : BaseModuleOptions
+{
+    public string Title { get; set; } = "App";
+    public string Version { get; set; } = "v1";
+    public string? Endpoint { get; set; }
 
-        public bool EnableTokenAuth { get; set; } = true;
-    }
+    public bool EnableTokenAuth { get; set; } = true;
 }
