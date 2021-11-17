052182b73f537e7a9dc510ce0687f8b88a4211a5
George Drak <sonicgd@gmail.com>
2021-11-17 17:39:20 +0500

    style: reformat code

--- src/Sitko.Core.App/Validation/FluentGraphValidator.cs
+++ src/Sitko.Core.App/Validation/FluentGraphValidator.cs
@@ -10,192 +10,191 @@ using FluentValidation.Results;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Sitko.Core.App.Validation
+namespace Sitko.Core.App.Validation;
+
+public class FluentGraphValidator
 {
-    public class FluentGraphValidator
+    private readonly ILogger<FluentGraphValidator> logger;
+    private readonly IServiceScope serviceScope;
+
+    public FluentGraphValidator(IServiceProvider serviceProvider, ILogger<FluentGraphValidator> logger)
     {
-        private readonly ILogger<FluentGraphValidator> logger;
-        private readonly IServiceScope serviceScope;
+        this.logger = logger;
+        serviceScope = serviceProvider.CreateScope();
+    }
 
-        public FluentGraphValidator(IServiceProvider serviceProvider, ILogger<FluentGraphValidator> logger)
+    private IValidationContext CreateValidationContext(object model, IValidatorSelector? validatorSelector = null)
+    {
+        validatorSelector ??= ValidatorOptions.Global.ValidatorSelectors.DefaultValidatorSelectorFactory();
+
+        var context = new ValidationContext<object>(model, new PropertyChain(), validatorSelector)
         {
-            this.logger = logger;
-            serviceScope = serviceProvider.CreateScope();
+            RootContextData = { ["_FV_ServiceProvider"] = serviceScope.ServiceProvider }
+        };
+
+        return context;
+    }
+
+    private IValidator? TryGetModelValidator(object model)
+    {
+        var validatorType = typeof(IValidator<>);
+        var formValidatorType = validatorType.MakeGenericType(model.GetType());
+        var validator = serviceScope.ServiceProvider.GetService(formValidatorType) as IValidator;
+        if (validator is null)
+        {
+            logger.LogWarning(
+                "FluentValidation.IValidator<{FormType}> is not registered in the application service provider",
+                model.GetType().FullName);
         }
 
-        private IValidationContext CreateValidationContext(object model, IValidatorSelector? validatorSelector = null)
+        return validator;
+    }
+
+    public async Task<ModelsValidationResult> TryValidateFieldAsync(object model, string fieldName,
+        ModelsValidationResult? result = null,
+        CancellationToken cancellationToken = default)
+    {
+        try
         {
-            validatorSelector ??= ValidatorOptions.Global.ValidatorSelectors.DefaultValidatorSelectorFactory();
-
-            var context = new ValidationContext<object>(model, new PropertyChain(), validatorSelector)
-            {
-                RootContextData = { ["_FV_ServiceProvider"] = serviceScope.ServiceProvider }
-            };
-
-            return context;
+            var validatorSelector = new MemberNameValidatorSelector(new[] { fieldName });
+            var validationContext = CreateValidationContext(model, validatorSelector);
+            return await TryValidateModelAsync(model, validationContext, result, cancellationToken);
         }
-
-        private IValidator? TryGetModelValidator(object model)
+        catch (Exception ex)
         {
-            var validatorType = typeof(IValidator<>);
-            var formValidatorType = validatorType.MakeGenericType(model.GetType());
-            var validator = serviceScope.ServiceProvider.GetService(formValidatorType) as IValidator;
-            if (validator is null)
+            var msg = $"An unhandled exception occurred when validating field name: '{fieldName}'";
+
+            msg += $" of model of type: '{model.GetType()}'";
+            throw new UnhandledValidationException(msg, ex);
+        }
+    }
+
+    public async Task<ModelsValidationResult> TryValidateModelAsync(object model,
+        IValidationContext? validationContext = null, ModelsValidationResult? result = null,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            validationContext ??= CreateValidationContext(model);
+            result ??= new ModelsValidationResult();
+            if (result.Results.Any(r => r.Model.Equals(model)))
             {
-                logger.LogWarning(
-                    "FluentValidation.IValidator<{FormType}> is not registered in the application service provider",
-                    model.GetType().FullName);
+                return result;
             }
 
-            return validator;
-        }
+            var modelResult = new ModelValidationResult(model);
+            result.Results.Add(modelResult);
 
-        public async Task<ModelsValidationResult> TryValidateFieldAsync(object model, string fieldName,
-            ModelsValidationResult? result = null,
-            CancellationToken cancellationToken = default)
-        {
-            try
+            var validator = TryGetModelValidator(model);
+            if (validator is not null)
             {
-                var validatorSelector = new MemberNameValidatorSelector(new[] { fieldName });
-                var validationContext = CreateValidationContext(model, validatorSelector);
-                return await TryValidateModelAsync(model, validationContext, result, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                var msg = $"An unhandled exception occurred when validating field name: '{fieldName}'";
-
-                msg += $" of model of type: '{model.GetType()}'";
-                throw new UnhandledValidationException(msg, ex);
-            }
-        }
-
-        public async Task<ModelsValidationResult> TryValidateModelAsync(object model,
-            IValidationContext? validationContext = null, ModelsValidationResult? result = null,
-            CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                validationContext ??= CreateValidationContext(model);
-                result ??= new ModelsValidationResult();
-                if (result.Results.Any(r => r.Model.Equals( model)))
+                var validationResult = await validator.ValidateAsync(validationContext, cancellationToken);
+                if (!validationResult.IsValid)
                 {
-                    return result;
+                    modelResult.Errors.AddRange(validationResult.Errors);
                 }
+            }
 
-                var modelResult = new ModelValidationResult(model);
-                result.Results.Add(modelResult);
+            foreach (var property in model.GetType().GetProperties())
+            {
+                var propertyModel = property.GetValue(model);
 
-                var validator = TryGetModelValidator(model);
-                if (validator is not null)
+
+                if (propertyModel is not string && propertyModel is IEnumerable enumerable)
                 {
-                    var validationResult = await validator.ValidateAsync(validationContext, cancellationToken);
-                    if (!validationResult.IsValid)
+                    foreach (var item in enumerable)
                     {
-                        modelResult.Errors.AddRange(validationResult.Errors);
-                    }
-                }
-
-                foreach (var property in model.GetType().GetProperties())
-                {
-                    var propertyModel = property.GetValue(model);
-
-
-                    if (propertyModel is not string && propertyModel is IEnumerable enumerable)
-                    {
-                        foreach (var item in enumerable)
-                        {
-                            await TryValidateModelAsync(item, result: result,
-                                cancellationToken: cancellationToken);
-                        }
-                    }
-                    else
-                    {
-                        if (propertyModel is null ||
-                            propertyModel.GetType().Module.ScopeName == "CommonLanguageRuntimeLibrary" ||
-                            propertyModel.GetType().Module.ScopeName.StartsWith("System") ||
-                            propertyModel.GetType().Namespace.StartsWith("System") ||
-                            propertyModel.GetType().Namespace.StartsWith("Microsoft"))
-                        {
-                            continue;
-                        }
-
-                        await TryValidateModelAsync(propertyModel, result: result,
+                        await TryValidateModelAsync(item, result: result,
                             cancellationToken: cancellationToken);
                     }
                 }
+                else
+                {
+                    if (propertyModel is null ||
+                        propertyModel.GetType().Module.ScopeName == "CommonLanguageRuntimeLibrary" ||
+                        propertyModel.GetType().Module.ScopeName.StartsWith("System") ||
+                        propertyModel.GetType().Namespace.StartsWith("System") ||
+                        propertyModel.GetType().Namespace.StartsWith("Microsoft"))
+                    {
+                        continue;
+                    }
 
-                return result;
+                    await TryValidateModelAsync(propertyModel, result: result,
+                        cancellationToken: cancellationToken);
+                }
             }
-            catch (Exception ex)
-            {
-                var msg = $"An unhandled exception occurred when validating object of type: '{model.GetType()}'";
-                throw new UnhandledValidationException(msg, ex);
-            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            var msg = $"An unhandled exception occurred when validating object of type: '{model.GetType()}'";
+            throw new UnhandledValidationException(msg, ex);
         }
     }
+}
 
-    public class ModelsValidationResult
+public class ModelsValidationResult
+{
+    public bool IsValid => Results.All(r => r.IsValid);
+    public HashSet<ModelValidationResult> Results { get; } = new();
+}
+
+public class ModelValidationResult : IEquatable<ModelValidationResult>
+{
+    public ModelValidationResult(object model) => Model = model;
+
+    public ModelValidationResult(object model, IEnumerable<ValidationFailure> errors)
     {
-        public bool IsValid => Results.All(r => r.IsValid);
-        public HashSet<ModelValidationResult> Results { get; } = new();
+        Model = model;
+        Errors.AddRange(errors);
     }
 
-    public class ModelValidationResult : IEquatable<ModelValidationResult>
+    public object Model { get; }
+    public bool IsValid => !Errors.Any();
+    public List<ValidationFailure> Errors { get; } = new();
+
+    public bool Equals(ModelValidationResult? other)
     {
-        public object Model { get; }
-        public bool IsValid => !Errors.Any();
-        public List<ValidationFailure> Errors { get; } = new();
-
-        public ModelValidationResult(object model) => Model = model;
-
-        public ModelValidationResult(object model, IEnumerable<ValidationFailure> errors)
+        if (ReferenceEquals(null, other))
         {
-            Model = model;
-            Errors.AddRange(errors);
+            return false;
         }
 
-        public bool Equals(ModelValidationResult? other)
+        if (ReferenceEquals(this, other))
         {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            return Model.Equals(other.Model);
+            return true;
         }
 
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != this.GetType())
-            {
-                return false;
-            }
-
-            return Equals((ModelValidationResult)obj);
-        }
-
-        public override int GetHashCode() => Model.GetHashCode();
+        return Model.Equals(other.Model);
     }
 
-    internal class UnhandledValidationException : Exception
+    public override bool Equals(object? obj)
     {
-        public UnhandledValidationException(string message, Exception innerException) : base(message, innerException)
+        if (ReferenceEquals(null, obj))
         {
+            return false;
         }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((ModelValidationResult)obj);
+    }
+
+    public override int GetHashCode() => Model.GetHashCode();
+}
+
+internal class UnhandledValidationException : Exception
+{
+    public UnhandledValidationException(string message, Exception innerException) : base(message, innerException)
+    {
     }
 }
--- src/Sitko.Core.Repository/IEntity.cs
+++ src/Sitko.Core.Repository/IEntity.cs
@@ -2,98 +2,97 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Sitko.Core.Repository
+namespace Sitko.Core.Repository;
+
+public interface IEntity<TEntityPk> : IEntity
 {
-    public interface IEntity<TEntityPk> : IEntity
-    {
-        TEntityPk Id { get; set; }
-    }
+    TEntityPk Id { get; set; }
+}
 
-    public interface IEntity
-    {
-        object? EntityId { get; }
-    }
+public interface IEntity
+{
+    object? EntityId { get; }
+}
 
-    public abstract class Entity<TEntityPk> : IEntity<TEntityPk>, IEquatable<Entity<TEntityPk>>
-    {
-        public virtual object? EntityId => Id;
+public abstract class Entity<TEntityPk> : IEntity<TEntityPk>, IEquatable<Entity<TEntityPk>>
+{
+    public virtual object? EntityId => Id;
 
-        [Key]
+    [Key]
 #pragma warning disable 8618
-        public virtual TEntityPk Id { get; set; }
+    public virtual TEntityPk Id { get; set; }
 #pragma warning restore 8618
 
-        public override string ToString() => $"{GetType().Name} [Id: {Id}]";
-
-        public bool Equals(Entity<TEntityPk>? other)
+    public bool Equals(Entity<TEntityPk>? other)
+    {
+        if (ReferenceEquals(null, other))
         {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
+            return false;
+        }
 
-            if (ReferenceEquals(this, other))
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        return EqualityComparer<TEntityPk>.Default.Equals(Id, other.Id);
+    }
+
+    public override string ToString() => $"{GetType().Name} [Id: {Id}]";
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((Entity<TEntityPk>)obj);
+    }
+
+    public override int GetHashCode() => EqualityComparer<TEntityPk>.Default.GetHashCode(Id);
+
+    public static bool operator ==(Entity<TEntityPk>? lhs, Entity<TEntityPk>? rhs)
+    {
+        if (lhs is null)
+        {
+            if (rhs is null)
             {
                 return true;
             }
 
-            if (GetType() != other.GetType())
-            {
-                return false;
-            }
-
-            return EqualityComparer<TEntityPk>.Default.Equals(Id, other.Id);
+            // Only the left side is null.
+            return false;
         }
 
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != GetType())
-            {
-                return false;
-            }
-
-            return Equals((Entity<TEntityPk>)obj);
-        }
-
-        public override int GetHashCode() => EqualityComparer<TEntityPk>.Default.GetHashCode(Id);
-
-        public static bool operator ==(Entity<TEntityPk>? lhs, Entity<TEntityPk>? rhs)
-        {
-            if (lhs is null)
-            {
-                if (rhs is null)
-                {
-                    return true;
-                }
-
-                // Only the left side is null.
-                return false;
-            }
-
-            // Equals handles case of null on right side.
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(Entity<TEntityPk>? lhs, Entity<TEntityPk>? rhs) => !(lhs == rhs);
+        // Equals handles case of null on right side.
+        return lhs.Equals(rhs);
     }
 
-    public abstract record EntityRecord<TEntityPk> : IEntity<TEntityPk>
-    {
-        public object? EntityId => Id;
+    public static bool operator !=(Entity<TEntityPk>? lhs, Entity<TEntityPk>? rhs) => !(lhs == rhs);
+}
 
-        [Key]
+public abstract record EntityRecord<TEntityPk> : IEntity<TEntityPk>
+{
+    public object? EntityId => Id;
+
+    [Key]
 #pragma warning disable 8618
-        public virtual TEntityPk Id { get; set; }
+    public virtual TEntityPk Id { get; set; }
 #pragma warning restore 8618
-    }
 }
