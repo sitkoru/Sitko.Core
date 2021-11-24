c9b36d60a85483aa0c776261e7be0a2f920b823b
George Drak <sonicgd@gmail.com>
2021-11-24 11:53:07 +0500

    test(repository): add entities equality test

---
+++ tests/Sitko.Core.Repository.Tests/EntityTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Sitko.Core.Repository.Tests;
+
+public class EntityTests
+{
+    [Fact]
+    public void Equality()
+    {
+        var fooEntity = new FooEntity { Id = Guid.NewGuid(), Test = "First" };
+        var fooEntityClone = new FooEntity { Id = fooEntity.Id, Test = "First Clone" };
+        var barEntity = new BarEntity { Id = fooEntity.Id };
+        Assert.True(fooEntity == fooEntityClone);
+        Assert.True(fooEntity.Equals(fooEntityClone));
+        Assert.False(fooEntity == barEntity);
+        Assert.False(fooEntity.Equals(barEntity));
+    }
+}
+
+public class FooEntity : Entity<Guid>
+{
+    public string Test { get; set; } = "";
+}
+
+public class BarEntity : Entity<Guid>
+{
+}
