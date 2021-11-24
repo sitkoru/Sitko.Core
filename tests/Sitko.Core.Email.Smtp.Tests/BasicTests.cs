242801fb52226c2df401a0dbda281a6c8b23c9f0
George Drak <sonicgd@gmail.com>
2021-11-24 11:51:57 +0500

    tests(smtp): read config from appsettings.json

--- tests/Sitko.Core.Email.Smtp.Tests/BasicTests.cs
+++ tests/Sitko.Core.Email.Smtp.Tests/BasicTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Sitko.Core.App.Web;
@@ -5,29 +6,37 @@ using Sitko.Core.Email.Tests;
 using Sitko.Core.Xunit;
 using Xunit.Abstractions;
 
-namespace Sitko.Core.Email.Smtp.Tests
+namespace Sitko.Core.Email.Smtp.Tests;
+
+public class BasicTests : BasicTests<SmtpTestsScope>
 {
-    public class BasicTests : BasicTests<SmtpTestsScope>
+    public BasicTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
     {
-        public BasicTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    }
+}
+
+public class SmtpTestsScope : BaseTestScope<TestApplication>
+{
+    protected override TestApplication CreateApplication()
+    {
+        var app = new TestApplication(Array.Empty<string>());
+        app.ConfigureAppConfiguration((_, _, builder) =>
         {
-        }
+            builder.AddJsonFile("appsettings.json");
+        });
+        return app;
     }
+}
 
-    public class SmtpTestsScope : BaseTestScope<TestApplication>
+public class TestStartup : BaseStartup
+{
+    public TestStartup(IConfiguration configuration, IHostEnvironment environment) : base(
+        configuration, environment)
     {
     }
+}
 
-    public class TestStartup : BaseStartup
-    {
-        public TestStartup(IConfiguration configuration, IHostEnvironment environment) : base(
-            configuration, environment)
-        {
-        }
-    }
-
-    public class TestApplication : WebApplication<TestStartup>
-    {
-        public TestApplication(string[] args) : base(args) => this.AddSmtpEmail();
-    }
+public class TestApplication : WebApplication<TestStartup>
+{
+    public TestApplication(string[] args) : base(args) => this.AddSmtpEmail();
 }
