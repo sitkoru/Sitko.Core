892e344e591767b6e0507eea8a3698ec6779fd71
George Drak <sonicgd@gmail.com>
2022-01-12 11:24:47 +0500

    fix: google auth module now correctly rejects unlisted users
    Refs: SITKO-CORE-T-2

--- src/Sitko.Core.Auth.Google/GoogleAuthModule.cs
+++ src/Sitko.Core.Auth.Google/GoogleAuthModule.cs
@@ -1,49 +1,57 @@
 ﻿using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace Sitko.Core.Auth.Google
+namespace Sitko.Core.Auth.Google;
+
+public class GoogleAuthModule : AuthModule<GoogleAuthModuleOptions>
 {
-    public class GoogleAuthModule : AuthModule<GoogleAuthModuleOptions>
+    public override string OptionsKey => "Auth:Google";
+
+    protected override void ConfigureAuthentication(AuthenticationBuilder authenticationBuilder,
+        GoogleAuthModuleOptions startupOptions) => authenticationBuilder.AddGoogle(options =>
     {
-        public override string OptionsKey => "Auth:Google";
-
-        protected override void ConfigureAuthentication(AuthenticationBuilder authenticationBuilder,
-            GoogleAuthModuleOptions startupOptions) => authenticationBuilder.AddGoogle(options =>
+        options.ClientId = startupOptions.ClientId;
+        options.ClientSecret = startupOptions.ClientSecret;
+        options.SaveTokens = true;
+        if (startupOptions.Users.Any())
         {
-            options.ClientId = startupOptions.ClientId;
-            options.ClientSecret = startupOptions.ClientSecret;
-            options.SaveTokens = true;
-            if (startupOptions.Users.Any())
+            options.Events = new OAuthEvents
             {
-                options.Events = new OAuthEvents
+                OnTicketReceived = async receivedContext =>
                 {
-                    OnTicketReceived = receivedContext =>
+                    var config = GetOptions(receivedContext.HttpContext.RequestServices);
+                    var email = receivedContext.Principal?.Claims
+                        .FirstOrDefault(c => c.Type == ClaimTypes.Email)
+                        ?.Value;
+                    string? error = null;
+                    if (email is null)
                     {
-                        var config = GetOptions(receivedContext.HttpContext.RequestServices);
-                        var email = receivedContext.Principal?.Claims
-                            .FirstOrDefault(c => c.Type == ClaimTypes.Email)
-                            ?.Value;
-                        if (email is null)
-                        {
-                            receivedContext.Fail($"Empty {email} is not allowed");
-                        }
-                        else if (!config.Users.Contains(email))
-                        {
-                            receivedContext.Fail($"User {email} is not allowed");
-                        }
-                        else
-                        {
-                            receivedContext.Success();
-                        }
-
-                        return Task.CompletedTask;
+                        error = $"Empty {email} is not allowed";
                     }
-                };
-            }
-        });
-    }
+                    else if (!config.Users.Contains(email))
+                    {
+                        error = $"User {email} is not allowed";
+                    }
+
+                    if (error is not null)
+                    {
+                        var logger = receivedContext.HttpContext.RequestServices
+                            .GetRequiredService<ILogger<GoogleAuthModule>>();
+                        logger.LogError("Auth error: {ErrorText}", error);
+                        receivedContext.Response.StatusCode = 403;
+                        await receivedContext.Response.WriteAsync(error);
+                        receivedContext.HandleResponse();
+                        return;
+                    }
+
+                    receivedContext.Success();
+                }
+            };
+        }
+    });
 }
