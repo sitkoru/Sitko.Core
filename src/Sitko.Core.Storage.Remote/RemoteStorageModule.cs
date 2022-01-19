be8c1846543a9030a24afe5bbf7741053c8a2685
George Drak <sonicgd@gmail.com>
2022-01-19 19:04:33 +0500

    feat(remotestorage): implement upload, download and existence check methods
    Refs: SITKO-CORE-T-4

--- src/Sitko.Core.Storage.Remote/RemoteStorageModule.cs
+++ src/Sitko.Core.Storage.Remote/RemoteStorageModule.cs
@@ -8,17 +8,18 @@ public class
     RemoteStorageModule<TStorageOptions> : StorageModule<RemoteStorage<TStorageOptions>, TStorageOptions>
     where TStorageOptions : StorageOptions, IRemoteStorageOptions, new()
 {
-    public override string OptionsKey => $"Storage:FileSystem:{typeof(TStorageOptions).Name}";
+    public override string OptionsKey => $"Storage:Remote:{typeof(TStorageOptions).Name}";
 
     public override void ConfigureServices(IApplicationContext context, IServiceCollection services,
         TStorageOptions startupOptions)
     {
         base.ConfigureServices(context, services, startupOptions);
+        services.AddHttpClient<RemoteStorage<TStorageOptions>>();
         services.AddSingleton<IStorage<TStorageOptions>, RemoteStorage<TStorageOptions>>();
     }
 }
 
 public interface IRemoteStorageOptions
 {
-    public Uri UploadUrl { get; }
+    public Uri RemoteUrl { get; }
 }
