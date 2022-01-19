8a678516b3701beb821379ab0bc588eeba63457a
George Drak <sonicgd@gmail.com>
2022-01-19 12:55:14 +0500

    feat(remotestorage): empty storage
    Refs: SITKO-CORE-T-4

---
+++ src/Sitko.Core.Storage.Remote/RemoteStorage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Sitko.Core.Storage.Cache;
+using Sitko.Core.Storage.Metadata;
+
+namespace Sitko.Core.Storage.Remote;
+
+public class RemoteStorage<TStorageOptions> : Storage<TStorageOptions>
+    where TStorageOptions : StorageOptions, IRemoteStorageOptions
+{
+    public RemoteStorage(IOptionsMonitor<TStorageOptions> options, ILogger<RemoteStorage<TStorageOptions>> logger,
+        IStorageCache<TStorageOptions>? cache,
+        IStorageMetadataProvider<TStorageOptions>? metadataProvider) : base(options, logger,
+        cache, metadataProvider)
+    {
+    }
+
+    protected override async Task<bool> DoSaveAsync(string path, Stream file,
+        CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+    protected override async Task<bool> DoDeleteAsync(string filePath, CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+
+    protected override async Task<bool> DoIsFileExistsAsync(StorageItem item,
+        CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+    protected override async Task DoDeleteAllAsync(CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+}
