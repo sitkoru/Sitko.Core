85c89495e0775d5dac8cf60b935d39d595f393a8
George Drak <sonicgd@gmail.com>
2022-01-19 12:55:14 +0500

    feat(remotestorage): empty storage
    Refs: SITKO-CORE-T-4

---
+++ src/Sitko.Core.Storage.Remote/RemoteStorageModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Storage.Remote;
+
+public class
+    RemoteStorageModule<TStorageOptions> : StorageModule<RemoteStorage<TStorageOptions>, TStorageOptions>
+    where TStorageOptions : StorageOptions, IRemoteStorageOptions, new()
+{
+    public override string OptionsKey => $"Storage:FileSystem:{typeof(TStorageOptions).Name}";
+
+    public override void ConfigureServices(IApplicationContext context, IServiceCollection services,
+        TStorageOptions startupOptions)
+    {
+        base.ConfigureServices(context, services, startupOptions);
+        services.AddSingleton<IStorage<TStorageOptions>, RemoteStorage<TStorageOptions>>();
+    }
+}
+
+public interface IRemoteStorageOptions
+{
+    public Uri UploadUrl { get; }
+}
