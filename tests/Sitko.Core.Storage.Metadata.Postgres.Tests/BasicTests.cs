7d91e894db5d36722cf3dbe7b624e44edf3ae50a
George Drak <sonicgd@gmail.com>
2022-01-20 20:31:46 +0500

    style(storage): code style in tests
    Refs: SITKO-CORE-T-4

--- tests/Sitko.Core.Storage.Metadata.Postgres.Tests/BasicTests.cs
+++ tests/Sitko.Core.Storage.Metadata.Postgres.Tests/BasicTests.cs
@@ -4,38 +4,38 @@ using Sitko.Core.Storage.Tests;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Sitko.Core.Storage.Metadata.Postgres.Tests
+namespace Sitko.Core.Storage.Metadata.Postgres.Tests;
+
+public class BasicTests : BasicTests<BasePostgresStorageTestScope>
 {
-    public class BasicTests : BasicTests<BasePostgresStorageTestScope>
+    public BasicTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
     {
-        public BasicTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    }
+
+    [Fact]
+    public async Task Refresh()
+    {
+        var scope = await GetScopeAsync();
+        var storage = scope.GetService<IStorage<TestS3StorageSettings>>();
+        var metadataProvider = scope.GetService<IStorageMetadataProvider<TestS3StorageSettings>>();
+
+
+        StorageItem uploaded;
+        const string fileName = "file.txt";
+        var metaData = new FileMetaData();
+        await using (var file = File.Open("Data/file.txt", FileMode.Open))
         {
+            uploaded = await storage.SaveAsync(file, fileName, "upload/dir1/dir2", metaData);
         }
 
-        [Fact]
-        public async Task Refresh()
-        {
-            var scope = await GetScopeAsync();
-            var storage = scope.GetService<IStorage<TestS3StorageSettings>>();
-            var metadataProvider = scope.GetService<IStorageMetadataProvider<TestS3StorageSettings>>();
+        Assert.NotNull(uploaded);
 
+        await metadataProvider.DeleteAllMetadataAsync();
 
-            StorageItem uploaded;
-            const string fileName = "file.txt";
-            var metaData = new FileMetaData();
-            await using (var file = File.Open("Data/file.txt", FileMode.Open))
-            {
-                uploaded = await storage.SaveAsync(file, fileName, "upload/dir1/dir2", metaData);
-            }
+        var uploadDirectoryContent = await storage.GetDirectoryContentsAsync("upload");
+        Assert.Empty(uploadDirectoryContent);
 
-            Assert.NotNull(uploaded);
-
-            await metadataProvider.DeleteAllMetadataAsync();
-
-            var uploadDirectoryContent = await storage.GetDirectoryContentsAsync("upload");
-            Assert.Empty(uploadDirectoryContent);
-
-            await storage.RefreshDirectoryContentsAsync("upload");
+        await storage.RefreshDirectoryContentsAsync("upload");
 
         var restored =
             new StorageItem(uploaded.FilePath)
