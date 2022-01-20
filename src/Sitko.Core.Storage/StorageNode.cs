675ce1e74bcecb1cfa310e17b17951745d6a8c77
George Drak <sonicgd@gmail.com>
2022-01-20 20:25:15 +0500

    style(storage): code style
    Refs: SITKO-CORE-T-4

--- src/Sitko.Core.Storage/StorageNode.cs
+++ src/Sitko.Core.Storage/StorageNode.cs
@@ -2,135 +2,132 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using JetBrains.Annotations;
 using Sitko.Core.App.Helpers;
 
-namespace Sitko.Core.Storage
+namespace Sitko.Core.Storage;
+
+public sealed record StorageNode
 {
-    using JetBrains.Annotations;
+    private readonly List<StorageNode> children = new();
+    [PublicAPI] public string Name { get; set; } = string.Empty;
+    [PublicAPI] public string FullPath { get; set; } = string.Empty;
+    [PublicAPI] public DateTimeOffset LastModified { get; set; } = DateTimeOffset.UtcNow;
+    [PublicAPI] public long Size { get; set; }
+    [PublicAPI] public StorageNodeType Type { get; private set; }
+    public IEnumerable<StorageNode> Children => children.ToArray();
+    [PublicAPI] public StorageItem? StorageItem { get; private set; }
 
-    public sealed record StorageNode
+    [PublicAPI] public string HumanSize => FilesHelper.HumanSize(Size);
+
+    [PublicAPI]
+    public static StorageNode CreateDirectory(string name, string fullPath,
+        IEnumerable<StorageNode>? directoryChildren = null)
     {
-        [PublicAPI] public string Name { get; set; } = string.Empty;
-        [PublicAPI] public string FullPath { get; set; } = string.Empty;
-        [PublicAPI] public DateTimeOffset LastModified { get; set; } = DateTimeOffset.Now;
-        [PublicAPI] public long Size { get; set; }
-        [PublicAPI] public StorageNodeType Type { get; private set; }
-        private readonly List<StorageNode> children = new();
-        public IEnumerable<StorageNode> Children => children.ToArray();
-        [PublicAPI] public StorageItem? StorageItem { get; private set; }
-
-        [PublicAPI]
-        public static StorageNode CreateDirectory(string name, string fullPath,
-            IEnumerable<StorageNode>? directoryChildren = null)
+        var node = new StorageNode { Type = StorageNodeType.Directory, Name = name, FullPath = fullPath };
+        if (directoryChildren is not null)
         {
-            var node = new StorageNode {Type = StorageNodeType.Directory, Name = name, FullPath = fullPath};
-            if (directoryChildren is not null)
-            {
-                node.SetChildren(directoryChildren);
-            }
-
-            return node;
+            node.SetChildren(directoryChildren);
         }
 
-        [PublicAPI]
-        public static StorageNode CreateStorageItem(StorageItem storageItem) =>
-            new()
-            {
-                Type = StorageNodeType.StorageItem,
-                Name = storageItem.FileName!,
-                FullPath = storageItem.FilePath,
-                Size = storageItem.FileSize,
-                LastModified = storageItem.LastModified,
-                StorageItem = storageItem
-            };
-
-        private void AddChild(StorageNode child)
-        {
-            if (Type == StorageNodeType.Directory)
-            {
-                children.Add(child);
-                Size += child.Size;
-            }
-        }
-
-        [PublicAPI]
-        public void SetChildren(IEnumerable<StorageNode> directoryChildren)
-        {
-            var directoryChildrenArr = directoryChildren.ToArray();
-            if (Type == StorageNodeType.Directory && directoryChildrenArr.Length > 0)
-            {
-                children.Clear();
-                children.AddRange(directoryChildrenArr);
-                Size += directoryChildrenArr.Sum(s => s.Size);
-            }
-        }
-
-        [PublicAPI]
-        public void AddItem(StorageItem item)
-        {
-            var parts = item.FilePath.Split("/");
-            var current = this;
-            foreach (var part in parts)
-            {
-                if (part == parts.Last())
-                {
-                    current.AddChild(CreateStorageItem(item));
-                }
-                else
-                {
-                    var child = current.Children.Where(n => n.Type == StorageNodeType.Directory)
-                        .FirstOrDefault(f => f.Name == part);
-                    if (child == null)
-                    {
-                        child = CreateDirectory(part, PreparePath(Path.Combine(current.FullPath, part)));
-                        current.AddChild(child);
-                    }
-
-                    current = child;
-                }
-            }
-        }
-
-        [PublicAPI]
-        public void RemoveItem(StorageItem item) => RemoveItem(item.FilePath);
-
-        [PublicAPI]
-        public void RemoveItem(string filePath)
-        {
-            var parts = filePath.Split("/");
-            var current = this;
-            foreach (var part in parts)
-            {
-                if (part == parts.Last())
-                {
-                    var child = current.children.FirstOrDefault(c =>
-                        c.Type == StorageNodeType.StorageItem && c.StorageItem!.FilePath == filePath);
-                    if (child != null)
-                    {
-                        current.children.Remove(child);
-                    }
-                }
-                else
-                {
-                    var child = current.Children.Where(n => n.Type == StorageNodeType.Directory)
-                        .FirstOrDefault(f => f.Name == part);
-                    if (child == null)
-                    {
-                        return;
-                    }
-                }
-            }
-        }
-
-        private string PreparePath(string path) => path.Replace("\\", "/").Replace("//", "/");
-
-        [PublicAPI] public string HumanSize => FilesHelper.HumanSize(Size);
+        return node;
     }
 
+    [PublicAPI]
+    public static StorageNode CreateStorageItem(StorageItem storageItem) =>
+        new()
+        {
+            Type = StorageNodeType.StorageItem,
+            Name = storageItem.FileName!,
+            FullPath = storageItem.FilePath,
+            Size = storageItem.FileSize,
+            LastModified = storageItem.LastModified,
+            StorageItem = storageItem
+        };
 
-    public enum StorageNodeType
+    private void AddChild(StorageNode child)
     {
-        Directory,
-        StorageItem
+        if (Type == StorageNodeType.Directory)
+        {
+            children.Add(child);
+            Size += child.Size;
+        }
     }
+
+    [PublicAPI]
+    public void SetChildren(IEnumerable<StorageNode> directoryChildren)
+    {
+        var directoryChildrenArr = directoryChildren.ToArray();
+        if (Type == StorageNodeType.Directory && directoryChildrenArr.Length > 0)
+        {
+            children.Clear();
+            children.AddRange(directoryChildrenArr);
+            Size += directoryChildrenArr.Sum(s => s.Size);
+        }
+    }
+
+    [PublicAPI]
+    public void AddItem(StorageItem item)
+    {
+        var parts = item.FilePath.Split("/");
+        var current = this;
+        foreach (var part in parts)
+        {
+            if (part == parts.Last())
+            {
+                current.AddChild(CreateStorageItem(item));
+            }
+            else
+            {
+                var child = current.Children.Where(n => n.Type == StorageNodeType.Directory)
+                    .FirstOrDefault(f => f.Name == part);
+                if (child == null)
+                {
+                    child = CreateDirectory(part, PreparePath(Path.Combine(current.FullPath, part)));
+                    current.AddChild(child);
+                }
+
+                current = child;
+            }
+        }
+    }
+
+    [PublicAPI]
+    public void RemoveItem(StorageItem item) => RemoveItem(item.FilePath);
+
+    [PublicAPI]
+    public void RemoveItem(string filePath)
+    {
+        var parts = filePath.Split("/");
+        var current = this;
+        foreach (var part in parts)
+        {
+            if (part == parts.Last())
+            {
+                var child = current.children.FirstOrDefault(c =>
+                    c.Type == StorageNodeType.StorageItem && c.StorageItem!.FilePath == filePath);
+                if (child != null)
+                {
+                    current.children.Remove(child);
+                }
+            }
+            else
+            {
+                var child = current.Children.Where(n => n.Type == StorageNodeType.Directory)
+                    .FirstOrDefault(f => f.Name == part);
+                if (child == null)
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    private string PreparePath(string path) => path.Replace("\\", "/").Replace("//", "/");
+}
+
+public enum StorageNodeType
+{
+    Directory,
+    StorageItem
 }
