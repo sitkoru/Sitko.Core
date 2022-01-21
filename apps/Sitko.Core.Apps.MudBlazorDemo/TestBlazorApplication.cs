81c00eb680d490f8e6ee365967ee14d590dcae14
George Drak <sonicgd@gmail.com>
2022-01-21 10:56:55 +0500

    feat(demo): use remote storage
    Refs: SITKO-CORE-T-4

--- apps/Sitko.Core.Apps.MudBlazorDemo/TestBlazorApplication.cs
+++ apps/Sitko.Core.Apps.MudBlazorDemo/TestBlazorApplication.cs
@@ -1,10 +1,13 @@
-﻿using Sitko.Core.App.Localization;
+﻿using System;
+using Sitko.Core.App.Localization;
 using Sitko.Core.Apps.MudBlazorDemo.Data;
 using Sitko.Core.Blazor.MudBlazor.Server;
 using Sitko.Core.Db.Postgres;
 using Sitko.Core.Repository.EntityFrameworkCore;
+using Sitko.Core.Storage;
 using Sitko.Core.Storage.FileSystem;
 using Sitko.Core.Storage.Metadata.Postgres;
+using Sitko.Core.Storage.Remote;
 
 namespace Sitko.Core.Apps.MudBlazorDemo;
 
@@ -16,7 +19,15 @@ public class TestBlazorApplication : MudBlazorApplication<Startup>
                 options.EnableSensitiveLogging = true;
             })
             .AddEFRepositories<BarContext>()
+            // backend storage, in wasm it should be in server project
             .AddFileSystemStorage<TestBlazorStorageOptions>()
+            // frontend storage, in wasm it should be in client project
+            .AddRemoteStorage<TestRemoteStorageOptions>()
             .AddPostgresStorageMetadata<TestBlazorStorageOptions>()
             .AddJsonLocalization();
+}
+
+public class TestRemoteStorageOptions : StorageOptions, IRemoteStorageOptions
+{
+    public Uri RemoteUrl { get; set; } = new("https://localhost");
 }
