c9a8256b912c4c7ccc8a8b592a14092d75e7e37d
George Drak <sonicgd@gmail.com>
2022-01-21 10:48:59 +0500

    feat(remotestorage): make access check methods virtual with default implementation
    Refs: SITKO-CORE-T-4

--- tests/Sitko.Core.Storage.Remote.Tests/Server/UploadController.cs
+++ tests/Sitko.Core.Storage.Remote.Tests/Server/UploadController.cs
@@ -1,8 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Sitko.Core.App.Results;
 using Sitko.Core.Storage.Remote.Server;
 using Sitko.Core.Storage.Tests;
 
@@ -15,17 +12,4 @@ public class UploadController : BaseRemoteStorageController<TestS3StorageSetting
         storage, logger)
     {
     }
-
-    protected override async Task<IOperationResult> CanReadAsync(string path, HttpRequest request) =>
-        new OperationResult();
-
-    protected override async Task<IOperationResult> CanDeleteAsync(string? path, HttpRequest request) =>
-        new OperationResult();
-
-    protected override async Task<IOperationResult> CanListAsync(string? path, HttpRequest request) =>
-        new OperationResult();
-
-    protected override async Task<IOperationResult>
-        CanUploadAsync(UploadStorageItem<FileMetaData> uploadStorageItem, HttpRequest request) =>
-        new OperationResult();
 }
