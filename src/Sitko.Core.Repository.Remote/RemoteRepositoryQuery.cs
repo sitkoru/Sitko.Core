28c8c70dfb8875c2c520ba5fb020d62166618c07
Alexander <zubkov_95@mail.ru>
2022-01-28 02:04:07 +0500

    feat: add queries for remote repo Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/RemoteRepositoryQuery.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+
+namespace Sitko.Core.Repository.Remote;
+
+internal class RemoteRepositoryQuerySource<TEntity> where TEntity : class
+{
+    public RemoteRepositoryQuerySource(IQueryable<TEntity> query) => Query = query;
+    internal IQueryable<TEntity> Query { get; set; }
+}
+
+public class RemoteRepositoryQuery<TEntity> : BaseRepositoryQuery<TEntity> where TEntity : class
+{
+    private List<Expression<Func<TEntity, bool>>> whereExpressions = new();
+    private List<Expression<Func<TEntity, object>>> orderByExpressions = new();
+    private List<Expression<Func<TEntity, object>>> orderByDescendingExpressions = new();
+
+    public override IRepositoryQuery<TEntity> Where(Expression<Func<TEntity, bool>> @where)
+    {
+        whereExpressions.Add(where);
+        return this;
+    }
+
+    public override IRepositoryQuery<TEntity> Where(Func<IQueryable<TEntity>, IQueryable<TEntity>> @where) => throw new NotImplementedException();
+
+    public override IRepositoryQuery<TEntity> Where(string whereStr, object?[] values) => throw new NotImplementedException();
+
+    public override IRepositoryQuery<TEntity> OrderByDescending(Expression<Func<TEntity, object>> orderBy)
+    {
+        orderByExpressions.Add(orderBy);
+        return this;
+    }
+
+    public override IRepositoryQuery<TEntity> OrderBy(Expression<Func<TEntity, object>> orderBy)
+    {
+        orderByDescendingExpressions.Add(orderBy);
+        return this;
+    }
+
+    public override IRepositoryQuery<TEntity> Order(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> order) => throw new NotImplementedException();
+
+    public override IRepositoryQuery<TEntity> Configure(Action<IRepositoryQuery<TEntity>>? configureQuery = null) => throw new NotImplementedException();
+
+    public override Task<IRepositoryQuery<TEntity>> ConfigureAsync(Func<IRepositoryQuery<TEntity>, Task>? configureQuery = null, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+    public override IIncludableRepositoryQuery<TEntity, TProperty> Include<TProperty>(Expression<Func<TEntity, TProperty>> navigationPropertyPath) => throw new NotImplementedException();
+
+    protected override void ApplySort((string propertyName, bool isDescending) sortQuery)
+    {
+        if (sortQuery.isDescending)
+        {
+            orderByDescendingExpressions.Add(entity => sortQuery.propertyName);
+        }
+    }
+
+    public SerializedQuery<TEntity>
+        Serialize() =>
+        new SerializedQuery<TEntity>(whereExpressions,
+            orderByExpressions,
+            orderByDescendingExpressions, Limit, Offset);
+}
---
+++ src/Sitko.Core.Repository.Remote/SerializedQuery.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq.Expressions;
+using Newtonsoft.Json;
+using Serialize.Linq.Serializers;
+using JsonSerializer = Serialize.Linq.Serializers.JsonSerializer;
+
+namespace Sitko.Core.Repository.Remote;
+
+public record SerializedQuery<TEntity> where TEntity : class
+{
+    private ExpressionSerializer _serializer = new ExpressionSerializer(new JsonSerializer());
+
+    [JsonConstructor]
+    public SerializedQuery()
+    {
+
+    }
+
+    public SerializedQuery(List<Expression<Func<TEntity, bool>>> where, List<Expression<Func<TEntity, object>>> orderBy,
+        List<Expression<Func<TEntity, object>>> orderByDescending, int? limit, int? offset)
+    {
+        Where = where.Select(node => _serializer.SerializeText(node)).ToList();
+        OrderBy = orderBy.Select(node => _serializer.SerializeText(node)).ToList();
+        OrderByDescending = orderByDescending.Select(node => _serializer.SerializeText(node)).ToList();
+        Limit = limit;
+        Offset = offset;
+    }
+
+    public IRepositoryQuery<TEntity> Apply(IRepositoryQuery<TEntity> query)
+    {
+        foreach (var expressionNode in Where)
+        {
+            var ex = _serializer.DeserializeText(expressionNode);
+            query = query.Where((Expression<Func<TEntity, bool>>)ex);
+        }
+
+        foreach (var expressionNode in OrderBy)
+        {
+            var ex = _serializer.DeserializeText(expressionNode);
+            query = query.OrderBy((Expression<Func<TEntity, object>>)ex);
+        }
+
+        foreach (var expressionNode in OrderByDescending)
+        {
+            var ex = _serializer.DeserializeText(expressionNode);
+            query = query.OrderByDescending((Expression<Func<TEntity, object>>)ex);
+        }
+
+        if (Limit > 0)
+        {
+            query = query.Take(Limit.Value);
+        }
+
+        if (Offset > 0)
+        {
+            query = query.Skip(Offset.Value);
+        }
+
+        return query;
+    }
+
+    public List<string> Where { get; set; }
+    public List<string> OrderBy { get; set; }
+    public List<string> OrderByDescending { get; set; }
+    public int? Limit { get; set; }
+    public int? Offset { get; set; }
+}
