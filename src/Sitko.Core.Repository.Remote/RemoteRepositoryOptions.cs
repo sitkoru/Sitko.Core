1d394bfcee254f2644ff00aacbee780bf789599d
Alexander <zubkov_95@mail.ru>
2022-01-28 02:04:23 +0500

    feat: add repository options Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/RemoteRepositoryOptions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using JetBrains.Annotations;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Repository.Remote;
+
+public class RemoteRepositoryOptions : BaseModuleOptions
+{
+    protected RemoteRepositoryOptions() => Name = GetType().Name;
+
+    [PublicAPI] public string RepositoryControllerApiRoute { get; set; }
+    [PublicAPI] public string Name { get; set; }
+}
+public abstract class RepositoryOptionsValidator<TRepositoryOptions> : AbstractValidator<TRepositoryOptions>
+    where TRepositoryOptions : RemoteRepositoryOptions
+{
+    public RepositoryOptionsValidator() => RuleFor(o => o.Name).NotEmpty()
+        .WithMessage($"Repository {typeof(TRepositoryOptions)} name is empty");
+}
