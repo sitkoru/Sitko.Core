282c36045a8c9cdf541428356e0165fe44c75e47
Alexander <zubkov_95@mail.ru>
2022-01-28 02:02:57 +0500

    feat: add repository transport interface Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/IRemoteRepositoryTransport.cs
@@ -0,0 +1,8 @@
+﻿namespace Sitko.Core.Repository.Remote;
+
+public interface IRemoteRepositoryTransport
+{
+     Task<T?> SendAsync<T>(CancellationToken cancellationToken = default);
+     Task<int> DeleteAsync<TEntityPK>();
+     Task<(TEntity[] items, int itemsCount)> GetAllAsync<TEntity>(SerializedQuery<TEntity> query, CancellationToken cancellationToken = default) where TEntity : class;
+}
