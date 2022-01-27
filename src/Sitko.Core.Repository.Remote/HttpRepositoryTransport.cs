1cd9b451d56c6aada6594049d2f0b9f96f8b376f
Alexander <zubkov_95@mail.ru>
2022-01-28 02:03:35 +0500

    feat: add http repository transport Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/HttpRepositoryTransport.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using Sitko.Core.App.Json;
+
+namespace Sitko.Core.Repository.Remote;
+
+public class HttpRepositoryTransport : IRemoteRepositoryTransport
+{
+    private readonly IHttpClientFactory httpClientFactory;
+    public HttpRepositoryTransport(IHttpClientFactory httpClientFactory) => this.httpClientFactory = httpClientFactory;
+
+    private HttpClient HttpClient
+    {
+        get
+        {
+            var client = httpClientFactory.CreateClient();
+            //client.BaseAddress = ;
+            return client;
+        }
+    }
+
+    public async Task<(TEntity[] items, int itemsCount)> GetAllAsync<TEntity>(SerializedQuery<TEntity> query,
+        CancellationToken cancellationToken = default) where TEntity : class
+    {
+        var json = JsonSerializer.Serialize(query);
+        var result = await HttpClient.GetAsync(json, cancellationToken);
+
+        if (!result.IsSuccessStatusCode)
+        {
+            throw new Exception();
+        }
+
+        return JsonSerializer.Deserialize<(TEntity[], int)>(result.Content.ToString());
+    }
+
+
+    public Task<T?> SendAsync<T>(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<int> DeleteAsync<T>()
+    {
+        throw new NotImplementedException();
+    }
+}
