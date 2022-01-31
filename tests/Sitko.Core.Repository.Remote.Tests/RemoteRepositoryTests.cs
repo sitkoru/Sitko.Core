65171b9575dcbf2fba087ef86891ac8b2ec0ae1f
Alexander <zubkov_95@mail.ru>
2022-01-31 21:01:53 +0500

    feat: add remote repo add test blank

--- tests/Sitko.Core.Repository.Remote.Tests/RemoteRepositoryTests.cs
+++ tests/Sitko.Core.Repository.Remote.Tests/RemoteRepositoryTests.cs
@@ -1,14 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Sitko.Core.Repository.Tests;
-using Sitko.Core.Xunit;
+using Sitko.Core.Repository.Tests.Data;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Sitko.Core.Repository.Remote.Tests;
 
-public class RemoteRepositoryTests : BaseTest<>
+public class RemoteRepositoryTests : BasicRepositoryTests<RemoteRepositoryTestScope>
 {
-    public RemoteRepositoryTests([NotNull] ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    public RemoteRepositoryTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
     {
+    }
+
+    [Fact]
+    public async Task Add()
+    {
+        var scope = await GetScopeAsync();
+
+        var repository = scope.GetService<BaseRemoteRepository<TestModel, Guid>>();
+
+        var item = await repository.GetAsync();
+
+        Assert.NotNull(item);
+        var oldValue = item!.Status;
+        item.Status = TestStatus.Disabled;
+
+        var result = await repository.UpdateAsync(item);
+        Assert.True(result.IsSuccess);
+        Assert.Empty(result.Errors);
+        Assert.NotEmpty(result.Changes);
+        Assert.NotEmpty(result.Changes.Where(c => c.Name == nameof(item.Status)));
+        var change = result.Changes.First(c => c.Name == nameof(item.Status));
+        Assert.Equal(oldValue, change.OriginalValue);
+        Assert.Equal(item.Status, change.CurrentValue);
+        await repository.RefreshAsync(item);
+        Assert.Equal(TestStatus.Disabled, item.Status);
     }
 }
