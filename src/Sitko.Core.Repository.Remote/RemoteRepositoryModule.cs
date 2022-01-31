370b31dc2922d3c9961d6a3c48dedb1b44678f6d
Alexander <zubkov_95@mail.ru>
2022-01-31 20:59:45 +0500

    feat: add modules to remote repo Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/RemoteRepositoryModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Sitko.Core.App;
+
+[assembly: InternalsVisibleTo("Sitko.Core.Repository.Remote.Tests")]
+
+namespace Sitko.Core.Repository.Remote;
+
+public class RemoteRepositoryModule : RepositoriesModule<RemoteRepositoryModuleOptions, IRemoteRepository>
+{
+    public override string OptionsKey => "Repositories:Remote";
+
+    public override void ConfigureServices(IApplicationContext context, IServiceCollection services,
+        RemoteRepositoryModuleOptions startupOptions)
+    {
+        base.ConfigureServices(context, services, startupOptions);
+        services.AddHttpClient<>();
+    }
+}
+
+public class RemoteRepositoryModuleOptions : RepositoriesModuleOptions<IRemoteRepository>
+{
+}
