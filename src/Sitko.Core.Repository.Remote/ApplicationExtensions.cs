a483da79cab45e1bf9097eb276fad6c5dfece75d
Alexander <zubkov_95@mail.ru>
2022-01-31 20:59:25 +0500

    feat: add extensions method to remote repo Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/ApplicationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Repository.Remote;
+
+[PublicAPI]
+public static class ApplicationExtensions
+{
+
+    public static Application AddRemoteRepositories(this Application application,
+        Action<IApplicationContext, RemoteRepositoryModuleOptions> configure,
+        string? optionsKey = null) =>
+        application.AddModule<RemoteRepositoryOptions, RemoteRepositoryModuleOptions>(configure, optionsKey);
+
+    public static Application AddRemoteRepositories(this Application application,
+        Action<RemoteRepositoryModuleOptions>? configure = null,
+        string? optionsKey = null) =>
+        application.AddModule<EFRepositoriesModule, RemoteRepositoryModuleOptions>(configure, optionsKey);
+
+    public static Application AddRemoteRepositories<TAssembly>(this Application application,
+        Action<IApplicationContext, RemoteRepositoryModuleOptions> configure,
+        string? optionsKey = null) =>
+        application.AddModule<EFRepositoriesModule, RemoteRepositoryModuleOptions>(
+            (applicationContext, moduleOptions) =>
+            {
+                moduleOptions.AddRepositoriesFromAssemblyOf<TAssembly>();
+                configure(applicationContext, moduleOptions);
+            },
+            optionsKey);
+
+    public static Application AddRemoteRepositories<TAssembly>(this Application application,
+        Action<RemoteRepositoryModuleOptions>? configure = null,
+        string? optionsKey = null) =>
+        application.AddModule<EFRepositoriesModule, RemoteRepositoryModuleOptions>(moduleOptions =>
+            {
+                moduleOptions.AddRepositoriesFromAssemblyOf<TAssembly>();
+                configure?.Invoke(moduleOptions);
+            },
+            optionsKey);
+}
