e320242d4e38309705421d0e1a28ebf3b7a0c617
Alexander <zubkov_95@mail.ru>
2022-01-31 21:04:23 +0500

    feat: add remote repo test scope Refs: SITKO-CORE-T-8

--- tests/Sitko.Core.Repository.Remote.Tests/RemoteRepositoryTestScope.cs
+++ tests/Sitko.Core.Repository.Remote.Tests/RemoteRepositoryTestScope.cs
@@ -1,6 +1,25 @@
-﻿namespace Sitko.Core.Repository.Remote.Tests;
+﻿using System;
+using Sitko.Core.Db.Postgres;
+using Sitko.Core.Repository.Tests.Data;
+using Sitko.Core.Xunit;
+using Sitko.Core.Xunit.Web;
 
-public class RemoteRepositoryTestScope :
+namespace Sitko.Core.Repository.Remote.Tests;
+
+public class RemoteRepositoryTestScope : WebTestScope
 {
+    protected override WebTestApplication ConfigureWebApplication(WebTestApplication application, string name)
+    {
+        base.ConfigureWebApplication(application, name);
+        application.AddPostgresDatabase<TestDbContext>();
+        //application.add
+        return application;
+    }
 
+    protected override TestApplication ConfigureApplication(TestApplication application, string name)
+    {
+        base.ConfigureApplication(application, name);
+        application.AddRemoteRepositories<>();
+        return application;
+    }
 }
