cbeab71e9bdf1e9f667b58dc25d88c6be49eccf9
Alexander <zubkov_95@mail.ru>
2022-02-02 18:49:43 +0500

    feat(tests): add server side controller Refs: SITKO-CORE-T-8

---
+++ tests/Sitko.Core.Repository.Remote.Tests/Server/RemoteController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Sitko.Core.Repository.Remote.Server;
+using Sitko.Core.Repository.Tests.Data;
+
+namespace Sitko.Core.Repository.Remote.Tests.Server;
+[Route("/test")]
+public class RemoteController : BaseRemoteRepositoryController<TestModel,Guid>
+{
+    public RemoteController(IRepository<TestModel, Guid> repository) : base(repository)
+    {
+    }
+}
