9b00b89d5d1700e21c85e3b68b0773132a66a859
Alexander <zubkov_95@mail.ru>
2022-02-02 18:53:29 +0500

    fix: change options module inheritance Refs: SITKO-CORE-T-8

--- src/Sitko.Core.Repository.Remote/RemoteRepositoryOptions.cs
+++ src/Sitko.Core.Repository.Remote/RemoteRepositoryOptions.cs
@@ -4,15 +4,15 @@ using Sitko.Core.App;
 
 namespace Sitko.Core.Repository.Remote;
 
-public class RemoteRepositoryOptions : BaseModuleOptions
+public class RemoteRepositoryOptions : RepositoriesModuleOptions<IRemoteRepository>
 {
     public Func<HttpClient>? HttpClientFactory { get; set; }
     [PublicAPI] public Uri RepositoryControllerApiRoute { get; set; } = new("http://localhost");
     [PublicAPI] public string Name { get; set; } = string.Empty;
 }
-public abstract class RepositoryOptionsValidator<TRepositoryOptions> : AbstractValidator<TRepositoryOptions>
-    where TRepositoryOptions : RemoteRepositoryOptions
-{
-    public RepositoryOptionsValidator() => RuleFor(o => o.Name).NotEmpty()
-        .WithMessage($"Repository {typeof(TRepositoryOptions)} name is empty");
-}
+// public abstract class RepositoryOptionsValidator<TRepositoryOptions> : AbstractValidator<TRepositoryOptions>
+//     where TRepositoryOptions : RemoteRepositoryOptions
+// {
+//     public RepositoryOptionsValidator() => RuleFor(o => o.Name).NotEmpty()
+//         .WithMessage($"Repository {typeof(TRepositoryOptions)} name is empty");
+// }
