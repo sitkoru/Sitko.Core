7cf8453922ff1b623764a76d91343fb1028b53aa
Alexander <zubkov_95@mail.ru>
2022-02-02 22:57:06 +0500

    feat: remote controller now have EfRepo DI Refs: SITKO-CORE-T-8

--- tests/Sitko.Core.Repository.Remote.Tests/Server/RemoteController.cs
+++ tests/Sitko.Core.Repository.Remote.Tests/Server/RemoteController.cs
@@ -2,13 +2,15 @@
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
 using Sitko.Core.Repository.Remote.Server;
+using Sitko.Core.Repository.Remote.Tests.Data;
 using Sitko.Core.Repository.Tests.Data;
 
 namespace Sitko.Core.Repository.Remote.Tests.Server;
+
 [Route("/test")]
 public class RemoteController : BaseRemoteRepositoryController<TestModel,Guid>
 {
-    public RemoteController(IRepository<TestModel, Guid> repository) : base(repository)
+    public RemoteController(TestEFRepository repository) : base(repository)
     {
     }
 }
