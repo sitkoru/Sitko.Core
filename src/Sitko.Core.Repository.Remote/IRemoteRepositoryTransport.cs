f909fcc3cadff80118d749f7187fc538cc3fa5d7
Alexander <zubkov_95@mail.ru>
2022-02-04 20:09:11 +0500

    feat: expand remote transport interface Refs: SITKO-CORE-T-8

--- src/Sitko.Core.Repository.Remote/IRemoteRepositoryTransport.cs
+++ src/Sitko.Core.Repository.Remote/IRemoteRepositoryTransport.cs
@@ -1,8 +1,33 @@
-﻿namespace Sitko.Core.Repository.Remote;
+﻿using System.Linq.Expressions;
+
+namespace Sitko.Core.Repository.Remote;
 
 public interface IRemoteRepositoryTransport
 {
-     Task<T?> SendAsync<T>(CancellationToken cancellationToken = default);
-     Task<int> DeleteAsync<TEntityPK>();
-     Task<(TEntity[] items, int itemsCount)> GetAllAsync<TEntity>(SerializedQuery<TEntity> query, CancellationToken cancellationToken = default) where TEntity : class;
+    Task<TEntity?> GetAsync<TEntity>(SerializedQuery<TEntity> query,
+        CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<int> CountAsync<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<int> CountAsync<TEntity>(SerializedQuery<TEntity> configureQuery,
+        CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<int> SumAsync<TEntity>(SerializedQuery<TEntity> configureQuery, CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<AddOrUpdateOperationResult<TEntity, TEntityPk>> AddAsync<TEntity, TEntityPk>(TEntity entity,
+        CancellationToken cancellationToken = default) where TEntity : class, IEntity<TEntityPk>;
+
+    Task<AddOrUpdateOperationResult<TEntity, TEntityPk>[]> AddAsync<TEntity, TEntityPk>(IEnumerable<TEntity> entities,
+        CancellationToken cancellationToken = default) where TEntity : class, IEntity<TEntityPk>;
+
+    Task<PropertyChange[]> UpdateAsync<TEntity>(TEntity entity, TEntity? oldEntity,
+        CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<bool> DeleteAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<(TEntity[] items, int itemsCount)> GetAllAsync<TEntity>(SerializedQuery<TEntity> query,
+        CancellationToken cancellationToken = default) where TEntity : class;
+
+    Task<(TEntity[] items, int itemsCount)> GetAllAsync<TEntity>(CancellationToken cancellationToken = default)
+        where TEntity : class;
 }
