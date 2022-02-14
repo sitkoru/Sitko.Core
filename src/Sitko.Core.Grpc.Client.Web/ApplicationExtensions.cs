91e35dab2cfc9133d2f89d78ac33bcfb1773ef57
George Drak <sonicgd@gmail.com>
2022-02-14 14:25:26 +0500

    feat(grpc): add grpc web wrapper module

---
+++ src/Sitko.Core.Grpc.Client.Web/ApplicationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Grpc.Core;
+using Grpc.Net.Client.Web;
+using JetBrains.Annotations;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Grpc.Client.Web;
+
+[PublicAPI]
+public static class ApplicationExtensions
+{
+    public static Application AddGrpcWebClient<TClient>(this Application application,
+        Action<IApplicationContext, GrpcWebClientModuleOptions<TClient>> configure,
+        string? optionsKey = null)
+        where TClient : ClientBase<TClient> =>
+        application.AddModule<GrpcWebClientModule<TClient>, GrpcWebClientModuleOptions<TClient>>((context, options) =>
+            {
+                options.ConfigureHttpHandler = handler => new GrpcWebHandler(handler);
+                configure(context, options);
+            },
+            optionsKey);
+
+    public static Application AddGrpcWebClient<TClient>(this Application application,
+        Action<GrpcWebClientModuleOptions<TClient>>? configure = null,
+        string? optionsKey = null)
+        where TClient : ClientBase<TClient> =>
+        application.AddModule<GrpcWebClientModule<TClient>, GrpcWebClientModuleOptions<TClient>>(options =>
+            {
+                options.ConfigureHttpHandler = handler => new GrpcWebHandler(handler);
+                configure?.Invoke(options);
+            },
+            optionsKey);
+}
