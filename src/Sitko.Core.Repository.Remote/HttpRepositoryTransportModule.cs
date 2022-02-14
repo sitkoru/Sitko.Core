624773b0d6377143ba388cb35ca0f94841109867
Alexander <zubkov_95@mail.ru>
2022-02-14 18:31:15 +0500

    feat: create standalone http transport module Refs: SITKO-CORE-T-8

---
+++ src/Sitko.Core.Repository.Remote/HttpRepositoryTransportModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Repository.Remote;
+
+public class HttpRepositoryTransportModule : BaseApplicationModule<HttpRepositoryTransportOptions>
+{
+    public override string OptionsKey => "Repositories:Remote:Transports:Http";
+
+    public override void ConfigureServices(IApplicationContext context, IServiceCollection services,
+        HttpRepositoryTransportOptions startupOptions)
+    {
+        base.ConfigureServices(context, services, startupOptions);
+        services.AddHttpClient();
+        services.AddSingleton<IRemoteRepositoryTransport, HttpRepositoryTransport>();
+    }
+}
