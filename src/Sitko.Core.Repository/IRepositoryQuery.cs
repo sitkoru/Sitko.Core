625c1b0131f78a4bcd572363b407680346aff56a
George Drak <sonicgd@gmail.com>
2022-02-17 02:42:52 +0500

    fix(remoterepository): don't need those select methods, reformat code
    Refs: SITKO-CORE-T-8

--- src/Sitko.Core.Repository/IRepositoryQuery.cs
+++ src/Sitko.Core.Repository/IRepositoryQuery.cs
@@ -4,49 +4,47 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
-namespace Sitko.Core.Repository
+namespace Sitko.Core.Repository;
+
+[PublicAPI]
+public interface IRepositoryQuery<TEntity> where TEntity : class
 {
-    using JetBrains.Annotations;
+    public int? Limit { get; }
+    public int? Offset { get; }
+    IRepositoryQuery<TEntity> Take(int take);
+    IRepositoryQuery<TEntity> Skip(int skip);
+    IRepositoryQuery<TEntity> Where(Expression<Func<TEntity, bool>> where);
+    IRepositoryQuery<TEntity> Where(Func<IQueryable<TEntity>, IQueryable<TEntity>> where);
+    IRepositoryQuery<TEntity> Where(string whereStr, object[] values);
+    IRepositoryQuery<TEntity> Where(string property, object value);
+    IRepositoryQuery<TEntity> Where(QueryContextCondition condition);
+    IRepositoryQuery<TEntity> Where(QueryContextConditionsGroup conditionsGroup);
+    IRepositoryQuery<TEntity> Where(IEnumerable<QueryContextConditionsGroup> conditionsGroups);
+    IRepositoryQuery<TEntity> Where(params QueryContextConditionsGroup[] conditionsGroups);
+    IRepositoryQuery<TEntity> Like(string property, object value);
+    IRepositoryQuery<TEntity> WhereByString(string whereJson);
+    IRepositoryQuery<TEntity> OrderByDescending(Expression<Func<TEntity, object>> orderBy);
+    IRepositoryQuery<TEntity> OrderBy(Expression<Func<TEntity, object>> orderBy);
+    IRepositoryQuery<TEntity> OrderBy(string property, bool isDescending);
+    IRepositoryQuery<TEntity> OrderByString(string orderBy);
+    IRepositoryQuery<TEntity> Order(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> order);
+    IRepositoryQuery<TEntity> Configure(Action<IRepositoryQuery<TEntity>>? configureQuery = null);
 
-    [PublicAPI]
-    public interface IRepositoryQuery<TEntity> where TEntity : class
-    {
-        public int? Limit { get; }
-        public int? Offset { get; }
-        IRepositoryQuery<TEntity> Take(int take);
-        IRepositoryQuery<TEntity> Skip(int skip);
-        IRepositoryQuery<TEntity> Where(Expression<Func<TEntity, bool>> where);
-        IRepositoryQuery<TEntity> Where(Func<IQueryable<TEntity>, IQueryable<TEntity>> where);
-        IRepositoryQuery<TEntity> Where(string whereStr, object[] values);
-        IRepositoryQuery<TEntity> Where(string property, object value);
-        IRepositoryQuery<TEntity> Where(QueryContextCondition condition);
-        IRepositoryQuery<TEntity> Where(QueryContextConditionsGroup conditionsGroup);
-        IRepositoryQuery<TEntity> Where(IEnumerable<QueryContextConditionsGroup> conditionsGroups);
-        IRepositoryQuery<TEntity> Where(params QueryContextConditionsGroup[] conditionsGroups);
-        IRepositoryQuery<TEntity> Like(string property, object value);
-        IRepositoryQuery<TEntity> WhereByString(string whereJson);
-        IRepositoryQuery<TEntity> OrderByDescending(Expression<Func<TEntity, object>> orderBy);
-        IRepositoryQuery<TEntity> OrderBy(Expression<Func<TEntity, object>> orderBy);
-        IRepositoryQuery<TEntity> OrderBy(string property, bool isDescending);
-        IRepositoryQuery<TEntity> OrderByString(string orderBy);
-        IRepositoryQuery<TEntity> Order(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> order);
-        IRepositoryQuery<TEntity> Configure(Action<IRepositoryQuery<TEntity>>? configureQuery = null);
+    Task<IRepositoryQuery<TEntity>> ConfigureAsync(Func<IRepositoryQuery<TEntity>, Task>? configureQuery = null,
+        CancellationToken cancellationToken = default);
 
-        Task<IRepositoryQuery<TEntity>> ConfigureAsync(Func<IRepositoryQuery<TEntity>, Task>? configureQuery = null,
-            CancellationToken cancellationToken = default);
+    IRepositoryQuery<TEntity> Paginate(int page, int itemsPerPage);
 
-        IRepositoryQuery<TEntity> Paginate(int page, int itemsPerPage);
-        IRepositoryQuery<TEntity> Select(Expression<Func<TEntity, int>> intSelect);
-        IRepositoryQuery<TEntity> Select(Expression<Func<TEntity, long>> longSelect);
-        IIncludableRepositoryQuery<TEntity, TProperty> Include<TProperty>(
-            Expression<Func<TEntity, TProperty>> navigationPropertyPath);
-        IRepositoryQuery<TEntity> Include(string navigationPropertyPath);
+    IIncludableRepositoryQuery<TEntity, TProperty> Include<TProperty>(
+        Expression<Func<TEntity, TProperty>> navigationPropertyPath);
 
-    }
+    IRepositoryQuery<TEntity> Include(string navigationPropertyPath);
+}
 
-    // ReSharper disable once UnusedTypeParameter
-    public interface IIncludableRepositoryQuery<TEntity, out TProperty> : IRepositoryQuery<TEntity>
-        where TEntity : class
-    {}
+// ReSharper disable once UnusedTypeParameter
+public interface IIncludableRepositoryQuery<TEntity, out TProperty> : IRepositoryQuery<TEntity>
+    where TEntity : class
+{
 }
