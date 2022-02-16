296e6a37b919ea15503d8fcca4cde79133747e52
George Drak <sonicgd@gmail.com>
2022-02-17 02:36:14 +0500

    tests(remoterepository): use types from correct namespace
    Refs: SITKO-CORE-T-8

--- tests/Sitko.Core.Repository.Remote.Tests/Server/BarController.cs
+++ tests/Sitko.Core.Repository.Remote.Tests/Server/BarController.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using Sitko.Core.Repository.Remote.Server;
-using Sitko.Core.Repository.Remote.Tests.Data;
 using Sitko.Core.Repository.Tests.Data;
 
 namespace Sitko.Core.Repository.Remote.Tests.Server;
 
 [Route("/api/BarModel")]
-public class BarController : BaseRemoteRepositoryController<BarModel,Guid>
+public class BarController : BaseRemoteRepositoryController<BarModel, Guid>
 {
     public BarController(BarEFRepository repository) : base(repository)
     {
