d04131279d24a682cb1fa11435b14a0292bc4d2c
George Drak <sonicgd@gmail.com>
2022-02-17 14:22:03 +0500

    tests(efrepository): restore ThenInclude tests
    Refs: SITKO-CORE-T-8

--- tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/EFTests.cs
+++ tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/EFTests.cs
@@ -30,4 +30,26 @@ public class EFTests : BasicRepositoryTests<EFTestScope>
         var res = await fooBarRepository.AddAsync(fooBar);
         res.IsSuccess.Should().BeTrue();
     }
+
+    [Fact]
+    public async Task ThenInclude()
+    {
+        var scope = await GetScopeAsync();
+
+        var repository = scope.GetService<IRepository<TestModel, Guid>>();
+        Assert.NotNull(repository);
+
+        var item = await repository.GetAsync(query => query.Where(model => model.Bars.Any())
+            .Include(testModel => testModel.Bars).ThenInclude(barModel => barModel.Foos));
+        Assert.NotNull(item);
+        Assert.NotNull(item!.Bars);
+        Assert.NotEmpty(item.Bars);
+        Assert.Single(item.Bars);
+        var bar = item.Bars.First();
+        Assert.Equal(item.Id, bar.TestId);
+        Assert.NotEmpty(bar.Foos);
+        var foo = bar.Foos.First();
+        Assert.NotNull(foo.Bar);
+        Assert.NotNull(foo.Bar!.Test);
+    }
 }
--- tests/Sitko.Core.Repository.Tests/BasicRepositoryTests.cs
+++ tests/Sitko.Core.Repository.Tests/BasicRepositoryTests.cs
@@ -138,27 +138,6 @@ public abstract class BasicRepositoryTests<TScope> : BaseTest<TScope> where TSco
         Assert.Equal(item.Id, item.Bars.First().TestId);
     }
 
-    // [Fact]
-    // public async Task ThenInclude()
-    // {
-    //     var scope = await GetScopeAsync();
-    //
-    //     var repository = scope.GetService<IRepository<TestModel, Guid>>();
-    //     Assert.NotNull(repository);
-    //
-    //     var item = await repository.GetAsync(query => query.Where(model => model.Bars.Any())
-    //         .Include(testModel => testModel.Bars).ThenInclude(barModel => barModel.Foos));
-    //     Assert.NotNull(item);
-    //     Assert.NotNull(item!.Bars);
-    //     Assert.NotEmpty(item.Bars);
-    //     Assert.Single(item.Bars);
-    //     var bar = item.Bars.First();
-    //     Assert.Equal(item.Id, bar.TestId);
-    //     Assert.NotEmpty(bar.Foos);
-    //     var foo = bar.Foos.First();
-    //     Assert.NotNull(foo.Bar);
-    //     Assert.NotNull(foo.Bar!.Test);
-    // }
 
     [Fact]
     public async Task IncludeWithPagination()
