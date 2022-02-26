df6ee18c3751cb8806c693ba31dc8c4ce9938b2b
George Drak <sonicgd@gmail.com>
2022-02-26 16:27:43 +0500

    feat(remoterepository): new WasmHttpRepositoryTransportModule with browser cookies in http requests

---
+++ src/Sitko.Core.Repository.Remote.Wasm/ApplicationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Repository.Remote.Wasm;
+
+[PublicAPI]
+public static class ApplicationExtensions
+{
+    public static Application AddWasmHttpRepositoryTransport(this Application application,
+        Action<HttpRepositoryTransportOptions>? configure = null,
+        string? optionsKey = null) =>
+        application.AddModule<WasmHttpRepositoryTransportModule, HttpRepositoryTransportOptions>(configure, optionsKey);
+
+    public static Application AddWasmHttpRepositoryTransport(this Application application,
+        Action<IApplicationContext, HttpRepositoryTransportOptions> configure,
+        string? optionsKey = null) =>
+        application.AddModule<WasmHttpRepositoryTransportModule, HttpRepositoryTransportOptions>(configure, optionsKey);
+}
---
+++ src/Sitko.Core.Repository.Remote.Wasm/WasmHttpRepositoryTransportModule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Sitko.Core.App;
+
+namespace Sitko.Core.Repository.Remote.Wasm;
+
+public class WasmHttpRepositoryTransportModule : HttpRepositoryTransportModule
+{
+    public override void ConfigureServices(IApplicationContext context, IServiceCollection services,
+        HttpRepositoryTransportOptions startupOptions)
+    {
+        base.ConfigureServices(context, services, startupOptions);
+        services.AddTransient<CookieHandler>();
+    }
+
+    protected override IHttpClientBuilder RegisterHttpClient(IApplicationContext context, IServiceCollection services,
+        HttpRepositoryTransportOptions startupOptions) =>
+        base.RegisterHttpClient(context, services, startupOptions).AddHttpMessageHandler<CookieHandler>();
+}
+
+public class CookieHandler : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
