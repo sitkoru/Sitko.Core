35faf1752311006065c703597e68bba0826b68aa
IgorAlymov <igor.alymov.97@mail.ru>
2022-03-14 16:50:28 +0500

    fix: added support for CancellationToken
    Refs: SITKO-CORE-T-9

--- src/Sitko.Core.App/Json/HttpClientExtensions.cs
+++ src/Sitko.Core.App/Json/HttpClientExtensions.cs
@@ -1,14 +1,15 @@
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sitko.Core.App.Json
 {
     public static class HttpClientExtensions
     {
-        public static async Task<T?> GetJsonAsync<T>(this HttpClient client, string url)
+        public static async Task<T?> GetJsonAsync<T>(this HttpClient client, string url, CancellationToken cancellationToken = default)
         {
-            var response = await client.GetAsync(url);
+            var response = await client.GetAsync(url, cancellationToken);
             return JsonSerializer.Deserialize<T>(await response.Content.ReadAsByteArrayAsync());
         }
     }
