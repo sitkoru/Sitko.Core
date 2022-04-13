08a3105711933d132e2be65b798c6796072fc40d
nadia <nadia__@bk.ru>
2022-04-13 17:30:05 +0500

    test: gzip and unzip state
    Refs: SITKO-CORE-T-10

---
+++ tests/Sitko.Core.App.Blazor.Tests/StateTest.cs
@@ -0,0 +1,58 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Sitko.Core.Blazor.Components;
+using Sitko.Core.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sitko.Core.App.Blazor.Tests;
+
+public class StateTest : BaseTest<StateTestScope>
+{
+    public StateTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public async Task Unzip()
+    {
+        var record = new TestRecord();
+        record.Blocks.Add(new TextBlock
+        {
+            Text = "Block1"
+        });
+        record.Blocks.Add(new TextBlock
+        {
+            Text = "Block2"
+        });
+
+        var scope = await GetScopeAsync();
+        var stateCompressor = scope.GetService<IStateCompressor>();
+
+        var gzip = await stateCompressor.ToGzipAsync(record);
+        var unzipRecord = await stateCompressor.FromGzipAsync<TestRecord>(gzip);
+        unzipRecord.Blocks.Count.Should().Be(2);
+    }
+}
+
+public record TestRecord
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public List<TestBlock> Blocks { get; set; } = new();
+}
+
+public abstract record TestBlock
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public override string ToString() => GetType().Name;
+}
+
+public record TextBlock : TestBlock
+{
+    public override string ToString() => Text;
+    public string Text { get; set; } = "";
+}
+#endif
---
+++ tests/Sitko.Core.App.Blazor.Tests/StateTestScope.cs
@@ -0,0 +1,14 @@
+#if NET6_0_OR_GREATER
+using Microsoft.Extensions.DependencyInjection;
+using Sitko.Core.Blazor.Components;
+using Sitko.Core.Xunit;
+
+namespace Sitko.Core.App.Blazor.Tests;
+
+public class StateTestScope : BaseTestScope
+{
+    protected override IServiceCollection ConfigureServices(IApplicationContext applicationContext,
+        IServiceCollection services, string name) => base.ConfigureServices(applicationContext, services, name)
+        .AddScoped<IStateCompressor, StateCompressor>();
+}
+#endif
