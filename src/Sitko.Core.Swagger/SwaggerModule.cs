3b8744b46472b2cc2fcdf1a94576f6b36f3be6b2
George Drak <sonicgd@gmail.com>
2022-08-01 16:54:30 +0500

    fix(swagger): correct bearer authentication

--- src/Sitko.Core.Swagger/SwaggerModule.cs
+++ src/Sitko.Core.Swagger/SwaggerModule.cs
@@ -29,19 +29,29 @@ public class SwaggerModule : BaseApplicationModule<SwaggerModuleOptions>, IWebAp
             c.SwaggerDoc("v1", new OpenApiInfo { Title = startupOptions.Title, Version = startupOptions.Version });
             if (startupOptions.EnableTokenAuth)
             {
-                c.AddSecurityDefinition("Bearer",
-                    new OpenApiSecurityScheme
-                    {
-                        Description = "Auth token",
-                        Name = "Authorization",
-                        In = ParameterLocation.Header,
-                        Type = SecuritySchemeType.ApiKey
-                    });
-                var security = new OpenApiSecurityRequirement
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    { new OpenApiSecurityScheme { Name = "Bearer" }, Array.Empty<string>() }
-                };
-                c.AddSecurityRequirement(security);
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             }
         });
     }
