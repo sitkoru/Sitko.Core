08b58ba7fc9db4bbbfe6eab6a10f62447a0df83b
George Drak <sonicgd@gmail.com>
2022-08-01 16:59:01 +0500

    fix(s3metadata): fix s3 storage metadata config key

--- src/Sitko.Core.Storage.S3/S3StorageMetadataModule.cs
+++ src/Sitko.Core.Storage.S3/S3StorageMetadataModule.cs
@@ -6,6 +6,6 @@ namespace Sitko.Core.Storage.S3
         S3StorageMetadataProvider<TStorageOptions>, S3StorageMetadataModuleOptions<TStorageOptions>>
         where TStorageOptions : S3StorageOptions, new()
     {
-        public override string OptionsKey => $"Storage:Metadata:FileSystem:{typeof(TStorageOptions).Name}";
+        public override string OptionsKey => $"Storage:Metadata:S3:{typeof(TStorageOptions).Name}";
     }
 }
