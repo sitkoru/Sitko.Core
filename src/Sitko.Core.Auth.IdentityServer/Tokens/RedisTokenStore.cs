d05e27349c83af05e5944285c0e3439db6dd2ae6
George Drak <sonicgd@gmail.com>
2023-02-02 18:42:51 +0500

    fix(auth): use unique key to store user token

--- src/Sitko.Core.Auth.IdentityServer/Tokens/RedisTokenStore.cs
+++ src/Sitko.Core.Auth.IdentityServer/Tokens/RedisTokenStore.cs
@@ -1,8 +1,10 @@
 using System.Security.Claims;
 using System.Text.Json;
 using Duende.AccessTokenManagement.OpenIdConnect;
+using IdentityModel;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Options;
+using Sitko.Core.App;
 using StackExchange.Redis;
 
 namespace Sitko.Core.Auth.IdentityServer.Tokens;
@@ -11,22 +13,32 @@ internal class RedisTokenStore : IUserTokenStore
 {
     private readonly IDatabase redis;
     private readonly IDataProtector dataProtector;
+    private readonly IApplicationContext applicationContext;
     private readonly IOptions<OidcIdentityServerModuleOptions> options;
 
-    public RedisTokenStore(IDataProtectionProvider dataProtectionProvider,
+    public RedisTokenStore(IDataProtectionProvider dataProtectionProvider, IApplicationContext applicationContext,
         IOptions<OidcIdentityServerModuleOptions> options)
     {
+        this.applicationContext = applicationContext;
         this.options = options;
-        redis = ConnectionMultiplexer.Connect($"{options.Value.RedisHost}:{options.Value.RedisPort}").GetDatabase(options.Value.RedisDb);
+        redis = ConnectionMultiplexer.Connect($"{options.Value.RedisHost}:{options.Value.RedisPort}")
+            .GetDatabase(options.Value.RedisDb);
         dataProtector = dataProtectionProvider.CreateProtector(nameof(RedisTokenStore));
+    }
+
+    private string GetUserTokenKey(ClaimsPrincipal user)
+    {
+        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
+                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
+        // token will be unique for user, application and environment
+        return $"DOT_{applicationContext.Name}_{applicationContext.Environment}_{userId}".ToSha256();
     }
 
     public Task<UserToken> GetTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
-        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
-                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
+        var userTokenKey = GetUserTokenKey(user);
 
-        var redisValue = redis.StringGet(userId);
+        var redisValue = redis.StringGet(userTokenKey);
         if (redisValue.HasValue)
         {
             var protectedJson = redisValue.ToString();
@@ -45,21 +57,19 @@ internal class RedisTokenStore : IUserTokenStore
 
     public Task StoreTokenAsync(ClaimsPrincipal user, UserToken token, UserTokenRequestParameters? parameters = null)
     {
-        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
-                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
+        var userTokenKey = GetUserTokenKey(user);
         var json = JsonSerializer.Serialize(token);
         var protectedJson = dataProtector.Protect(json);
-        redis.StringSet(userId, protectedJson);
+        redis.StringSet(userTokenKey, protectedJson);
 
         return Task.CompletedTask;
     }
 
     public Task ClearTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
-        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
-                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
+        var userTokenKey = GetUserTokenKey(user);
 
-        redis.KeyDelete(userId);
+        redis.KeyDelete(userTokenKey);
         return Task.CompletedTask;
     }
 }
