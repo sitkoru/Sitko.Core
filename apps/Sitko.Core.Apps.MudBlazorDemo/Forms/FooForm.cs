ff254dce8c38887dbffc0b2053b5512d10ec586d
George Drak <sonicgd@gmail.com>
2023-02-02 17:21:04 +0500

    feat(apps): add demo form with auto-reset and long save

---
+++ apps/Sitko.Core.Apps.MudBlazorDemo/Forms/FooForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sitko.Core.Apps.MudBlazorDemo.Data.Entities;
+using Sitko.Core.Apps.MudBlazorDemo.Data.Repositories;
+using Sitko.Core.Blazor.Forms;
+using Sitko.Core.Blazor.MudBlazorComponents;
+
+namespace Sitko.Core.Apps.MudBlazorDemo.Forms;
+
+public class FooForm : BaseMudRepositoryForm<FooModel, Guid, FooRepository>
+{
+    protected override async Task<FormSaveResult> AddAsync(FooModel entity)
+    {
+        await Task.Delay(2000);
+        return await base.AddAsync(entity);
+    }
+}
