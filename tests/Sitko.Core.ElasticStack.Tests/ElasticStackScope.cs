6e0a6a90ea2ccf0752dabe515e0c0f57ea717072
George Drak <sonicgd@gmail.com>
2023-03-01 15:44:47 +0500

    test(elasticstack): fill test settings

--- tests/Sitko.Core.ElasticStack.Tests/ElasticStackScope.cs
+++ tests/Sitko.Core.ElasticStack.Tests/ElasticStackScope.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog.Sinks.Elasticsearch;
 using Sitko.Core.App.Web;
 using Sitko.Core.Xunit;
 
@@ -19,6 +20,12 @@ public class ElasticStartup : BaseStartup
 
 public class ElasticApplication : WebApplication<ElasticStartup>
 {
-    public ElasticApplication(string[] args) : base(args) => this.AddElasticStack();
+    public ElasticApplication(string[] args) : base(args) => this.AddElasticStack(options =>
+    {
+        options.ElasticSearchUrls = new List<Uri> { new("http://localhost:9200") };
+        options.LoggingTemplateVersion = AutoRegisterTemplateVersion.ESv8;
+        options.LoggingIndexFormat = "logs-test";
+        options.EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog | EmitEventFailureHandling.RaiseCallback;
+        options.FailureCallback = e => Console.WriteLine("Unable to submit event to elastic: " + e.MessageTemplate);
+    });
 }
-
