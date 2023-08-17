922aa3942ecc35533c7d147fdebc99e6ae68af40
IgorAlymov <igor.alymov.97@mail.ru>
2023-08-17 10:15:37 +0500

    feat: added extension for apply request params to query
    Refs: SITKO-CORE-T-17

---
+++ src/Sitko.Core.Repository.Grpc/RepositoryExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Web;
+using Sitko.Core.Grpc;
+
+namespace Sitko.Core.Repository.Grpc;
+
+public static class RepositoryExtensions
+{
+    public static IRepositoryQuery<TEntity> ApplyRequestParams<TEntity>(this ApiRequestInfo requestParams,
+        IRepositoryQuery<TEntity> query) where TEntity : class, IEntity
+    {
+        if (requestParams.Limit > 0)
+        {
+            query = query.Take(requestParams.Limit);
+        }
+
+        if (requestParams.Offset > 0)
+        {
+            query = query.Skip(requestParams.Offset);
+        }
+
+        if (!string.IsNullOrEmpty(requestParams.OrderBy))
+        {
+            query = query.OrderByString(requestParams.OrderBy);
+        }
+
+        if (string.IsNullOrEmpty(requestParams.Filter) || requestParams.Filter == "null")
+        {
+            return query;
+        }
+
+        var filter = requestParams.Filter;
+        var num = filter.Length % 4;
+        if (num > 0)
+        {
+            filter += new string('=', 4 - num);
+        }
+
+        var whereJson = HttpUtility.UrlDecode(Encoding.UTF8.GetString(Convert.FromBase64String(filter)));
+        if (!string.IsNullOrEmpty(whereJson))
+        {
+            query = query.WhereByString(whereJson);
+        }
+
+        return query;
+    }
+}
