1315bf2481a69b4fb5222c7210f461c6fbda09ad
George Drak <sonicgd@gmail.com>
2023-10-12 16:13:07 +0500

    fix: single cluster for all kafka tasks modules

---
+++ src/Sitko.Core.Kafka/KafkaConfigurator.cs
@@ -0,0 +1,59 @@
+﻿using KafkaFlow;
+using KafkaFlow.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sitko.Core.Kafka;
+
+public class KafkaConfigurator
+{
+    private static readonly Dictionary<string, KafkaConfigurator> Configurators = new();
+    private readonly string[] brokers;
+
+    private KafkaConfigurator(string[] brokers) => this.brokers = brokers;
+
+    private readonly List<Action<IConsumerConfigurationBuilder>> consumerActions = new();
+    private readonly Dictionary<string, Action<IProducerConfigurationBuilder>> producerActions = new();
+
+    public KafkaConfigurator AddProducer(string name, Action<IProducerConfigurationBuilder> configure)
+    {
+        producerActions[name] = configure;
+        return this;
+    }
+
+    public KafkaConfigurator AddConsumer(Action<IConsumerConfigurationBuilder> configure)
+    {
+        consumerActions.Add(configure);
+        return this;
+    }
+
+    public IServiceCollection Build(string name, IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddKafkaFlowHostedService(builder =>
+        {
+            builder
+                .UseMicrosoftLog()
+                .AddCluster(clusterBuilder =>
+                {
+                    clusterBuilder
+                        .WithName(name)
+                        .WithBrokers(brokers);
+                    foreach (var (producerName, configure) in producerActions)
+                    {
+                        clusterBuilder.AddProducer(producerName, configurationBuilder =>
+                        {
+                            configure(configurationBuilder);
+                        });
+                    }
+
+                    foreach (var consumerAction in consumerActions)
+                    {
+                        clusterBuilder.AddConsumer(consumerAction);
+                    }
+                });
+        });
+        return serviceCollection;
+    }
+
+    public static KafkaConfigurator Create(string[] brokers) =>
+        Configurators.SafeGetOrAdd(string.Join("|", brokers.OrderBy(s => s, StringComparer.OrdinalIgnoreCase)), _ => new KafkaConfigurator(brokers));
+}
--- src/Sitko.Core.Tasks.Kafka/KafkaTasksModule.cs
+++ src/Sitko.Core.Tasks.Kafka/KafkaTasksModule.cs
@@ -6,6 +6,7 @@ using KafkaFlow.Serializer;
 using KafkaFlow.TypedHandler;
 using Microsoft.Extensions.DependencyInjection;
 using Sitko.Core.App;
+using Sitko.Core.Kafka;
 using Sitko.Core.Tasks.Data;
 using Sitko.Core.Tasks.Data.Entities;
 using Sitko.Core.Tasks.Execution;
@@ -32,68 +33,52 @@ public class
                 : startupOptions.TopicPrefix)
             : "";
         var kafkaTopic = $"{kafkaTopicPrefix}_{startupOptions.TasksTopic}";
-        services.AddKafkaFlowHostedService(builder =>
+
+        var kafkaConfigurator = KafkaConfigurator.Create(startupOptions.Brokers);
+        kafkaConfigurator
+            .AddProducer("default", builder =>
+            {
+                builder.DefaultTopic(kafkaTopic);
+                builder.AddMiddlewares(middlewareBuilder =>
+                    middlewareBuilder.AddSerializer<JsonCoreSerializer>());
+            });
+        var executorType = typeof(KafkaExecutor<,>);
+        foreach (var groupConsumers in executors.GroupBy(r => r.GroupId))
         {
-            builder
-                .UseMicrosoftLog()
-                .AddCluster(clusterBuilder =>
+            var commonRegistration = groupConsumers.First();
+            var name = $"{applicationContext.Name}/{applicationContext.Id}/{commonRegistration.GroupId}";
+            var parallelThreadCount = groupConsumers.Max(r => r.ParallelThreadCount);
+            var bufferSize = groupConsumers.Max(r => r.BufferSize);
+            kafkaConfigurator.AddConsumer(consumerBuilder =>
+            {
+                consumerBuilder.Topic(kafkaTopic);
+                consumerBuilder.WithName(name);
+                consumerBuilder.WithGroupId(commonRegistration.GroupId);
+                consumerBuilder.WithWorkersCount(parallelThreadCount);
+                consumerBuilder.WithBufferSize(bufferSize);
+                // для гарантии порядка событий
+                consumerBuilder
+                    .WithWorkDistributionStrategy<BytesSumDistributionStrategy>();
+                var consumerConfig = new ConsumerConfig
                 {
-                    clusterBuilder
-                        .WithName($"Tasks_{typeof(TBaseTask).Name}")
-                        .WithBrokers(startupOptions.Brokers)
-                        .AddProducer("default", producerBuilder =>
-                        {
-                            producerBuilder
-                                .DefaultTopic(kafkaTopic)
-                                .AddMiddlewares(configurationBuilder =>
-                                    configurationBuilder.AddSerializer<JsonCoreSerializer>());
-                        });
-                    // регистрируем консьюмеры на каждую группу экзекьюторов
-                    var executorType = typeof(KafkaExecutor<,>);
-                    foreach (var groupConsumers in executors.GroupBy(r => r.GroupId))
+                    AutoOffsetReset = AutoOffsetReset.Latest, ClientId = name, GroupInstanceId = name, PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+                };
+                consumerBuilder.WithConsumerConfig(consumerConfig);
+                consumerBuilder.AddMiddlewares(
+                    middlewares =>
                     {
-                        var commonRegistration = groupConsumers.First();
-                        var name =
-                            $"{applicationContext.Name}/{applicationContext.Id}/{commonRegistration.GroupId}";
-
-                        var parallelThreadCount = groupConsumers.Max(r => r.ParallelThreadCount);
-                        var bufferSize = groupConsumers.Max(r => r.BufferSize);
-
-                        clusterBuilder.AddConsumer(
-                            consumerBuilder =>
-                            {
-                                consumerBuilder.Topic(kafkaTopic);
-                                consumerBuilder.WithName(name);
-                                consumerBuilder.WithGroupId(commonRegistration.GroupId);
-                                consumerBuilder.WithWorkersCount(parallelThreadCount);
-                                consumerBuilder.WithBufferSize(bufferSize);
-                                // для гарантии порядка событий
-                                consumerBuilder
-                                    .WithWorkDistributionStrategy<BytesSumDistributionStrategy>();
-                                var consumerConfig = new ConsumerConfig
-                                {
-                                    AutoOffsetReset = AutoOffsetReset.Latest,
-                                    ClientId = name,
-                                    GroupInstanceId = name,
-                                    PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
-                                };
-                                consumerBuilder.WithConsumerConfig(consumerConfig);
-                                consumerBuilder.AddMiddlewares(
-                                    middlewares =>
-                                    {
-                                        middlewares
-                                            .AddSerializer<JsonCoreSerializer>();
-                                        middlewares.AddTypedHandlers(handlers =>
-                                            handlers.AddHandlers(groupConsumers.Select(r =>
-                                                    executorType.MakeGenericType(r.EventType, r.ExecutorType)))
-                                                .WithHandlerLifetime(InstanceLifetime.Scoped));
-                                    }
-                                );
-                            }
-                        );
+                        middlewares
+                            .AddSerializer<JsonCoreSerializer>();
+                        middlewares.AddTypedHandlers(handlers =>
+                            handlers.AddHandlers(groupConsumers.Select(r =>
+                                    executorType.MakeGenericType(r.EventType, r.ExecutorType)))
+                                .WithHandlerLifetime(InstanceLifetime.Scoped));
                     }
-                });
-        });
+                );
+            });
+        }
+
+        kafkaConfigurator.Build($"Kafka_Tasks_Cluster", services);
     }
 }
 
