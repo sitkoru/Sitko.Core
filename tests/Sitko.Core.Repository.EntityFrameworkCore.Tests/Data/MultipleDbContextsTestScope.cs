0b598b913166e5a5961b4223c80ad52df8bf1c58
George Drak <sonicgd@gmail.com>
2023-10-13 15:08:26 +0500

    fix(ef): configure second db context

--- tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/Data/MultipleDbContextsTestScope.cs
+++ tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/Data/MultipleDbContextsTestScope.cs
@@ -8,7 +8,7 @@ public class MultipleDbContextsTestScope : EFTestScope
     protected override IHostApplicationBuilder ConfigureApplication(IHostApplicationBuilder hostBuilder, string name)
     {
         base.ConfigureApplication(hostBuilder, name);
-        AddDbContext<SecondTestDbContext>(hostBuilder, name);
+        AddDbContext<SecondTestDbContext>(hostBuilder, name, configurePostgres: ConfigurePostgresDatabaseModule);
         hostBuilder.AddEFRepositories<MultipleDbContextsTestScope>();
         return hostBuilder;
     }
