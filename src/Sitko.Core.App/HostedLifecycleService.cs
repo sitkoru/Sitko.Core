479a0ca90e1ba1329cdf70451fa80df71bb82c5f
George Drak <sonicgd@gmail.com>
2023-12-07 00:27:25 +0500

    feat(app): move app init logic outside of LifecycleService to use elsewhere

--- src/Sitko.Core.App/HostedLifecycleService.cs
+++ src/Sitko.Core.App/HostedLifecycleService.cs
@@ -1,146 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Sitko.Core.App.Logging;
+﻿using Microsoft.Extensions.Hosting;
 
 namespace Sitko.Core.App;
 
-internal class HostedLifecycleService : IHostedLifecycleService
+internal class HostedLifecycleService(IApplicationLifecycle lifecycle) : IHostedLifecycleService
 {
-    private readonly ILogger<HostedLifecycleService> logger;
-    private readonly IApplicationContext applicationContext;
-    private readonly IServiceProvider serviceProvider;
-
-
-    private readonly IReadOnlyList<ApplicationModuleRegistration> enabledModules;
-
-    public HostedLifecycleService(ILogger<HostedLifecycleService> logger, IApplicationContext applicationContext,
-        IServiceProvider serviceProvider, IEnumerable<ApplicationModuleRegistration> applicationModuleRegistrations)
-    {
-        this.logger = logger;
-        this.applicationContext = applicationContext;
-        this.serviceProvider = serviceProvider;
-        enabledModules =
-            ModulesHelper.GetEnabledModuleRegistrations(applicationContext, applicationModuleRegistrations);
-    }
-
     public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
-    public async Task StartingAsync(CancellationToken cancellationToken)
-    {
-        await using var scope = serviceProvider.CreateAsyncScope();
+    public Task StartingAsync(CancellationToken cancellationToken) =>
+        lifecycle.StartingAsync(cancellationToken);
 
-        foreach (var enabledModule in enabledModules)
-        {
-            var shouldContinue = await enabledModule.GetInstance()
-                .OnBeforeRunAsync(applicationContext, scope.ServiceProvider);
-            if (!shouldContinue)
-            {
-                Environment.Exit(0);
-                return;
-            }
-        }
+    public Task StartedAsync(CancellationToken cancellationToken) =>
+        lifecycle.StartedAsync(cancellationToken);
 
-        logger.LogInformation("Check required modules");
-        var modulesCheckSuccess = true;
-        foreach (var registration in enabledModules)
-        {
-            var result =
-                registration.CheckRequiredModules(applicationContext,
-                    enabledModules.Select(r => r.Type).ToArray());
-            if (!result.isSuccess)
-            {
-                foreach (var missingModule in result.missingModules)
-                {
-                    Log.Information(
-                        $"Required module {missingModule} for module {registration.Type} is not registered");
-                }
+    public Task StoppingAsync(CancellationToken cancellationToken) =>
+        lifecycle.StoppingAsync(cancellationToken);
 
-                modulesCheckSuccess = false;
-            }
-        }
-
-        if (!modulesCheckSuccess)
-        {
-            throw new InvalidOperationException("Check required modules failed");
-        }
-
-        logger.LogInformation("Init modules");
-
-        foreach (var configurationModule in enabledModules.Select(module => module.GetInstance())
-                     .OfType<IConfigurationModule>())
-        {
-            configurationModule.CheckConfiguration(applicationContext, scope.ServiceProvider);
-        }
-
-        foreach (var registration in enabledModules)
-        {
-            logger.LogInformation("Init module {Module}", registration.Type);
-            await registration.InitAsync(applicationContext, scope.ServiceProvider);
-        }
-
-        foreach (var enabledModule in enabledModules)
-        {
-            var shouldContinue =
-                await enabledModule.GetInstance().OnAfterRunAsync(applicationContext, scope.ServiceProvider);
-            if (!shouldContinue)
-            {
-                Environment.Exit(0);
-            }
-        }
-    }
-
-    public async Task StartedAsync(CancellationToken cancellationToken)
-    {
-        foreach (var moduleRegistration in enabledModules)
-        {
-            try
-            {
-                await moduleRegistration.ApplicationStarted(applicationContext, serviceProvider);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on application started hook in module {Module}: {ErrorText}",
-                    moduleRegistration.Type,
-                    ex.ToString());
-            }
-        }
-    }
-
-    public async Task StoppingAsync(CancellationToken cancellationToken)
-    {
-        foreach (var moduleRegistration in enabledModules)
-        {
-            try
-            {
-                await moduleRegistration.ApplicationStopping(applicationContext, serviceProvider);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on application stopping hook in module {Module}: {ErrorText}",
-                    moduleRegistration.Type,
-                    ex.ToString());
-            }
-        }
-    }
-
-    public async Task StoppedAsync(CancellationToken cancellationToken)
-    {
-        foreach (var moduleRegistration in enabledModules)
-        {
-            try
-            {
-                await moduleRegistration.ApplicationStopped(applicationContext, serviceProvider);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on application stopped hook in module {Module}: {ErrorText}",
-                    moduleRegistration.Type,
-                    ex.ToString());
-            }
-        }
-    }
+    public Task StoppedAsync(CancellationToken cancellationToken) =>
+        lifecycle.StoppedAsync(cancellationToken);
 }
