6594d8196481a6ea4ffdc6b47cfec765a42df8de
George Drak <sonicgd@gmail.com>
2023-12-07 00:27:25 +0500

    feat(app): move app init logic outside of LifecycleService to use elsewhere

---
+++ src/Sitko.Core.App/IApplicationLifecycle.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitko.Core.App;
+
+public interface IApplicationLifecycle
+{
+    Task StartingAsync(CancellationToken cancellationToken);
+
+    Task StartedAsync(CancellationToken cancellationToken);
+
+    Task StoppingAsync(CancellationToken cancellationToken);
+
+    Task StoppedAsync(CancellationToken cancellationToken);
+}
