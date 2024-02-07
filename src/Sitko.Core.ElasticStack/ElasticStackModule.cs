d1c209b1803db195ac68f1df18795c4e608dd9c0
George Drak <sonicgd@gmail.com>
2024-02-07 17:51:14 +0500

    fix(elasticstack): make apm works with IHostApplicationBuilder

--- src/Sitko.Core.ElasticStack/ElasticStackModule.cs
+++ src/Sitko.Core.ElasticStack/ElasticStackModule.cs
@@ -1,21 +1,20 @@
 using System.Globalization;
+using Elastic.Apm.Config;
 using Elastic.Apm.NetCoreAll;
 using Elastic.Apm.SerilogEnricher;
 using Elastic.CommonSchema.Serilog;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.Elasticsearch;
 using Sitko.Core.App;
-using Sitko.Core.App.Web;
 
 namespace Sitko.Core.ElasticStack;
 
 public class ElasticStackModule : BaseApplicationModule<ElasticStackModuleOptions>,
     IHostBuilderModule<ElasticStackModuleOptions>, ILoggingModule<ElasticStackModuleOptions>,
-    IWebApplicationModule<ElasticStackModuleOptions>
+    IConfigurationModule<ElasticStackModuleOptions>
 {
     public override string OptionsKey => "ElasticApm";
 
@@ -24,66 +23,7 @@ public class ElasticStackModule : BaseApplicationModule<ElasticStackModuleOption
     {
         if (startupOptions.ApmEnabled)
         {
-            Environment.SetEnvironmentVariable("ELASTIC_APM_SERVICE_NAME", context.Name);
-            Environment.SetEnvironmentVariable("ELASTIC_APM_SERVICE_VERSION", context.Version);
-            Environment.SetEnvironmentVariable("ELASTIC_APM_TRANSACTION_SAMPLE_RATE",
-                startupOptions.ApmTransactionSampleRate.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:TransactionMaxSpans",
-                startupOptions.ApmTransactionMaxSpans.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:CentralConfig",
-                startupOptions.ApmCentralConfig.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:SanitizeFieldNames",
-                startupOptions.ApmCentralConfig.ToString(CultureInfo.InvariantCulture));
-            if (startupOptions.ApmSanitizeFieldNames != null && startupOptions.ApmSanitizeFieldNames.Count != 0)
-            {
-                Environment.SetEnvironmentVariable("ElasticApm:SanitizeFieldNames",
-                    string.Join(", ", startupOptions.ApmSanitizeFieldNames));
-            }
-
-            if (startupOptions.ApmGlobalLabels.Count != 0)
-            {
-                Environment.SetEnvironmentVariable("ElasticApm:GlobalLabels",
-                    string.Join(",", startupOptions.ApmGlobalLabels.Select(kv => $"{kv.Key}={kv.Value}")));
-            }
-
-            Environment.SetEnvironmentVariable("ElasticApm:ServerUrls",
-                string.Join(",", startupOptions.ApmServerUrls));
-
-            Environment.SetEnvironmentVariable("ElasticApm:SecretToken", startupOptions.ApmSecretToken);
-            Environment.SetEnvironmentVariable("ElasticApm:ApiKey", startupOptions.ApmApiKey);
-            Environment.SetEnvironmentVariable("ElasticApm:VerifyServerCert",
-                startupOptions.ApmVerifyServerCert.ToString());
-            Environment.SetEnvironmentVariable("ElasticApm:FlushInterval",
-                $"{TimeSpan.FromSeconds(startupOptions.ApmFlushIntervalInSeconds).TotalSeconds}s");
-            Environment.SetEnvironmentVariable("ElasticApm:MaxBatchEventCount",
-                startupOptions.ApmMaxBatchEventCount.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:MaxQueueEventCount",
-                startupOptions.ApmMaxQueueEventCount.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:MetricsInterval",
-                $"{TimeSpan.FromSeconds(startupOptions.ApmMetricsIntervalInSeconds).TotalSeconds}s");
-            if (startupOptions.ApmDisableMetrics != null && startupOptions.ApmDisableMetrics.Count != 0)
-            {
-                Environment.SetEnvironmentVariable("ElasticApm:DisableMetrics",
-                    string.Join(",", startupOptions.ApmDisableMetrics));
-            }
-
-            Environment.SetEnvironmentVariable("ElasticApm:CaptureBody", startupOptions.ApmCaptureBody);
-            if (startupOptions.ApmCaptureBodyContentTypes != null &&
-                startupOptions.ApmCaptureBodyContentTypes.Count != 0)
-            {
-                Environment.SetEnvironmentVariable("ElasticApm:CaptureBodyContentTypes",
-                    string.Join(",", startupOptions.ApmCaptureBodyContentTypes));
-            }
-
-            Environment.SetEnvironmentVariable("ElasticApm:CaptureHeaders",
-                startupOptions.ApmCaptureHeaders.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:UseElasticTraceparentHeader",
-                startupOptions.ApmUseElasticTraceparentHeader.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:StackTraceLimit",
-                startupOptions.ApmStackTraceLimit.ToString(CultureInfo.InvariantCulture));
-            Environment.SetEnvironmentVariable("ElasticApm:SpanFramesMinDuration",
-                $"{TimeSpan.FromSeconds(startupOptions.ApmSpanFramesMinDurationInSeconds).TotalMilliseconds}ms");
-            Environment.SetEnvironmentVariable("ElasticApm:ApmLogLevel", startupOptions.ApmLogLevel);
+            hostBuilder.ToHostBuilder().UseAllElasticApm();
         }
     }
 
@@ -138,13 +78,79 @@ public class ElasticStackModule : BaseApplicationModule<ElasticStackModuleOption
         return loggerConfiguration;
     }
 
-    public void ConfigureEndpoints(IApplicationContext applicationContext,
-        IApplicationBuilder appBuilder, IEndpointRouteBuilder endpoints)
+    public void ConfigureAppConfiguration(IApplicationContext context,
+        IConfigurationBuilder configurationBuilder,
+        ElasticStackModuleOptions startupOptions)
     {
-        var options = GetOptions(appBuilder.ApplicationServices);
-        if (options.ApmEnabled)
+        if (startupOptions.ApmEnabled)
         {
-            appBuilder.UseAllElasticApm();
+            var apmOptions = new Dictionary<string, string?>();
+            AddOption(apmOptions, ConfigurationOption.ServiceName, context.Name);
+            AddOption(apmOptions, ConfigurationOption.ServiceVersion, context.Version);
+            AddOption(apmOptions, ConfigurationOption.TransactionSampleRate,
+                startupOptions.ApmTransactionSampleRate.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.TransactionMaxSpans,
+                startupOptions.ApmTransactionMaxSpans.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.CentralConfig,
+                startupOptions.ApmCentralConfig.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.SecretToken, startupOptions.ApmSecretToken);
+            AddOption(apmOptions, ConfigurationOption.ApiKey, startupOptions.ApmApiKey);
+            AddOption(apmOptions, ConfigurationOption.VerifyServerCert, startupOptions.ApmVerifyServerCert.ToString());
+            AddOption(apmOptions, ConfigurationOption.FlushInterval,
+                $"{TimeSpan.FromSeconds(startupOptions.ApmFlushIntervalInSeconds).TotalSeconds}s");
+            AddOption(apmOptions, ConfigurationOption.MaxBatchEventCount,
+                startupOptions.ApmMaxBatchEventCount.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.MaxQueueEventCount,
+                startupOptions.ApmMaxQueueEventCount.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.MetricsInterval,
+                $"{TimeSpan.FromSeconds(startupOptions.ApmMetricsIntervalInSeconds).TotalSeconds}s");
+            AddOption(apmOptions, ConfigurationOption.CaptureBody, startupOptions.ApmCaptureBody);
+            AddOption(apmOptions, ConfigurationOption.CaptureHeaders,
+                startupOptions.ApmCaptureHeaders.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.UseElasticTraceparentHeader,
+                startupOptions.ApmUseElasticTraceparentHeader.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.StackTraceLimit,
+                startupOptions.ApmStackTraceLimit.ToString(CultureInfo.InvariantCulture));
+            AddOption(apmOptions, ConfigurationOption.SpanStackTraceMinDuration,
+                $"{TimeSpan.FromSeconds(startupOptions.ApmSpanFramesMinDurationInSeconds).TotalMilliseconds}ms");
+            AddOption(apmOptions, ConfigurationOption.LogLevel, startupOptions.ApmLogLevel);
+            AddOption(apmOptions, ConfigurationOption.ServerUrl,
+                startupOptions.ApmServerUrls.OrderBy(_ => Guid.NewGuid()).First().ToString());
+
+            if (startupOptions.ApmSanitizeFieldNames != null && startupOptions.ApmSanitizeFieldNames.Count != 0)
+            {
+                AddOption(apmOptions, ConfigurationOption.SanitizeFieldNames,
+                    string.Join(", ", startupOptions.ApmSanitizeFieldNames));
+            }
+
+            if (startupOptions.ApmGlobalLabels.Count != 0)
+            {
+                AddOption(apmOptions, ConfigurationOption.GlobalLabels,
+                    string.Join(",", startupOptions.ApmGlobalLabels.Select(kv => $"{kv.Key}={kv.Value}")));
+            }
+
+            if (startupOptions.ApmDisableMetrics != null && startupOptions.ApmDisableMetrics.Count != 0)
+            {
+                AddOption(apmOptions, ConfigurationOption.DisableMetrics,
+                    string.Join(",", startupOptions.ApmDisableMetrics));
+            }
+
+            if (startupOptions.ApmCaptureBodyContentTypes != null &&
+                startupOptions.ApmCaptureBodyContentTypes.Count != 0)
+            {
+                AddOption(apmOptions, ConfigurationOption.CaptureBodyContentTypes,
+                    string.Join(",", startupOptions.ApmCaptureBodyContentTypes));
+            }
+
+            configurationBuilder.AddInMemoryCollection(apmOptions);
         }
+    }
+
+    private static void AddOption(IDictionary<string, string?> options, ConfigurationOption option, string? value) =>
+        options[$"ElasticApm:{Enum.GetName(typeof(CompareOptions), option)}"] = value;
+
+    public void CheckConfiguration(IApplicationContext context, IServiceProvider serviceProvider)
+    {
+        // do nothing
     }
 }
