826d8eca148cab20089eb761585aae2b89a52795
nadia <nadia__@bk.ru>
2024-04-03 17:29:08 +0500

    feat(mail): use fluent mail instead of mailkit

--- src/Sitko.Core.Email.Smtp/SmtpEmailModuleConfig.cs
+++ src/Sitko.Core.Email.Smtp/SmtpEmailModuleConfig.cs
@@ -1,5 +1,4 @@
 using FluentValidation;
-using MailKit.Security;
 
 namespace Sitko.Core.Email.Smtp;
 
@@ -9,7 +8,7 @@ public class SmtpEmailModuleOptions : FluentEmailModuleOptions
     public int Port { get; set; } = 25;
     public string UserName { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public SecureSocketOptions SocketOptions { get; set; } = SecureSocketOptions.Auto;
+    public bool EnableSsl { get; set; }
 }
 
 public class SmtpEmailModuleOptionsValidator : FluentEmailModuleOptionsValidator<SmtpEmailModuleOptions>
