25cce949bf0c2b3c7f495064a731aae91a3f30ed
nadia <nadia__@bk.ru>
2024-05-21 12:35:57 +0500

    feat: register self provider factory to each client

---
+++ src/Sitko.Core.Grpc.Client/IGrpcTokenProviderFactory.cs
@@ -0,0 +1,16 @@
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sitko.Core.Grpc.Client;
+
+public interface IGrpcTokenProviderFactory<TClient> where TClient : ClientBase<TClient>
+{
+    public IGrpcTokenProvider GetProvider(IServiceProvider serviceProvider);
+}
+
+internal class GrpcTokenProviderFactory<TClient, TTokenProvider> : IGrpcTokenProviderFactory<TClient> where TClient : ClientBase<TClient>
+    where TTokenProvider : class, IGrpcTokenProvider
+
+{
+    public IGrpcTokenProvider GetProvider(IServiceProvider serviceProvider) => serviceProvider.GetService<TTokenProvider>()!;
+}
