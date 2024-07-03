51ff1980c504968640800cecc60b11ae018dde2c
SonicGD <sonicgd@gmail.com>
2024-07-03 15:17:27 +0500

    build: fix nightly command

--- nightly.cmd
+++ nightly.cmd
@@ -3,4 +3,4 @@ for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 
 set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
 
 DEL /Q /F /S "%1\Sitko.Core.*"
-dotnet pack -c Release /p:Version=8.0.0-nightly.%ldt% /p:PublishDir=%1
+dotnet pack -c Release /p:Version=11.20.0-nightly.%ldt% -p:PackageOutputPath=%1
