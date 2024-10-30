e1415aea00ba5587fb32e3686ba2b885c7adc8c7
SonicGD <sonicgd@gmail.com>
2024-10-30 16:11:15 +0500

    feat: use postgres module for storage

--- src/Sitko.Core.Storage.Metadata.Postgres/ApplicationExtensions.cs
+++ src/Sitko.Core.Storage.Metadata.Postgres/ApplicationExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using JetBrains.Annotations;
 using Sitko.Core.App;
+using Sitko.Core.Db.Postgres;
+using Sitko.Core.Storage.Metadata.Postgres.DB;
 
 namespace Sitko.Core.Storage.Metadata.Postgres;
 
@@ -9,18 +11,31 @@ public static class ApplicationExtensions
     public static Application AddPostgresStorageMetadata<TStorageOptions>(this Application application,
         Action<IApplicationContext, PostgresStorageMetadataModuleOptions<TStorageOptions>> configure,
         string? optionsKey = null)
-        where TStorageOptions : StorageOptions =>
-        application
+        where TStorageOptions : StorageOptions
+    {
+        AddPostgresDatabase<TStorageOptions>(application);
+        return application
             .AddModule<PostgresStorageMetadataModule<TStorageOptions>,
                 PostgresStorageMetadataModuleOptions<TStorageOptions>>(
                 configure, optionsKey);
+    }
 
     public static Application AddPostgresStorageMetadata<TStorageOptions>(this Application application,
         Action<PostgresStorageMetadataModuleOptions<TStorageOptions>>? configure = null, string? optionsKey = null)
-        where TStorageOptions : StorageOptions =>
-        application
+        where TStorageOptions : StorageOptions
+    {
+        AddPostgresDatabase<TStorageOptions>(application);
+        return application
             .AddModule<PostgresStorageMetadataModule<TStorageOptions>,
                 PostgresStorageMetadataModuleOptions<TStorageOptions>>(
                 configure, optionsKey);
+    }
+
+    private static void AddPostgresDatabase<TStorageOptions>(Application application)
+        where TStorageOptions : StorageOptions =>
+        application.AddPostgresDatabase<StorageDbContext>(options =>
+            {
+                options.Schema = StorageDbContext.Schema;
+            }, $"Storage:Metadata:Postgres:{typeof(TStorageOptions).Name}");
 }
 
--- src/Sitko.Core.Storage.Metadata.Postgres/PostgresStorageMetadataModule.cs
+++ src/Sitko.Core.Storage.Metadata.Postgres/PostgresStorageMetadataModule.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Sitko.Core.App;
-using Sitko.Core.Storage.Metadata.Postgres.DB;
-
-namespace Sitko.Core.Storage.Metadata.Postgres;
+﻿namespace Sitko.Core.Storage.Metadata.Postgres;
 
 public class
     PostgresStorageMetadataModule<TStorageOptions> : BaseStorageMetadataModule<TStorageOptions,
@@ -12,21 +6,6 @@ public class
     where TStorageOptions : StorageOptions
 {
     public override string OptionsKey => $"Storage:Metadata:Postgres:{typeof(TStorageOptions).Name}";
-    public override string[] OptionKeys => new[] { "Storage:Metadata:Postgres:Default", OptionsKey };
-
-    public override void ConfigureServices(IApplicationContext applicationContext, IServiceCollection services,
-        PostgresStorageMetadataModuleOptions<TStorageOptions> startupOptions)
-    {
-        base.ConfigureServices(applicationContext, services, startupOptions);
-        services.AddDbContextFactory<StorageDbContext>((serviceProvider, builder) =>
-        {
-            var options = serviceProvider
-                .GetRequiredService<IOptions<PostgresStorageMetadataModuleOptions<TStorageOptions>>>();
-            builder.UseNpgsql(options.Value.GetConnectionString(), optionsBuilder =>
-            {
-                optionsBuilder.MigrationsHistoryTable("__EFMigrationsHistory", StorageDbContext.Schema);
-            });
-        });
-    }
+    public override string[] OptionKeys => ["Storage:Metadata:Postgres:Default", OptionsKey];
 }
 
