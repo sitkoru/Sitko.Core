6975701d8672b8d46f221418266039cc11bb518a
SonicGD <sonicgd@gmail.com>
2025-07-09 18:54:36 +0500

    fix(oidc): connect to redis only when needed

--- src/Sitko.Core.Auth.IdentityServer/Tokens/RedisTokenStore.cs
+++ src/Sitko.Core.Auth.IdentityServer/Tokens/RedisTokenStore.cs
@@ -11,40 +11,36 @@ namespace Sitko.Core.Auth.IdentityServer.Tokens;
 
 internal class RedisTokenStore : IUserTokenStore
 {
-    private readonly IDatabase redis;
-    private readonly IDataProtector dataProtector;
     private readonly IApplicationContext applicationContext;
+    private readonly IDataProtector dataProtector;
     private readonly IOptions<OidcIdentityServerModuleOptions> options;
+    private readonly Lazy<IDatabase> redis;
 
     public RedisTokenStore(IDataProtectionProvider dataProtectionProvider, IApplicationContext applicationContext,
         IOptions<OidcIdentityServerModuleOptions> options)
     {
         this.applicationContext = applicationContext;
         this.options = options;
-        redis = ConnectionMultiplexer.Connect($"{options.Value.RedisHost}:{options.Value.RedisPort}", configureOptions =>
-            {
-                if (!string.IsNullOrEmpty(options.Value.RedisPassword))
-                {
-                    configureOptions.Password = options.Value.RedisPassword;
-                }
-            })
-            .GetDatabase(options.Value.RedisDb);
+        redis = new Lazy<IDatabase>(() =>
+        {
+            return ConnectionMultiplexer.Connect($"{options.Value.RedisHost}:{options.Value.RedisPort}",
+                    configureOptions =>
+                    {
+                        if (!string.IsNullOrEmpty(options.Value.RedisPassword))
+                        {
+                            configureOptions.Password = options.Value.RedisPassword;
+                        }
+                    })
+                .GetDatabase(options.Value.RedisDb);
+        }, LazyThreadSafetyMode.ExecutionAndPublication);
         dataProtector = dataProtectionProvider.CreateProtector(nameof(RedisTokenStore));
-    }
-
-    private string GetUserTokenKey(ClaimsPrincipal user)
-    {
-        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
-                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
-        // token will be unique for user, application and environment
-        return $"DOT_{applicationContext.Name}_{applicationContext.Environment}_{userId}".ToSha256();
     }
 
     public Task<UserToken> GetTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
         var userTokenKey = GetUserTokenKey(user);
 
-        var redisValue = redis.StringGet(userTokenKey);
+        var redisValue = redis.Value.StringGet(userTokenKey);
         if (redisValue.HasValue)
         {
             var protectedJson = redisValue.ToString();
@@ -66,7 +62,7 @@ internal class RedisTokenStore : IUserTokenStore
         var userTokenKey = GetUserTokenKey(user);
         var json = JsonSerializer.Serialize(token);
         var protectedJson = dataProtector.Protect(json);
-        redis.StringSet(userTokenKey, protectedJson);
+        redis.Value.StringSet(userTokenKey, protectedJson);
 
         return Task.CompletedTask;
     }
@@ -75,7 +71,15 @@ internal class RedisTokenStore : IUserTokenStore
     {
         var userTokenKey = GetUserTokenKey(user);
 
-        redis.KeyDelete(userTokenKey);
+        redis.Value.KeyDelete(userTokenKey);
         return Task.CompletedTask;
+    }
+
+    private string GetUserTokenKey(ClaimsPrincipal user)
+    {
+        var userId = user.FindFirst(options.Value.UserIdClaimName)?.Value ??
+                     throw new InvalidOperationException($"no {options.Value.UserIdClaimName} claim");
+        // token will be unique for user, application and environment
+        return $"DOT_{applicationContext.Name}_{applicationContext.Environment}_{userId}".ToSha256();
     }
 }
