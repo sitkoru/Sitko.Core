da8f5bfb62f5b7cbd95044a65c9c333190aa9aa3
SonicGD <sonicgd@gmail.com>
2025-07-23 17:28:04 +0500

    test(kafka): fix concurrency

--- tests/Sitko.Core.Queue.Kafka.Tests/Data/EventRegistrator.cs
+++ tests/Sitko.Core.Queue.Kafka.Tests/Data/EventRegistrator.cs
@@ -1,12 +1,14 @@
+using System.Collections.Concurrent;
+
 namespace Sitko.Core.Queue.Kafka.Tests.Data;
 
 public static class EventRegistrator
 {
-    private static readonly HashSet<Guid> RegisteredEvents = new();
-    private static readonly Dictionary<int, Guid[]> RegisteredBatches = new();
+    private static readonly ConcurrentBag<Guid> RegisteredEvents = new();
+    private static readonly ConcurrentDictionary<int, Guid[]> RegisteredBatches = new();
     public static int BatchesCount => RegisteredBatches.Count;
     public static int BatchesMessagesCount => RegisteredBatches.SelectMany(x => x.Value).Count();
-    public static int ProcessedCount => RegisteredEvents.Count;
+    public static int ProcessedCount => RegisteredEvents.Distinct().Count();
 
     public static void Register(Guid messageId) => RegisteredEvents.Add(messageId);
     public static void RegisterBatch(Guid[] messageIds) => RegisteredBatches[RegisteredBatches.Count] = messageIds;
