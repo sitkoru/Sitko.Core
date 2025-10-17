67e6a16a26f13ee46aa254252f9b800692a10750
SonicGD <sonicgd@gmail.com>
2025-10-17 18:39:30 +0500

    fix(efrepository): always use postgres in tests

--- tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/Data/BaseEFTestScope.cs
+++ tests/Sitko.Core.Repository.EntityFrameworkCore.Tests/Data/BaseEFTestScope.cs
@@ -1,6 +1,23 @@
-﻿using Sitko.Core.Repository.Tests.Data;
+﻿using Microsoft.Extensions.Hosting;
+using Sitko.Core.App;
+using Sitko.Core.Repository.Tests.Data;
 using Sitko.Core.Xunit;
 
 namespace Sitko.Core.Repository.EntityFrameworkCore.Tests.Data;
 
-public abstract class BaseEFTestScope : DbBaseTestScope<TestDbContext>;
+public abstract class BaseEFTestScope : DbBaseTestScope<HostApplicationBuilder, TestDbContext, BaseEFTestConfig>
+{
+    protected override IHost BuildApplication(HostApplicationBuilder builder) => builder.Build();
+
+    protected override HostApplicationBuilder CreateHostBuilder()
+    {
+        var builder = Host.CreateApplicationBuilder();
+        builder.AddSitkoCore();
+        return builder;
+    }
+}
+
+public class BaseEFTestConfig : BaseDbTestConfig
+{
+    public override bool UsePostgres => true;
+}
